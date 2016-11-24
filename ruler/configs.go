d291ad1e7a3fda44d049a5b28b828a1048dea307
Jonathan Lange <jml@mumak.net>
2016-11-24 15:30:11 +0000

    Implement client for multi-org route

--- ruler/configs.go
+++ ruler/configs.go
@@ -5,19 +5,49 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"time"
 
 	"github.com/prometheus/prometheus/promql"
 	"github.com/prometheus/prometheus/rules"
 )
 
+// TODO: Extract configs client logic into go client library (ala users)
+
 type cortexConfig struct {
 	RulesFiles map[string]string `json:"rules_files"`
 }
 
+// getOrgConfigs returns all Cortex configurations from a configs api server
+// that have been updated since the given time.
+func getOrgConfigs(configsAPIURL *url.URL, since time.Duration) (map[string]cortexConfig, error) {
+	var suffix string
+	if since == 0 {
+		suffix = fmt.Sprintf("?since=%s", since)
+	} else {
+		suffix = ""
+	}
+	url := fmt.Sprintf("%s/private/api/configs/org/cortex%s", configsAPIURL.String(), suffix)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Invalid response from configs server: %v", res.StatusCode)
+	}
+	var configs map[string]cortexConfig
+	if err := json.NewDecoder(res.Body).Decode(&configs); err != nil {
+		return nil, err
+	}
+	return configs, nil
+}
+
 // getOrgConfig gets the organization's cortex config from a configs api server.
 func getOrgConfig(configsAPIURL *url.URL, userID string) (*cortexConfig, error) {
-	// TODO: Extract configs client logic into go client library (ala users)
-	// TODO: Fix configs server so that we not need org ID in the URL to get authenticated org
 	url := fmt.Sprintf("%s/api/configs/org/%s/cortex", configsAPIURL.String(), userID)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {

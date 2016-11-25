8ca7efd2dab751f74f2d6ff7e6f97137248e7330
Tom Wilkie <tom.wilkie@gmail.com>
2016-11-25 11:03:21 +0000

    Get rid of the old ingester http interface

--- distributor/distributor.go
+++ distributor/distributor.go
@@ -136,26 +136,18 @@ func (d *Distributor) getClientFor(ingester ring.IngesterDesc) (cortex.IngesterC
 		return client, nil
 	}
 
-	if ingester.GRPCHostname != "" {
-		conn, err := grpc.Dial(
-			ingester.GRPCHostname,
-			grpc.WithInsecure(),
-			grpc.WithUnaryInterceptor(grpc_middleware.ChainUnaryClient(
-				otgrpc.OpenTracingClientInterceptor(opentracing.GlobalTracer()),
-				middleware.ClientUserHeaderInterceptor,
-			)),
-		)
-		if err != nil {
-			return nil, err
-		}
-		client = cortex.NewIngesterClient(conn)
-	} else {
-		var err error
-		client, err = NewHTTPIngesterClient(ingester.Hostname, d.cfg.RemoteTimeout)
-		if err != nil {
-			return nil, err
-		}
+	conn, err := grpc.Dial(
+		ingester.GRPCHostname,
+		grpc.WithInsecure(),
+		grpc.WithUnaryInterceptor(grpc_middleware.ChainUnaryClient(
+			otgrpc.OpenTracingClientInterceptor(opentracing.GlobalTracer()),
+			middleware.ClientUserHeaderInterceptor,
+		)),
+	)
+	if err != nil {
+		return nil, err
 	}
+	client = cortex.NewIngesterClient(conn)
 
 	d.clients[ingester.Hostname] = client
 	return client, nil

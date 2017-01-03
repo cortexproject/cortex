234c526e3fd1c3ebe545c1f9c0103765e6e6b3cb
Julius Volz <julius@soundcloud.com>
2017-01-03 18:02:26 +0100

    Allow switching to daily instead of hourly index buckets
    There can be slight overlap around the switchover time, but I deemed it
    safer (and not incorrect) than risking gaps due to some calculation
    error.

--- chunk/chunk_store_test.go
+++ chunk/chunk_store_test.go
@@ -146,3 +146,41 @@ func diff(want, have interface{}) string {
 	})
 	return "\n" + text
 }
+
+func TestBigBuckets(t *testing.T) {
+	scenarios := []struct {
+		from, through, dayEpochFrom model.Time
+		buckets                     []string
+	}{
+		{
+			from:         model.TimeFromUnix(0),
+			through:      model.TimeFromUnix(0).Add(3*24*time.Hour) - 1,
+			dayEpochFrom: model.TimeFromUnix(0).Add(1 * 24 * time.Hour),
+			buckets:      []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "d1", "d2"},
+		},
+		{
+			from:         model.TimeFromUnix(0),
+			through:      model.TimeFromUnix(0).Add(3*24*time.Hour) - 1,
+			dayEpochFrom: model.TimeFromUnix(0).Add(2*24*time.Hour) - 1, // Only the day matters for the epoch start time, not the time.
+			buckets:      []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "d1", "d2"},
+		},
+		{
+			from:         model.TimeFromUnix(0),
+			through:      model.TimeFromUnix(0).Add(3*24*time.Hour) - 1,
+			dayEpochFrom: model.TimeFromUnix(0).Add(1*24*time.Hour) - 1,
+			buckets:      []string{"0", "d0", "d1", "d2"},
+		},
+		{
+			from:         model.TimeFromUnix(0),
+			through:      model.TimeFromUnix(0).Add(2 * 24 * time.Hour),
+			dayEpochFrom: model.TimeFromUnix(0).Add(99 * 24 * time.Hour),
+			buckets:      []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"},
+		},
+	}
+	for i, s := range scenarios {
+		buckets := bigBuckets(s.from, s.through, s.dayEpochFrom)
+		if !reflect.DeepEqual(buckets, s.buckets) {
+			t.Fatalf("%d. unexpected buckets; want %v, got %v", i, s.buckets, buckets)
+		}
+	}
+}

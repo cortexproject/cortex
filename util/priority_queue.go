d5c07ce41f0355cf70f75ce9bbe9ab80d206387f
Jonathan Lange <jml@mumak.net>
2017-01-12 15:17:25 +0000

    Scheduling queue
    Has some tests, but I'm not quite up for plumbing a fake clock through
    `AfterFunc`.

--- util/priority_queue.go
+++ util/priority_queue.go
@@ -3,6 +3,7 @@ package util
 import (
 	"container/heap"
 	"sync"
+	"time"
 )
 
 // PriorityQueue is a priority queue.
@@ -25,6 +26,7 @@ type queue []Op
 func (q queue) Len() int           { return len(q) }
 func (q queue) Less(i, j int) bool { return q[i].Priority() > q[j].Priority() }
 func (q queue) Swap(i, j int)      { q[i], q[j] = q[j], q[i] }
+func (q queue) Top() interface{}   { return q[len(q)-1] }
 
 // Push and Pop use pointer receivers because they modify the slice's length,
 // not just its contents.
@@ -66,24 +68,36 @@ func (pq *PriorityQueue) Close() {
 	pq.cond.Broadcast()
 }
 
-// Enqueue adds an operation to the queue in priority order. If the operation
-// is already on the queue, it will be ignored.
-func (pq *PriorityQueue) Enqueue(op Op) {
-	pq.lock.Lock()
-	defer pq.lock.Unlock()
-
+// enqueue adds an operation to the queue in priority order, but does *not*
+// make it available for dequeueing. If the operation is already on the queue,
+// it will be ignored.
+//
+// Return 'true' if the item is newly added to the queue, 'false' if it was
+// already there.
+func (pq *PriorityQueue) enqueue(op Op) bool {
 	if pq.closed {
 		panic("enqueue on closed queue")
 	}
 
 	_, enqueued := pq.hit[op.Key()]
 	if enqueued {
-		return
+		return false
 	}
 
 	pq.hit[op.Key()] = struct{}{}
 	heap.Push(&pq.queue, op)
-	pq.cond.Broadcast()
+	return true
+}
+
+// Enqueue adds an operation to the queue in priority order. If the operation
+// is already on the queue, it will be ignored.
+func (pq *PriorityQueue) Enqueue(op Op) {
+	pq.lock.Lock()
+	defer pq.lock.Unlock()
+
+	if pq.enqueue(op) {
+		pq.cond.Broadcast()
+	}
 }
 
 // Dequeue will return the op with the highest priority; block if queue is
@@ -103,4 +117,127 @@ func (pq *PriorityQueue) Dequeue() Op {
 	op := heap.Pop(&pq.queue).(Op)
 	delete(pq.hit, op.Key())
 	return op
+}
+
+// ScheduledItem is an item in a queue of scheduled items.
+type ScheduledItem interface {
+	Key() string
+	// Scheduled returns the earliest possible time the time is available for
+	// dequeueing.
+	Scheduled() time.Time
+}
+
+type scheduledOp struct {
+	ScheduledItem
+}
+
+// Priority implements Op.
+func (op scheduledOp) Priority() int64 {
+	return -op.Scheduled().Unix()
+}
+
+// SchedulingQueue is like a priority queue, but the first item is the oldest
+// scheduled item.
+type SchedulingQueue struct {
+	*PriorityQueue
+	timer *time.Timer
+}
+
+// NewPriorityQueue makes a new priority queue.
+func NewSchedulingQueue() *SchedulingQueue {
+	pq := NewPriorityQueue()
+	return &SchedulingQueue{
+		PriorityQueue: pq,
+	}
+}
+
+func (sq *SchedulingQueue) front() ScheduledItem {
+	if len(sq.queue) == 0 {
+		return nil
+	}
+	top := sq.PriorityQueue.queue.Top().(scheduledOp)
+	return top.ScheduledItem
+}
+
+func (sq *SchedulingQueue) frontChanged() {
+	front := sq.front()
+	if front == nil {
+		sq.cancelTimer()
+		return
+	}
+
+	delay := front.Scheduled().Sub(time.Now())
+	if delay <= 0 {
+		sq.cancelTimer()
+		sq.cond.Broadcast()
+		return
+	}
+
+	sq.rescheduleTimer(delay)
+}
+
+func (sq *SchedulingQueue) cancelTimer() {
+	if sq.timer != nil {
+		if !sq.timer.Stop() {
+			<-sq.timer.C
+		}
+		sq.timer = nil
+	}
+}
+
+func (sq *SchedulingQueue) rescheduleTimer(delay time.Duration) {
+	if sq.timer == nil {
+		sq.timer = time.AfterFunc(delay, sq.cond.Broadcast)
+	} else {
+		if !sq.timer.Stop() {
+			<-sq.timer.C
+		}
+		sq.timer.Reset(delay)
+	}
+}
+
+// Enqueue schedules an item for later Dequeueing.
+func (sq *SchedulingQueue) Enqueue(item ScheduledItem) {
+	sq.lock.Lock()
+	defer sq.lock.Unlock()
+
+	if !sq.enqueue(scheduledOp{item}) {
+		return
+	}
+	front := sq.front() // Won't be nil because we just added something!
+	if front.Key() == item.Key() {
+		// New item went to front of the queue.
+		sq.frontChanged()
+	}
+}
+
+// Dequeue takes an item from the queue. If there are no items, or the first
+// item isn't ready to be scheduled, it blocks.
+func (sq *SchedulingQueue) Dequeue() ScheduledItem {
+	sq.lock.Lock()
+	defer sq.lock.Unlock()
+
+	// Wait until there's something to dequeue.
+	for {
+		front := sq.front()
+		if front == nil && sq.closed {
+			// Queue is empty and can't have anything more added, so no point
+			// waiting.
+			return nil
+		}
+		if front != nil {
+			if !front.Scheduled().After(time.Now()) {
+				// Front item is ready to be run, so no point waiting.
+				break
+			}
+		}
+		// Either the queue is empty & open, or the first item is scheduled
+		// for some time in the future. Wait for that to change.
+		sq.cond.Wait()
+	}
+
+	op := heap.Pop(&sq.queue).(scheduledOp)
+	delete(sq.hit, op.Key())
+	sq.frontChanged()
+	return op.ScheduledItem
 }
--- util/priority_queue_test.go
+++ util/priority_queue_test.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"runtime"
 	"strconv"
 	"testing"
@@ -85,5 +86,80 @@ func TestPriorityQueueWait(t *testing.T) {
 	case <-done:
 	case <-time.After(100 * time.Millisecond):
 		t.Fatal("Close didn't unblock Dequeue.")
+	}
+}
+
+type sched time.Time
+
+func (s sched) Scheduled() time.Time {
+	return time.Time(s)
+}
+
+func (s sched) Key() string {
+	return time.Time(s).Format("2006-01-02 15:04:05.000")
+}
+
+func TestSchedulingQueuePriorities(t *testing.T) {
+	queue := NewSchedulingQueue()
+	oneHourAgo := sched(time.Now().Add(-time.Hour))
+	twoHoursAgo := sched(time.Now().Add(-2 * time.Hour))
+	queue.Enqueue(oneHourAgo)
+	queue.Enqueue(twoHoursAgo)
+
+	assert.Equal(t, twoHoursAgo, queue.Dequeue().(sched), fmt.Sprintf("Expected to dequeue %v", twoHoursAgo))
+	assert.Equal(t, oneHourAgo, queue.Dequeue().(sched), fmt.Sprintf("Expected to dequeue %v", oneHourAgo))
+
+	queue.Close()
+	assert.Nil(t, queue.Dequeue(), "Expect nil dequeue")
+}
+
+func TestSchedulingQueuePriorities2(t *testing.T) {
+	queue := NewSchedulingQueue()
+	oneHourAgo := sched(time.Now().Add(-time.Hour))
+	twoHoursAgo := sched(time.Now().Add(-2 * time.Hour))
+	queue.Enqueue(twoHoursAgo)
+	queue.Enqueue(oneHourAgo)
+
+	assert.Equal(t, twoHoursAgo, queue.Dequeue().(sched), fmt.Sprintf("Expected to dequeue %v", twoHoursAgo))
+	assert.Equal(t, oneHourAgo, queue.Dequeue().(sched), fmt.Sprintf("Expected to dequeue %v", oneHourAgo))
+
+	queue.Close()
+	assert.Nil(t, queue.Dequeue(), "Expect nil dequeue")
+}
+
+func TestSchedulingQueueWaitOnEmpty(t *testing.T) {
+	queue := NewSchedulingQueue()
+
+	done := make(chan struct{})
+	go func() {
+		assert.Nil(t, queue.Dequeue(), "Expect nil dequeue")
+		close(done)
+	}()
+
+	runtime.Gosched()
+	queue.Close()
+	select {
+	case <-done:
+	case <-time.After(100 * time.Millisecond):
+		t.Fatal("Close didn't unblock Dequeue.")
+	}
+}
+
+func TestSchedulingQueueWaitOnItem(t *testing.T) {
+	queue := NewSchedulingQueue()
+
+	oneHourAgo := sched(time.Now().Add(-time.Hour))
+	done := make(chan struct{})
+	go func() {
+		assert.Equal(t, oneHourAgo, queue.Dequeue(), "Expected value")
+		close(done)
+	}()
+
+	runtime.Gosched()
+	queue.Enqueue(oneHourAgo)
+	select {
+	case <-done:
+	case <-time.After(100 * time.Millisecond):
+		t.Fatal("Queueing item didn't unblock Dequeue.")
 	}
 }

2454404e6044c824bcd78a557241a4850cfb8278
Tom Wilkie <tom.wilkie@gmail.com>
2017-01-17 15:30:40 +0000

    Make it build...

--- util/validate.go
+++ util/validate.go
@@ -3,7 +3,6 @@ package util
 import (
 	"fmt"
 	"regexp"
-	"strings"
 
 	"github.com/prometheus/common/model"
 )
@@ -12,7 +11,7 @@ var validLabelRE = regexp.MustCompile(`^[a-zA-Z_][a-zA-Z0-9_]*$`)
 
 // ValidateSample returns an err if the sample is invalid
 func ValidateSample(s *model.Sample) error {
-	for k, v := range s.Metric {
+	for k, _ := range s.Metric {
 		if !validLabelRE.MatchString(string(k)) {
 			return fmt.Errorf("invalid label: %s", k)
 		}

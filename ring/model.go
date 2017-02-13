97d3129999af76859a7192480883b2f0b2746600
Tom Wilkie <tomwilkie@users.noreply.github.com>
2017-02-13 15:13:12 +0000

    A a quick benchmark to see how quick it is to workout the ring mapping. (#290)
    * A a quick benchmark to see how quick it is to workout the ring mapping.
    
    * Add pprof endpoints

--- ring/model.go
+++ ring/model.go
@@ -1,7 +1,6 @@
 package ring
 
 import (
-	"encoding/json"
 	"sort"
 	"time"
 
@@ -20,77 +19,10 @@ func ProtoDescFactory() proto.Message {
 	return newDesc()
 }
 
-// DescFactory makes new Descs
-func DescFactory() interface{} {
-	return newDesc()
-}
-
 func newDesc() *Desc {
 	return &Desc{
 		Ingesters: map[string]*IngesterDesc{},
 	}
-}
-
-type oldIngesterDesc struct {
-	Hostname     string        `json:"hostname"`
-	Timestamp    time.Time     `json:"timestamp"`
-	State        IngesterState `json:"state"`
-	GRPCHostname string        `json:"grpc_hostname"`
-	ProtoRing    bool          `json:"proto_ring"`
-}
-
-type oldTokenDesc struct {
-	Token    uint32 `json:"tokens"`
-	Ingester string `json:"ingester"`
-}
-
-// UnmarshalJSON allows the new proto IngesterDescs to read the old JSON format.
-//
-// NB grpc_hostname in the old format is just addr in the new.
-func (d *IngesterDesc) UnmarshalJSON(in []byte) error {
-	var tmp oldIngesterDesc
-	if err := json.Unmarshal(in, &tmp); err != nil {
-		return err
-	}
-
-	d.Addr = tmp.GRPCHostname
-	d.Timestamp = tmp.Timestamp.Unix()
-	d.State = tmp.State
-	d.ProtoRing = tmp.ProtoRing
-	return nil
-}
-
-// MarshalJSON allows the new proto IngesterDescs to write the old JSON format.
-//
-// NB grpc_hostname in the old format is just hostname in the new.
-func (d *IngesterDesc) MarshalJSON() ([]byte, error) {
-	return json.Marshal(oldIngesterDesc{
-		Hostname:     "",
-		Timestamp:    time.Unix(d.Timestamp, 0),
-		State:        d.State,
-		GRPCHostname: d.Addr,
-		ProtoRing:    d.ProtoRing,
-	})
-}
-
-// UnmarshalJSON allows the new proto TokenDescs to read the old JSON format.
-func (d *TokenDesc) UnmarshalJSON(in []byte) error {
-	var tmp oldTokenDesc
-	if err := json.Unmarshal(in, &tmp); err != nil {
-		return err
-	}
-
-	d.Token = tmp.Token
-	d.Ingester = tmp.Ingester
-	return nil
-}
-
-// MarshalJSON allows the new proto TokenDescs to write the old JSON format.
-func (d *TokenDesc) MarshalJSON() ([]byte, error) {
-	return json.Marshal(oldTokenDesc{
-		Token:    d.Token,
-		Ingester: d.Ingester,
-	})
 }
 
 func (d *Desc) addIngester(id, addr string, tokens []uint32, state IngesterState) {
---
+++ ring/ring_test.go
@@ -0,0 +1,45 @@
+package ring
+
+import (
+	"fmt"
+	"testing"
+)
+
+const (
+	numIngester = 100
+	numTokens   = 512
+)
+
+func BenchmarkRing(b *testing.B) {
+	// Make a random ring with N ingesters, and M tokens per ingests
+	desc := newDesc()
+	takenTokens := []uint32{}
+	for i := 0; i < numIngester; i++ {
+		tokens := generateTokens(numTokens, takenTokens)
+		takenTokens = append(takenTokens, tokens...)
+		desc.addIngester(fmt.Sprintf("%d", i), fmt.Sprintf("ingester%d", i), tokens, IngesterState_ACTIVE)
+	}
+
+	consul := newMockConsulClient()
+	ringBytes, err := ProtoCodec{}.Encode(desc)
+	if err != nil {
+		b.Fatal(err)
+	}
+	consul.PutBytes(consulKey, ringBytes)
+
+	r, err := New(Config{
+		ConsulConfig: ConsulConfig{
+			mock: consul,
+		},
+	})
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	// Generate a batch of N random keys, and look them up
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		keys := generateTokens(100, nil)
+		r.BatchGet(keys, 3, Write)
+	}
+}

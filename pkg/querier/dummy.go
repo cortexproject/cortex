273302e66f35ac5fb9dc1cdd0d0ae47b053477b1
Tom Wilkie <tom.wilkie@gmail.com>
2017-06-30 11:28:52 +0100

    Lint

--- pkg/querier/dummy.go
+++ pkg/querier/dummy.go
@@ -2,12 +2,14 @@ package querier
 
 import "github.com/prometheus/prometheus/retrieval"
 
-// DummyTargetRetriever implements TargetRetriever
+// DummyTargetRetriever implements TargetRetriever.
 type DummyTargetRetriever struct{}
 
+// Targets implements TargetRetriever.
 func (r DummyTargetRetriever) Targets() []*retrieval.Target { return nil }
 
-// DummyAlertmanagerRetriever implements AlertmanagerRetriever
+// DummyAlertmanagerRetriever implements AlertmanagerRetriever.
 type DummyAlertmanagerRetriever struct{}
 
+// Alertmanagers implements AlertmanagerRetriever.
 func (r DummyAlertmanagerRetriever) Alertmanagers() []string { return nil }

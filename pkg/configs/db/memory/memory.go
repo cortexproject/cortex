c989ef2731127b8b6d79808b5ec9b9b28a5be6e9
Jonathan Lange <jml@mumak.net>
2017-12-06 15:59:59 +0000

    Remove redundant `config` struct

--- pkg/configs/db/memory/memory.go
+++ pkg/configs/db/memory/memory.go
@@ -6,28 +6,16 @@ import (
 	"github.com/weaveworks/cortex/pkg/configs"
 )
 
-type config struct {
-	cfg configs.Config
-	id  configs.ID
-}
-
-func (c config) toView() configs.View {
-	return configs.View{
-		ID:     c.id,
-		Config: c.cfg,
-	}
-}
-
 // DB is an in-memory database for testing, and local development
 type DB struct {
-	cfgs map[string]config
+	cfgs map[string]configs.View
 	id   uint
 }
 
 // New creates a new in-memory database
 func New(_, _ string) (*DB, error) {
 	return &DB{
-		cfgs: map[string]config{},
+		cfgs: map[string]configs.View{},
 		id:   0,
 	}, nil
 }
@@ -38,31 +26,27 @@ func (d *DB) GetConfig(userID string) (configs.View, error) {
 	if !ok {
 		return configs.View{}, sql.ErrNoRows
 	}
-	return c.toView(), nil
+	return c, nil
 }
 
 // SetConfig sets configuration for a user.
 func (d *DB) SetConfig(userID string, cfg configs.Config) error {
-	d.cfgs[userID] = config{cfg: cfg, id: configs.ID(d.id)}
+	d.cfgs[userID] = configs.View{Config: cfg, ID: configs.ID(d.id)}
 	d.id++
 	return nil
 }
 
 // GetAllConfigs gets all of the configs.
 func (d *DB) GetAllConfigs() (map[string]configs.View, error) {
-	cfgs := map[string]configs.View{}
-	for user, c := range d.cfgs {
-		cfgs[user] = c.toView()
-	}
-	return cfgs, nil
+	return d.cfgs, nil
 }
 
 // GetConfigs gets all of the configs that have changed recently.
 func (d *DB) GetConfigs(since configs.ID) (map[string]configs.View, error) {
 	cfgs := map[string]configs.View{}
 	for user, c := range d.cfgs {
-		if c.id > since {
-			cfgs[user] = c.toView()
+		if c.ID > since {
+			cfgs[user] = c
 		}
 	}
 	return cfgs, nil

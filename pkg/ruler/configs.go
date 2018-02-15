79203225231dc36a4f36e5e6ac020ae98fd6cf37
Jonathan Lange <jml@mumak.net>
2018-02-15 17:09:26 +0000

    Revert "Revert "Remove default for database flag (#708)" (#710)"
    This reverts commit 110b705e1db879ef4f9f4a3a8cbdcf8cc1784373.

--- pkg/ruler/configs.go
+++ pkg/ruler/configs.go
@@ -43,7 +43,7 @@ func NewRulesAPI(cfg ConfigStoreConfig) (RulesAPI, error) {
 	// All of this falderal is to allow for a smooth transition away from
 	// using the configs server and toward directly connecting to the database.
 	// See https://github.com/weaveworks/cortex/issues/619
-	if cfg.ConfigsAPIURL.URL != nil {
+	if cfg.DBConfig.URI != "" {
 		return configsClient{
 			URL:     cfg.ConfigsAPIURL.URL,
 			Timeout: cfg.ClientTimeout,

a44a48f52162c7649266659cc274cee63032a861
Cody Boggs <cody@freshtracks.io>
2018-03-21 09:46:24 -0600

    pull latest weaveworks/common

--- vendor/github.com/weaveworks/common/middleware/grpc_logging.go
+++ vendor/github.com/weaveworks/common/middleware/grpc_logging.go
@@ -12,13 +12,22 @@ import (
 
 const gRPC = "gRPC"
 
-// ServerLoggingInterceptor logs gRPC requests, errors and latency.
-var ServerLoggingInterceptor = func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+// GRPCServerLog logs grpc requests, errors, and latency.
+type GRPCServerLog struct {
+	// WithRequest will log the entire request rather than just the error
+	WithRequest bool
+}
+
+// UnaryServerInterceptor returns an interceptor that logs gRPC requests
+func (s GRPCServerLog) UnaryServerInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 	begin := time.Now()
 	resp, err := handler(ctx, req)
 	entry := logging.With(ctx).WithFields(log.Fields{"method": info.FullMethod, "duration": time.Since(begin)})
 	if err != nil {
-		entry.WithField("request", req).WithError(err).Warn(gRPC)
+		if s.WithRequest {
+			entry = entry.WithField("request", req)
+		}
+		entry.WithError(err).Warn(gRPC)
 	} else {
 		entry.Debugf("%s (success)", gRPC)
 	}

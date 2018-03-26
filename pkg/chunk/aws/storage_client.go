510d41b8c0fd776cea10fa6d1125d0458e9e7be7
Ken Haines <khaines@ea.com>
2018-03-26 10:17:22 -0700

    adding debug output of dropped dynamo requests

--- pkg/chunk/aws/storage_client.go
+++ pkg/chunk/aws/storage_client.go
@@ -627,8 +627,10 @@ func (a storageClient) getDynamoDBChunks(ctx context.Context, chunks []chunk.Chu
 				backoff.Wait()
 				continue
 			}else if ok && awsErr.Code() == "ValidationException"{
-				// this write will never work, so just continue on
+				// this write will never work, so the only option is to drop the offending items and continue.
+				// TODO: add more debug options for capturing data/telemetry about the offending items?
 				level.Warn(util.Logger).Log("Data lost while flushing to Dynamo: %v",awsErr)
+				level.Debug(util.Logger).Log("Dropped request details: \n%v",requests)
 				continue
 			}
 

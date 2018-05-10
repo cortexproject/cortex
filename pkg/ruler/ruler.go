9032d98a299b862204b133d6ca39ff226c64398f
Roger Steneteg <rsteneteg@ea.com>
2018-05-10 15:56:34 -0500

    renamed querier.lookback-delta to promql.lookback-delta and added the parameter to the ruler as well

--- pkg/ruler/ruler.go
+++ pkg/ruler/ruler.go
@@ -112,6 +112,9 @@ func (cfg *Config) RegisterFlags(f *flag.FlagSet) {
 	f.IntVar(&cfg.NotificationQueueCapacity, "ruler.notification-queue-capacity", 10000, "Capacity of the queue for notifications to be sent to the Alertmanager.")
 	f.DurationVar(&cfg.NotificationTimeout, "ruler.notification-timeout", 10*time.Second, "HTTP timeout duration when sending notifications to the Alertmanager.")
 	f.DurationVar(&cfg.GroupTimeout, "ruler.group-timeout", 10*time.Second, "Timeout for rule group evaluation, including sending result to ingester")
+	if flag.Lookup("promql.lookback-delta") == nil {
+		flag.DurationVar(&promql.LookbackDelta, "promql.lookback-delta", promql.LookbackDelta, "Time since the last sample after which a time series is considered stale and ignored by expression evaluations.")
+	}
 }
 
 // Ruler evaluates rules.

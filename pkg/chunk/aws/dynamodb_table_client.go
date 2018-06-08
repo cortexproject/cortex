672cfa89a575f923699f18c15ef3ac97b9de1e22
Bryan Boreham <bjboreham@gmail.com>
2018-06-08 13:01:00 +0000

    Warn but don't fail on limit-exceeded error
    and bump the failure counter too

--- pkg/chunk/aws/dynamodb_table_client.go
+++ pkg/chunk/aws/dynamodb_table_client.go
@@ -353,7 +353,12 @@ func (d dynamoTableClient) UpdateTable(ctx context.Context, current, expected ch
 				return err
 			})
 		}); err != nil {
-			return err
+			recordDynamoError(expected.Name, err, "DynamoDB.UpdateTable")
+			if awsErr, ok := err.(awserr.Error); ok && awsErr.Code() == "LimitExceededException" {
+				level.Warn(util.Logger).Log("msg", "update limit exceeded", "err", err)
+			} else {
+				return err
+			}
 		}
 	}
 

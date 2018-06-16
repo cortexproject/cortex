916d9f9fab354f2acf2cb9a0c796a522329c6e19
Tom Wilkie <tom.wilkie@gmail.com>
2018-06-16 16:16:03 +0100

    Test all combinations of various distributor paramters - sharding, number of ingester, number of failed ingesters etc.
    Make the mockIngester stateful, so we check we read back stuff that was written.
    Generate the list of test cases to cover all the combinations.
    
    Signed-off-by: Tom Wilkie <tom.wilkie@gmail.com>

--- pkg/distributor/distributor_test.go
+++ pkg/distributor/distributor_test.go
@@ -3,6 +3,9 @@ package distributor
 import (
 	"fmt"
 	"net/http"
+	"sort"
+	"strconv"
+	"sync"
 	"testing"
 	"time"
 
@@ -22,171 +25,195 @@ import (
 var (
 	errFail = fmt.Errorf("Fail")
 	success = &client.WriteResponse{}
+	ctx     = user.InjectOrgID(context.Background(), "user")
 )
 
 func TestDistributorPush(t *testing.T) {
-	ctx := user.InjectOrgID(context.Background(), "user")
 	for i, tc := range []struct {
-		ingesters        []mockIngester
+		numIngesters     int
+		happyIngesters   int
 		samples          int
 		expectedResponse *client.WriteResponse
 		expectedError    error
 	}{
 		// A push of no samples shouldn't block or return error, even if ingesters are sad
 		{
-			ingesters:        []mockIngester{{}, {}, {}},
+			numIngesters:     3,
+			happyIngesters:   0,
 			expectedResponse: success,
 		},
 
 		// A push to 3 happy ingesters should succeed
 		{
-			samples: 10,
-			ingesters: []mockIngester{
-				{happy: true},
-				{happy: true},
-				{happy: true},
-			},
+			numIngesters:     3,
+			happyIngesters:   3,
+			samples:          10,
 			expectedResponse: success,
 		},
 
 		// A push to 2 happy ingesters should succeed
 		{
-			samples: 10,
-			ingesters: []mockIngester{
-				{},
-				{happy: true},
-				{happy: true},
-			},
+			numIngesters:     3,
+			happyIngesters:   2,
+			samples:          10,
 			expectedResponse: success,
 		},
 
 		// A push to 1 happy ingesters should fail
 		{
-			samples: 10,
-			ingesters: []mockIngester{
-				{},
-				{},
-				{happy: true},
-			},
-			expectedError: errFail,
+			numIngesters:   3,
+			happyIngesters: 1,
+			samples:        10,
+			expectedError:  errFail,
 		},
 
 		// A push to 0 happy ingesters should fail
 		{
-			samples:       10,
-			ingesters:     []mockIngester{{}, {}, {}},
-			expectedError: errFail,
+			numIngesters:   3,
+			happyIngesters: 0,
+			samples:        10,
+			expectedError:  errFail,
 		},
 
 		// A push exceeding burst size should fail
 		{
-			samples: 30,
-			ingesters: []mockIngester{
-				{happy: true},
-				{happy: true},
-				{happy: true},
-			},
-			expectedError: httpgrpc.Errorf(http.StatusTooManyRequests, "ingestion rate limit (20) exceeded while adding 30 samples"),
+			numIngesters:   3,
+			happyIngesters: 3,
+			samples:        30,
+			expectedError:  httpgrpc.Errorf(http.StatusTooManyRequests, "ingestion rate limit (20) exceeded while adding 30 samples"),
 		},
 	} {
-		t.Run(fmt.Sprintf("[%d]", i), func(t *testing.T) {
-			d := prepare(t, tc.ingesters)
+		for _, shardByMetricName := range []bool{true, false} {
+			t.Run(fmt.Sprintf("[%d](shardByMetricName=%v)", i, shardByMetricName), func(t *testing.T) {
+				d := prepare(t, tc.numIngesters, tc.happyIngesters, shardByMetricName)
+				defer d.Stop()
+
+				request := makeWriteRequest(tc.samples)
+				response, err := d.Push(ctx, request)
+				assert.Equal(t, tc.expectedResponse, response)
+				assert.Equal(t, tc.expectedError, err)
+			})
+		}
+	}
+}
+
+func TestDistributorPushQuery(t *testing.T) {
+	nameMatcher := mustEqualMatcher("__name__", "foo")
+	barMatcher := mustEqualMatcher("bar", "baz")
+
+	type testcase struct {
+		name              string
+		numIngesters      int
+		happyIngesters    int
+		samples           int
+		matchers          []*labels.Matcher
+		expectedResponse  model.Matrix
+		expectedError     error
+		shardByMetricName bool
+	}
+
+	// We'll programatically build the test cases now, as we want complete
+	// coverage along quite a few different axis.
+	testcases := []testcase{}
+
+	// Run every test in both sharding modes.
+	for _, shardByMetricName := range []bool{true, false} {
+
+		// Test with between 3 and 10 ingesters.
+		for numIngesters := 3; numIngesters < 10; numIngesters++ {
+
+			// Test with between 0 and numIngesters "happy" ingesters.
+			for happyIngesters := 0; happyIngesters <= numIngesters; happyIngesters++ {
+
+				// Queriers with more than one failed ingester should fail.
+				if numIngesters-happyIngesters > 1 {
+					testcases = append(testcases, testcase{
+						name:              fmt.Sprintf("ExpectFail(shardByMetricName=%v,numIngester=%d,happyIngester=%d)", shardByMetricName, numIngesters, happyIngesters),
+						numIngesters:      numIngesters,
+						happyIngesters:    happyIngesters,
+						matchers:          []*labels.Matcher{nameMatcher, barMatcher},
+						expectedError:     errFail,
+						shardByMetricName: shardByMetricName,
+					})
+					continue
+				}
+
+				// Reading all the samples back should succeed.
+				testcases = append(testcases, testcase{
+					name:              fmt.Sprintf("ReadAll(shardByMetricName=%v,numIngester=%d,happyIngester=%d)", shardByMetricName, numIngesters, happyIngesters),
+					numIngesters:      numIngesters,
+					happyIngesters:    happyIngesters,
+					samples:           10,
+					matchers:          []*labels.Matcher{nameMatcher, barMatcher},
+					expectedResponse:  expectedResponse(0, 10),
+					shardByMetricName: shardByMetricName,
+				})
+
+				// As should reading none of the samples back.
+				testcases = append(testcases, testcase{
+					name:              fmt.Sprintf("ReadNone(shardByMetricName=%v,numIngester=%d,happyIngester=%d)", shardByMetricName, numIngesters, happyIngesters),
+					numIngesters:      numIngesters,
+					happyIngesters:    happyIngesters,
+					samples:           10,
+					matchers:          []*labels.Matcher{nameMatcher, mustEqualMatcher("not", "found")},
+					expectedResponse:  expectedResponse(0, 0),
+					shardByMetricName: shardByMetricName,
+				})
+
+				// And reading each sample individually.
+				for i := 0; i < 10; i++ {
+					testcases = append(testcases, testcase{
+						name:              fmt.Sprintf("ReadOne(shardByMetricName=%v, sample=%d,numIngester=%d,happyIngester=%d)", shardByMetricName, i, numIngesters, happyIngesters),
+						numIngesters:      numIngesters,
+						happyIngesters:    happyIngesters,
+						samples:           10,
+						matchers:          []*labels.Matcher{nameMatcher, mustEqualMatcher("sample", strconv.Itoa(i))},
+						expectedResponse:  expectedResponse(i, i+1),
+						shardByMetricName: shardByMetricName,
+					})
+				}
+			}
+		}
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			d := prepare(t, tc.numIngesters, tc.happyIngesters, tc.shardByMetricName)
 			defer d.Stop()
 
 			request := makeWriteRequest(tc.samples)
-			response, err := d.Push(ctx, request)
-			assert.Equal(t, tc.expectedResponse, response, "Wrong response")
-			assert.Equal(t, tc.expectedError, err, "Wrong error")
+			writeResponse, err := d.Push(ctx, request)
+			assert.Equal(t, &client.WriteResponse{}, writeResponse)
+			assert.Nil(t, err)
+
+			response, err := d.Query(ctx, 0, 10, tc.matchers...)
+			sort.Sort(response)
+			assert.Equal(t, tc.expectedResponse, response)
+			assert.Equal(t, tc.expectedError, err)
 		})
 	}
 }
 
-func TestDistributorQuery(t *testing.T) {
-	ctx := user.InjectOrgID(context.Background(), "user")
-
-	nameMatcher, err := labels.NewMatcher(labels.MatchEqual, "__name__", "foo")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	jobMatcher, err := labels.NewMatcher(labels.MatchEqual, "job", "foo")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	matchers := []*labels.Matcher{
-		nameMatcher,
-		jobMatcher,
-	}
-
-	for i, tc := range []struct {
-		ingesters        []mockIngester
-		expectedResponse model.Matrix
-		expectedError    error
-	}{
-		// A query to 3 happy ingesters should succeed
-		{
-			ingesters: []mockIngester{
-				{happy: true},
-				{happy: true},
-				{happy: true},
-			},
-			expectedResponse: expectedResponse(0, 2),
-		},
-
-		// A query to 2 happy ingesters should succeed
-		{
-			ingesters: []mockIngester{
-				{happy: false},
-				{happy: true},
-				{happy: true},
-			},
-			expectedResponse: expectedResponse(0, 2),
-		},
-
-		// A query to 1 happy ingesters should fail
-		{
-			ingesters: []mockIngester{
-				{happy: false},
-				{happy: false},
-				{happy: true},
-			},
-			expectedError: errFail,
-		},
-
-		// A query to 0 happy ingesters should succeed
-		{
-			ingesters: []mockIngester{
-				{happy: false},
-				{happy: false},
-				{happy: false},
-			},
-			expectedError: errFail,
-		},
-	} {
-		t.Run(fmt.Sprintf("[%d]", i), func(t *testing.T) {
-			d := prepare(t, tc.ingesters)
-			defer d.Stop()
-
-			for _, matcher := range matchers {
-				response, err := d.Query(ctx, 0, 10, matcher)
-				assert.Equal(t, tc.expectedResponse, response, "Wrong response")
-				assert.Equal(t, tc.expectedError, err, "Wrong error")
-			}
+func prepare(t *testing.T, numIngesters, happyIngesters int, shardByMetricName bool) *Distributor {
+	ingesters := []mockIngester{}
+	for i := 0; i < happyIngesters; i++ {
+		ingesters = append(ingesters, mockIngester{
+			happy: true,
 		})
 	}
-}
+	for i := happyIngesters; i < numIngesters; i++ {
+		ingesters = append(ingesters, mockIngester{})
+	}
 
-func prepare(t *testing.T, ingesters []mockIngester) *Distributor {
 	ingesterDescs := []*ring.IngesterDesc{}
-	ingestersByAddr := map[string]mockIngester{}
-	for i, ingester := range ingesters {
+	ingestersByAddr := map[string]*mockIngester{}
+	for i := range ingesters {
 		addr := fmt.Sprintf("%d", i)
 		ingesterDescs = append(ingesterDescs, &ring.IngesterDesc{
 			Addr:      addr,
 			Timestamp: time.Now().Unix(),
 		})
-		ingestersByAddr[addr] = ingester
+		ingestersByAddr[addr] = &ingesters[i]
 	}
 
 	ring := mockRing{
@@ -207,6 +234,7 @@ func prepare(t *testing.T, ingesters []mockIngester) *Distributor {
 		IngestionRateLimit:    20,
 		IngestionBurstSize:    20,
 		ingesterClientFactory: factory,
+		ShardByMetricName:     true,
 	}, ring)
 	if err != nil {
 		t.Fatal(err)
@@ -236,20 +264,31 @@ func makeWriteRequest(samples int) *client.WriteRequest {
 }
 
 func expectedResponse(start, end int) model.Matrix {
-	result := model.Matrix{
-		&model.SampleStream{
-			Metric: model.Metric{"__name__": "foo"},
-		},
-	}
+	result := model.Matrix{}
 	for i := start; i < end; i++ {
-		result[0].Values = append(result[0].Values,
-			model.SamplePair{
-				Value:     model.SampleValue(i),
-				Timestamp: model.Time(i),
+		result = append(result, &model.SampleStream{
+			Metric: model.Metric{
+				"__name__": "foo",
+				"bar":      "baz",
+				"sample":   model.LabelValue(fmt.Sprintf("%d", i)),
 			},
-		)
+			Values: []model.SamplePair{
+				model.SamplePair{
+					Value:     model.SampleValue(i),
+					Timestamp: model.Time(i),
+				},
+			},
+		})
 	}
 	return result
+}
+
+func mustEqualMatcher(k, v string) *labels.Matcher {
+	m, err := labels.NewMatcher(labels.MatchEqual, k, v)
+	if err != nil {
+		panic(err)
+	}
+	return m
 }
 
 // mockRing doesn't do virtual nodes, just returns mod(key) + replicationFactor
@@ -295,46 +334,78 @@ func (r mockRing) ReplicationFactor() int {
 }
 
 type mockIngester struct {
+	sync.Mutex
 	client.IngesterClient
-	happy bool
-	stats client.UsersStatsResponse
+	happy      bool
+	stats      client.UsersStatsResponse
+	timeseries map[uint32]*client.TimeSeries
 }
 
-func (i mockIngester) Push(ctx context.Context, in *client.WriteRequest, opts ...grpc.CallOption) (*client.WriteResponse, error) {
+func (i *mockIngester) Push(ctx context.Context, req *client.WriteRequest, opts ...grpc.CallOption) (*client.WriteResponse, error) {
+	i.Lock()
+	defer i.Unlock()
+
 	if !i.happy {
 		return nil, errFail
 	}
+
+	if i.timeseries == nil {
+		i.timeseries = map[uint32]*client.TimeSeries{}
+	}
+
+	orgid, err := user.ExtractOrgID(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	for j := range req.Timeseries {
+		hash, _ := shardByAllLabels(orgid, req.Timeseries[j].Labels)
+		existing, ok := i.timeseries[hash]
+		if !ok {
+			i.timeseries[hash] = &req.Timeseries[j]
+		} else {
+			existing.Samples = append(existing.Samples, req.Timeseries[j].Samples...)
+		}
+	}
+
 	return &client.WriteResponse{}, nil
 }
 
-func (i mockIngester) Query(ctx context.Context, in *client.QueryRequest, opts ...grpc.CallOption) (*client.QueryResponse, error) {
+func (i *mockIngester) Query(ctx context.Context, req *client.QueryRequest, opts ...grpc.CallOption) (*client.QueryResponse, error) {
+	i.Lock()
+	defer i.Unlock()
+
 	if !i.happy {
 		return nil, errFail
 	}
-	return &client.QueryResponse{
-		Timeseries: []client.TimeSeries{
-			{
-				Labels: []client.LabelPair{
-					{
-						Name:  []byte("__name__"),
-						Value: []byte("foo"),
-					},
-				},
-				Samples: []client.Sample{
-					{
-						Value:       0,
-						TimestampMs: 0,
-					},
-					{
-						Value:       1,
-						TimestampMs: 1,
-					},
-				},
-			},
-		},
-	}, nil
+
+	_, _, matchers, err := client.FromQueryRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	response := client.QueryResponse{}
+	for _, ts := range i.timeseries {
+		if match(ts.Labels, matchers) {
+			response.Timeseries = append(response.Timeseries, *ts)
+		}
+	}
+	return &response, nil
 }
 
-func (i mockIngester) AllUserStats(ctx context.Context, in *client.UserStatsRequest, opts ...grpc.CallOption) (*client.UsersStatsResponse, error) {
+func (i *mockIngester) AllUserStats(ctx context.Context, in *client.UserStatsRequest, opts ...grpc.CallOption) (*client.UsersStatsResponse, error) {
 	return &i.stats, nil
+}
+
+func match(labels []client.LabelPair, matchers []*labels.Matcher) bool {
+outer:
+	for _, matcher := range matchers {
+		for _, labels := range labels {
+			if matcher.Name == string(labels.Name) && matcher.Matches(string(labels.Value)) {
+				continue outer
+			}
+		}
+		return false
+	}
+	return true
 }

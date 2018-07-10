aef2e91beafce5428bd806327e3df8ef79b384e4
Tom Wilkie <tom.wilkie@gmail.com>
2018-07-10 17:57:36 +0100

    Review feedback: Use simple Less function for chunks in test.
    Signed-off-by: Tom Wilkie <tom.wilkie@gmail.com>

--- pkg/chunk/storage/by_key_test.go
+++ pkg/chunk/storage/by_key_test.go
@@ -9,13 +9,4 @@ type ByKey []chunk.Chunk
 
 func (cs ByKey) Len() int           { return len(cs) }
 func (cs ByKey) Swap(i, j int)      { cs[i], cs[j] = cs[j], cs[i] }
-func (cs ByKey) Less(i, j int) bool { return lessByKey(cs[i], cs[j]) }
-
-// This comparison uses all the same information as Chunk.ExternalKey()
-func lessByKey(a, b chunk.Chunk) bool {
-	return a.UserID < b.UserID ||
-		(a.UserID == b.UserID && (a.Fingerprint < b.Fingerprint ||
-			(a.Fingerprint == b.Fingerprint && (a.From < b.From ||
-				(a.From == b.From && (a.Through < b.Through ||
-					(a.Through == b.Through && a.Checksum < b.Checksum)))))))
-}
+func (cs ByKey) Less(i, j int) bool { return cs[i].ExternalKey() < cs[j].ExternalKey() }

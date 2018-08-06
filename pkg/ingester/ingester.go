cortexproject/cortex

commit 6f0118a0afaebcfa80a09540f8b4f31a9016541d

Record validation errors in metrics, not log lines (#890)
* Record validation errors in metrics, not log lines

To reduce the causes of high log volumes

* Add all validation failures to metrics

* Add userID to validation metric in the ingester

pkg/ingester/ingester.go
@@ -23,6 +23,7 @@ import (
 	"github.com/weaveworks/cortex/pkg/ingester/client"
 	"github.com/weaveworks/cortex/pkg/ring"
 	"github.com/weaveworks/cortex/pkg/util"
+	"github.com/weaveworks/cortex/pkg/util/validation"
 )
 
 const (
@@ -335,6 +336,11 @@ func (i *Ingester) append(ctx context.Context, sample *model.Sample) error {
 		Value:     sample.Value,
 		Timestamp: sample.Timestamp,
 	}); err != nil {
+		if mse, ok := err.(*memorySeriesError); ok {
+			validation.DiscardedSamples.WithLabelValues(mse.errorType, state.userID).Inc()
+			// Use a dumb string template to avoid the message being parsed as a template
+			err = httpgrpc.Errorf(http.StatusBadRequest, "%s", mse.message)
+		}
 		return err
 	}
 

pkg/ingester/series.go
@@ -2,15 +2,13 @@ package ingester
 
 import (
 	"fmt"
-	"net/http"
 	"sort"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/model"
-	"github.com/weaveworks/common/httpgrpc"
+
 	"github.com/weaveworks/cortex/pkg/prom1/storage/local/chunk"
 	"github.com/weaveworks/cortex/pkg/prom1/storage/metric"
-	"github.com/weaveworks/cortex/pkg/util/validation"
 )
 
 var (
@@ -41,6 +39,15 @@ type memorySeries struct {
 	lastSampleValue    model.SampleValue
 }
 
+type memorySeriesError struct {
+	message   string
+	errorType string
+}
+
+func (error *memorySeriesError) Error() string {
+	return error.message
+}
+
 // newMemorySeries returns a pointer to a newly allocated memorySeries for the
 // given metric.
 func newMemorySeries(m model.Metric) *memorySeries {
@@ -65,12 +72,16 @@ func (s *memorySeries) add(v model.SamplePair) error {
 		return nil
 	}
 	if v.Timestamp == s.lastTime {
-		validation.DiscardedSamples.WithLabelValues(duplicateSample).Inc()
-		return httpgrpc.Errorf(http.StatusBadRequest, "sample with repeated timestamp but different value for series %v; last value: %v, incoming value: %v", s.metric, s.lastSampleValue, v.Value)
+		return &memorySeriesError{
+			message:   fmt.Sprintf("sample with repeated timestamp but different value for series %v; last value: %v, incoming value: %v", s.metric, s.lastSampleValue, v.Value),
+			errorType: "new-value-for-timestamp",
+		}
 	}
 	if v.Timestamp < s.lastTime {
-		validation.DiscardedSamples.WithLabelValues(outOfOrderTimestamp).Inc()
-		return httpgrpc.Errorf(http.StatusBadRequest, "sample timestamp out of order for series %v; last timestamp: %v, incoming timestamp: %v", s.metric, s.lastTime, v.Timestamp) // Caused by the caller.
+		return &memorySeriesError{
+			message:   fmt.Sprintf("sample timestamp out of order for series %v; last timestamp: %v, incoming timestamp: %v", s.metric, s.lastTime, v.Timestamp),
+			errorType: "sample-out-of-order",
+		}
 	}
 
 	if len(s.chunkDescs) == 0 || s.headChunkClosed {

pkg/distributor/distributor.go
@@ -14,7 +14,6 @@ import (
 	"golang.org/x/time/rate"
 	"google.golang.org/grpc/health/grpc_health_v1"
 
-	"github.com/go-kit/kit/log/level"
 	opentracing "github.com/opentracing/opentracing-go"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
@@ -231,16 +230,14 @@ func (d *Distributor) Push(ctx context.Context, req *client.WriteRequest) (*clie
 			return nil, err
 		}
 
-		if err := d.cfg.validationConfig.ValidateLabels(ts.Labels); err != nil {
-			level.Error(util.WithContext(ctx, util.Logger)).Log("msg", "error validating sample", "err", err)
+		if err := d.cfg.validationConfig.ValidateLabels(userID, ts.Labels); err != nil {
 			lastPartialErr = err
 			continue
 		}
 
 		metricName, _ := extract.MetricNameFromLabelPairs(ts.Labels)
 		for _, s := range ts.Samples {
-			if err := d.cfg.validationConfig.ValidateSample(metricName, s); err != nil {
-				level.Error(util.WithContext(ctx, util.Logger)).Log("msg", "error validating sample", "err", err)
+			if err := d.cfg.validationConfig.ValidateSample(userID, metricName, s); err != nil {
 				lastPartialErr = err
 				continue
 			}

pkg/util/validation/validate.go
@@ -28,6 +28,9 @@ const (
 	greaterThanMaxSampleAge = "greater_than_max_sample_age"
 	maxLabelNamesPerSeries  = "max_label_names_per_series"
 	tooFarInFuture          = "too_far_in_future"
+	invalidLabel            = "label_invalid"
+	labelNameTooLong        = "label_name_too_long"
+	labelValueTooLong       = "label_value_too_long"
 )
 
 // DiscardedSamples is a metric of the number of discarded samples, by reason.
@@ -36,7 +39,7 @@ var DiscardedSamples = prometheus.NewCounterVec(
 		Name: "cortex_discarded_samples_total",
 		Help: "The total number of samples that were discarded.",
 	},
-	[]string{discardReasonLabel},
+	[]string{discardReasonLabel, "user"},
 )
 
 func init() {
@@ -71,14 +74,14 @@ func (cfg *Config) RegisterFlags(f *flag.FlagSet) {
 }
 
 // ValidateSample returns an err if the sample is invalid.
-func (cfg *Config) ValidateSample(metricName []byte, s client.Sample) error {
+func (cfg *Config) ValidateSample(userID string, metricName []byte, s client.Sample) error {
 	if cfg.RejectOldSamples && model.Time(s.TimestampMs) < model.Now().Add(-cfg.RejectOldSamplesMaxAge) {
-		DiscardedSamples.WithLabelValues(greaterThanMaxSampleAge).Inc()
+		DiscardedSamples.WithLabelValues(greaterThanMaxSampleAge, userID).Inc()
 		return httpgrpc.Errorf(http.StatusBadRequest, errTooOld, metricName, model.Time(s.TimestampMs))
 	}
 
 	if model.Time(s.TimestampMs) > model.Now().Add(cfg.CreationGracePeriod) {
-		DiscardedSamples.WithLabelValues(tooFarInFuture).Inc()
+		DiscardedSamples.WithLabelValues(tooFarInFuture, userID).Inc()
 		return httpgrpc.Errorf(http.StatusBadRequest, errTooNew, metricName, model.Time(s.TimestampMs))
 	}
 
@@ -86,7 +89,7 @@ func (cfg *Config) ValidateSample(metricName []byte, s client.Sample) error {
 }
 
 // ValidateLabels returns an err if the labels are invalid.
-func (cfg *Config) ValidateLabels(ls []client.LabelPair) error {
+func (cfg *Config) ValidateLabels(userID string, ls []client.LabelPair) error {
 	metricName, err := extract.MetricNameFromLabelPairs(ls)
 	if err != nil {
 		return httpgrpc.Errorf(http.StatusBadRequest, errMissingMetricName)
@@ -98,19 +101,30 @@ func (cfg *Config) ValidateLabels(ls []client.LabelPair) error {
 
 	numLabelNames := len(ls)
 	if numLabelNames > cfg.MaxLabelNamesPerSeries {
-		DiscardedSamples.WithLabelValues(maxLabelNamesPerSeries).Inc()
+		DiscardedSamples.WithLabelValues(maxLabelNamesPerSeries, userID).Inc()
 		return httpgrpc.Errorf(http.StatusBadRequest, errTooManyLabels, metricName, numLabelNames, cfg.MaxLabelNamesPerSeries)
 	}
 
 	for _, l := range ls {
+		var errTemplate string
+		var reason string
+		var cause interface{}
 		if !model.LabelName(l.Name).IsValid() {
-			return httpgrpc.Errorf(http.StatusBadRequest, errInvalidLabel, l.Name, metricName)
+			reason = invalidLabel
+			errTemplate = errInvalidLabel
+			cause = l.Name
+		} else if len(l.Name) > cfg.MaxLabelNameLength {
+			reason = labelNameTooLong
+			errTemplate = errLabelNameTooLong
+			cause = l.Name
+		} else if len(l.Value) > cfg.MaxLabelValueLength {
+			reason = labelValueTooLong
+			errTemplate = errLabelValueTooLong
+			cause = l.Value
 		}
-		if len(l.Name) > cfg.MaxLabelNameLength {
-			return httpgrpc.Errorf(http.StatusBadRequest, errLabelNameTooLong, l.Name, metricName)
-		}
-		if len(l.Value) > cfg.MaxLabelValueLength {
-			return httpgrpc.Errorf(http.StatusBadRequest, errLabelValueTooLong, l.Value, metricName)
+		if errTemplate != "" {
+			DiscardedSamples.WithLabelValues(reason, userID).Inc()
+			return httpgrpc.Errorf(http.StatusBadRequest, errTemplate, cause, metricName)
 		}
 	}
 	return nil

pkg/util/validation/validate_test.go
@@ -13,6 +13,7 @@ import (
 
 func TestValidateLabels(t *testing.T) {
 	var cfg Config
+	userID := "testUser"
 	util.DefaultValues(&cfg)
 	cfg.MaxLabelValueLength = 25
 	cfg.MaxLabelNameLength = 25
@@ -48,7 +49,7 @@ func TestValidateLabels(t *testing.T) {
 		},
 	} {
 
-		err := cfg.ValidateLabels(client.ToLabelPairs(c.metric))
+		err := cfg.ValidateLabels(userID, client.ToLabelPairs(c.metric))
 		assert.Equal(t, c.err, err, "wrong error")
 	}
 }

commit f1ef91a1c9ee734ab6661411359d57da04fef3ac

Fix ingester error propagation bug introduced by #890
Signed-off-by: Tom Wilkie <tom.wilkie@gmail.com>

pkg/ingester/ingester.go
@@ -23,15 +23,10 @@ import (
 	"github.com/weaveworks/cortex/pkg/ingester/client"
 	"github.com/weaveworks/cortex/pkg/ring"
 	"github.com/weaveworks/cortex/pkg/util"
-	"github.com/weaveworks/cortex/pkg/util/validation"
 )
 
 const (
 	ingesterSubsystem = "ingester"
-
-	// Reasons to discard samples.
-	outOfOrderTimestamp = "timestamp_out_of_order"
-	duplicateSample     = "multiple_values_for_timestamp"
 
 	// DefaultConcurrentFlush is the number of series to flush concurrently
 	DefaultConcurrentFlush = 50
@@ -332,15 +327,10 @@ func (i *Ingester) append(ctx context.Context, sample *model.Sample) error {
 	}()
 
 	prevNumChunks := len(series.chunkDescs)
-	if err := series.add(model.SamplePair{
+	if err := series.add(ctx, model.SamplePair{
 		Value:     sample.Value,
 		Timestamp: sample.Timestamp,
 	}); err != nil {
-		if mse, ok := err.(*memorySeriesError); ok {
-			validation.DiscardedSamples.WithLabelValues(mse.errorType, state.userID).Inc()
-			// Use a dumb string template to avoid the message being parsed as a template
-			err = httpgrpc.Errorf(http.StatusBadRequest, "%s", mse.message)
-		}
 		return err
 	}
 

pkg/ingester/series.go
@@ -1,14 +1,28 @@
 package ingester
 
 import (
+	"context"
 	"fmt"
+	"net/http"
 	"sort"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/model"
 
+	"github.com/weaveworks/common/httpgrpc"
+	"github.com/weaveworks/common/user"
 	"github.com/weaveworks/cortex/pkg/prom1/storage/local/chunk"
 	"github.com/weaveworks/cortex/pkg/prom1/storage/metric"
+	"github.com/weaveworks/cortex/pkg/util/validation"
+)
+
+const (
+	// Reasons to discard samples.
+	outOfOrderTimestamp = "timestamp_out_of_order"
+	duplicateSample     = "multiple_values_for_timestamp"
+
+	duplicateSampleMessage = "sample with repeated timestamp but different value for series %v; last value: %v, incoming value: %v"
+	outOfOrderMessage      = "sample timestamp out of order for series %v; last timestamp: %v, incoming timestamp: %v"
 )
 
 var (
@@ -39,15 +53,6 @@ type memorySeries struct {
 	lastSampleValue    model.SampleValue
 }
 
-type memorySeriesError struct {
-	message   string
-	errorType string
-}
-
-func (error *memorySeriesError) Error() string {
-	return error.message
-}
-
 // newMemorySeries returns a pointer to a newly allocated memorySeries for the
 // given metric.
 func newMemorySeries(m model.Metric) *memorySeries {
@@ -61,7 +66,12 @@ func newMemorySeries(m model.Metric) *memorySeries {
 // completed chunks (which are now eligible for persistence).
 //
 // The caller must have locked the fingerprint of the series.
-func (s *memorySeries) add(v model.SamplePair) error {
+func (s *memorySeries) add(ctx context.Context, v model.SamplePair) error {
+	userID, err := user.ExtractOrgID(ctx)
+	if err != nil {
+		return err
+	}
+
 	// Don't report "no-op appends", i.e. where timestamp and sample
 	// value are the same as for the last append, as they are a
 	// common occurrence when using client-side timestamps
@@ -72,16 +82,12 @@ func (s *memorySeries) add(v model.SamplePair) error {
 		return nil
 	}
 	if v.Timestamp == s.lastTime {
-		return &memorySeriesError{
-			message:   fmt.Sprintf("sample with repeated timestamp but different value for series %v; last value: %v, incoming value: %v", s.metric, s.lastSampleValue, v.Value),
-			errorType: "new-value-for-timestamp",
-		}
+		validation.DiscardedSamples.WithLabelValues(duplicateSample, userID).Inc()
+		return httpgrpc.Errorf(http.StatusBadRequest, duplicateSampleMessage, s.metric, s.lastSampleValue, v.Value)
 	}
 	if v.Timestamp < s.lastTime {
-		return &memorySeriesError{
-			message:   fmt.Sprintf("sample timestamp out of order for series %v; last timestamp: %v, incoming timestamp: %v", s.metric, s.lastTime, v.Timestamp),
-			errorType: "sample-out-of-order",
-		}
+		validation.DiscardedSamples.WithLabelValues(outOfOrderTimestamp, userID).Inc()
+		return httpgrpc.Errorf(http.StatusBadRequest, outOfOrderMessage, s.metric, s.lastTime, v.Timestamp)
 	}
 
 	if len(s.chunkDescs) == 0 || s.headChunkClosed {
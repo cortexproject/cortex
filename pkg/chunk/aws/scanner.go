1fd3b65b67231a8c60298a66732c092172dc1496
Bryan Boreham <bjboreham@gmail.com>
2018-09-22 13:49:56 +0000

    Refactor: remove separate segmentScan function

--- pkg/chunk/aws/scanner.go
+++ pkg/chunk/aws/scanner.go
@@ -10,13 +10,26 @@ import (
 	"github.com/cortexproject/cortex/pkg/chunk"
 )
 
+// ScanTable reads the whole of a table on multiple goroutines in
+// parallel, calling back with batches of results on one of the
+// callbacks for each goroutine.
 func (a storageClient) ScanTable(ctx context.Context, tableName string, callbacks []func(result chunk.ReadBatch)) error {
 	var outerErr error
 	var readerGroup sync.WaitGroup
 	readerGroup.Add(len(callbacks))
 	for segment, callback := range callbacks {
 		go func(segment int, callback func(result chunk.ReadBatch)) {
-			err := a.segmentScan(segment, len(callbacks), tableName, callback)
+			input := &dynamodb.ScanInput{
+				TableName:            aws.String(tableName),
+				ProjectionExpression: aws.String(hashKey + "," + rangeKey),
+				Segment:              aws.Int64(int64(segment)),
+				TotalSegments:        aws.Int64(int64(len(callbacks))),
+				//ReturnConsumedCapacity: aws.String(dynamodb.ReturnConsumedCapacityTotal),
+			}
+			err := a.DynamoDB.ScanPages(input, func(page *dynamodb.ScanOutput, lastPage bool) bool {
+				callback(&dynamoDBReadResponse{items: page.Items})
+				return true
+			})
 			if err != nil {
 				outerErr = err
 				// TODO: abort all segments
@@ -27,23 +40,4 @@ func (a storageClient) ScanTable(ctx context.Context, tableName string, callback
 	// Wait until all reader segments have finished
 	readerGroup.Wait()
 	return outerErr
-}
-
-func (a storageClient) segmentScan(segment, totalSegments int, tableName string, callback func(result chunk.ReadBatch)) error {
-	input := &dynamodb.ScanInput{
-		TableName:            aws.String(tableName),
-		ProjectionExpression: aws.String(hashKey + "," + rangeKey),
-		Segment:              aws.Int64(int64(segment)),
-		TotalSegments:        aws.Int64(int64(totalSegments)),
-		//ReturnConsumedCapacity: aws.String(dynamodb.ReturnConsumedCapacityTotal),
-	}
-
-	err := a.DynamoDB.ScanPages(input, func(page *dynamodb.ScanOutput, lastPage bool) bool {
-		callback(&dynamoDBReadResponse{items: page.Items})
-		return true
-	})
-	if err != nil {
-		return err
-	}
-	return nil
 }

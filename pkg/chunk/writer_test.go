147ee1e532bbf026e033858a273bc592a306b833
Bryan Boreham <bjboreham@gmail.com>
2018-09-24 10:57:21 +0000

    Extend test to read back

--- pkg/chunk/writer_test.go
+++ pkg/chunk/writer_test.go
@@ -1,17 +1,24 @@
 package chunk
 
 import (
+	"context"
+	"reflect"
 	"testing"
+	"time"
 
 	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/promql"
 	"github.com/stretchr/testify/require"
+	"github.com/weaveworks/common/test"
+	"github.com/weaveworks/common/user"
 
 	"github.com/cortexproject/cortex/pkg/util"
 )
 
 func TestWriter(t *testing.T) {
+	ctx := user.InjectOrgID(context.Background(), userID)
 	now := model.Now()
-	fooChunk1, _, _, _ := dummyChunks(now)
+	chunk1, chunk2, _, _ := dummyChunks(now)
 
 	st := newTestChunkStore(t, "v6")
 	defer st.Stop()
@@ -22,9 +29,29 @@ func TestWriter(t *testing.T) {
 	writer := NewWriter(cfg, store.storage)
 	writer.Run()
 
-	writeReqs, err := store.calculateIndexEntries(userID, fooChunk1.From, fooChunk1.Through, fooChunk1)
-	require.NoError(t, err)
-	writer.Write <- writeReqs
+	sendChunk := func(chunk Chunk) {
+		// can't use writer to store chunks with mock storage as it isn't flexible enough
+		err := store.storage.PutChunks(ctx, []Chunk{chunk})
+		require.NoError(t, err)
+		writeReqs, err := store.calculateIndexEntries(userID, chunk.From, chunk.Through, chunk)
+		require.NoError(t, err)
+		writer.Write <- writeReqs
+	}
 
+	sendChunk(chunk1)
+	sendChunk(chunk2)
+
+	// Ensure chunks are flushed to store
 	writer.Stop()
+
+	// Now read back the data
+	matchers, err := promql.ParseMetricSelector("foo")
+	require.NoError(t, err)
+	chunks, err := store.Get(ctx, now.Add(-time.Hour), now, matchers...)
+	require.NoError(t, err)
+
+	expect := []Chunk{chunk1, chunk2}
+	if !reflect.DeepEqual(expect, chunks) {
+		t.Fatalf("wrong chunks - %s", test.Diff(expect, chunks))
+	}
 }

13ca3d598894206bfcbc49f65026fb003b3a8ffd
Tom Wilkie <tom.wilkie@gmail.com>
2018-10-09 12:36:32 +0100

    Review feedback.
    Signed-off-by: Tom Wilkie <tom.wilkie@gmail.com>

--- pkg/querier/frontend/results_cache.go
+++ pkg/querier/frontend/results_cache.go
@@ -73,14 +73,14 @@ func (s resultsCache) Do(ctx context.Context, r *queryRangeRequest) (*apiRespons
 
 	maxCacheTime := int64(model.Now().Add(-s.cfg.MaxCacheFreshness))
 	if r.start > maxCacheTime {
-		response, extents, err = s.handleFresh(ctx, r)
+		return s.next.Do(ctx, r)
+	}
+
+	cached, ok := s.get(ctx, key)
+	if ok {
+		response, extents, err = s.handleHit(ctx, r, cached)
 	} else {
-		cached, ok := s.get(ctx, key)
-		if ok {
-			response, extents, err = s.handleHit(ctx, r, cached)
-		} else {
-			response, extents, err = s.handleMiss(ctx, r)
-		}
+		response, extents, err = s.handleMiss(ctx, r)
 	}
 
 	if err == nil && len(extents) > 0 {
@@ -89,18 +89,6 @@ func (s resultsCache) Do(ctx context.Context, r *queryRangeRequest) (*apiRespons
 	}
 
 	return response, err
-}
-
-func (s resultsCache) handleFresh(ctx context.Context, r *queryRangeRequest) (*apiResponse, []extent, error) {
-	span, ctx := opentracing.StartSpanFromContext(ctx, "handle_fresh")
-	defer span.Finish()
-
-	response, err := s.next.Do(ctx, r)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return response, nil, nil
 }
 
 func (s resultsCache) handleMiss(ctx context.Context, r *queryRangeRequest) (*apiResponse, []extent, error) {

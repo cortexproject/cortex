69535c956ca636cdf2d6dffcb9ae89bf1d0750a4
Bryan Boreham <bryan@weave.works>
2019-01-08 12:06:33 +0000

    Add ingester metric for chunk storage per user
    Signed-off-by: Bryan Boreham <bryan@weave.works>

--- pkg/ingester/flush.go
+++ pkg/ingester/flush.go
@@ -40,6 +40,10 @@ var (
 		Help:    "Distribution of stored chunk sizes (when stored).",
 		Buckets: prometheus.ExponentialBuckets(10, 10, 5), // biggest bucket is 5*2^(11-1) = 5120
 	})
+	chunkSizePerUser = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "cortex_ingester_chunk_bytes_total",
+		Help: "Total bytes stored in chunks per user.",
+	}, []string{"user"})
 	chunkAge = promauto.NewHistogram(prometheus.HistogramOpts{
 		Name: "cortex_ingester_chunk_age_seconds",
 		Help: "Distribution of chunk ages (when stored).",
@@ -315,6 +319,7 @@ func (i *Ingester) flushChunks(ctx context.Context, fp model.Fingerprint, metric
 		return err
 	}
 
+	sizePerUser := chunkSizePerUser.WithLabelValues(userID)
 	// Record statistsics only when actual put request did not return error.
 	for i, chunkDesc := range chunkDescs {
 		utilization, length := chunkDesc.C.Utilization(), chunkDesc.C.Len()
@@ -323,6 +328,7 @@ func (i *Ingester) flushChunks(ctx context.Context, fp model.Fingerprint, metric
 		chunkUtilization.Observe(utilization)
 		chunkLength.Observe(float64(length))
 		chunkSize.Observe(float64(size))
+		sizePerUser.Add(float64(size))
 		chunkAge.Observe(model.Now().Sub(chunkDesc.FirstTime).Seconds())
 	}
 

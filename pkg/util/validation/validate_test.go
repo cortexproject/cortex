4412888970ddd3105f01f2c4dfdadf29c871523f
Tom Wilkie <tom.wilkie@gmail.com>
2019-02-09 11:52:48 +0000

    Review feedback
    Signed-off-by: Tom Wilkie <tom.wilkie@gmail.com>

--- pkg/util/validation/validate_test.go
+++ pkg/util/validation/validate_test.go
@@ -19,7 +19,7 @@ func TestValidateLabels(t *testing.T) {
 	flagext.DefaultValues(&cfg)
 	cfg.MaxLabelValueLength = 25
 	cfg.MaxLabelNameLength = 25
-	cfg.MaxLabelNamesPerSeries = 64
+	cfg.MaxLabelNamesPerSeries = 2
 	overrides, err := NewOverrides(cfg)
 	require.NoError(t, err)
 
@@ -37,7 +37,7 @@ func TestValidateLabels(t *testing.T) {
 		},
 		{
 			map[model.LabelName]model.LabelValue{model.MetricNameLabel: "valid", "foo ": "bar"},
-			httpgrpc.Errorf(http.StatusBadRequest, errInvalidLabel, "foo ", "valid"),
+			httpgrpc.Errorf(http.StatusBadRequest, errInvalidLabel, "foo ", `valid{foo ="bar"}`),
 		},
 		{
 			map[model.LabelName]model.LabelValue{model.MetricNameLabel: "valid"},
@@ -45,11 +45,15 @@ func TestValidateLabels(t *testing.T) {
 		},
 		{
 			map[model.LabelName]model.LabelValue{model.MetricNameLabel: "badLabelName", "this_is_a_really_really_long_name_that_should_cause_an_error": "test_value_please_ignore"},
-			httpgrpc.Errorf(http.StatusBadRequest, errLabelNameTooLong, "this_is_a_really_really_long_name_that_should_cause_an_error", "badLabelName"),
+			httpgrpc.Errorf(http.StatusBadRequest, errLabelNameTooLong, "this_is_a_really_really_long_name_that_should_cause_an_error", `badLabelName{this_is_a_really_really_long_name_that_should_cause_an_error="test_value_please_ignore"}`),
 		},
 		{
 			map[model.LabelName]model.LabelValue{model.MetricNameLabel: "badLabelValue", "much_shorter_name": "test_value_please_ignore_no_really_nothing_to_see_here"},
-			httpgrpc.Errorf(http.StatusBadRequest, errLabelValueTooLong, "test_value_please_ignore_no_really_nothing_to_see_here", "badLabelValue"),
+			httpgrpc.Errorf(http.StatusBadRequest, errLabelValueTooLong, "test_value_please_ignore_no_really_nothing_to_see_here", `badLabelValue{much_shorter_name="test_value_please_ignore_no_really_nothing_to_see_here"}`),
+		},
+		{
+			map[model.LabelName]model.LabelValue{model.MetricNameLabel: "foo", "bar": "baz", "blip": "blop"},
+			httpgrpc.Errorf(http.StatusBadRequest, errTooManyLabels, `foo{bar="baz", blip="blop"}`, 3, 2),
 		},
 	} {
 

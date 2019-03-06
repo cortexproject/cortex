64e3a6f4e4d0cf5695a4e5efbdb5cb03a730de46
Goutham Veeramachaneni <gouthamve@gmail.com>
2019-03-06 13:37:53 +0530

    Make sure server has port attached
    Also make sure that we check for search domains
    
    Revert the TCP lookup change and fallback to UDP
    
    Signed-off-by: Goutham Veeramachaneni <gouthamve@gmail.com>

--- pkg/util/dns.go
+++ pkg/util/dns.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"fmt"
+	"net"
 
 	"github.com/go-kit/kit/log/level"
 	"github.com/miekg/dns"
@@ -9,54 +10,58 @@ import (
 
 const defaultResolvConf = "/etc/resolv.conf"
 
-// GetDefaultDNSServers returns the DNS servers from the default
-// resolver configuration.
-func GetDefaultDNSServers() ([]string, error) {
-	conf, err := dns.ClientConfigFromFile(defaultResolvConf)
-	if err != nil {
-		return nil, err
+func getDefaultDNSServers(conf *dns.ClientConfig) []string {
+	servers := make([]string, 0, len(conf.Servers))
+	for _, server := range conf.Servers {
+		servers = append(servers, net.JoinHostPort(server, conf.Port))
 	}
-	return conf.Servers, nil
+
+	return servers
 }
 
 // LookupSRV tries to resolve an SRV query of the given service, proto and domain name.
 // proto can be 'tcp' or udp'.
 // The query will be of the form _service._proto.name.
 func LookupSRV(service, proto, name string) ([]*dns.SRV, error) {
-	dnsServers, err := GetDefaultDNSServers()
+	conf, err := dns.ClientConfigFromFile(defaultResolvConf)
 	if err != nil {
 		return nil, err
 	}
 
-	msg := &dns.Msg{}
-	name = "_" + service + "._" + proto + "." + name
-	msg.SetQuestion(dns.Fqdn(name), dns.TypeSRV)
+	dnsServers := getDefaultDNSServers(conf)
 
-	client := dns.Client{
-		Net: "tcp",
-	}
-	var result []*dns.SRV
+	name = "_" + service + "._" + proto + "." + name
+	names := conf.NameList(name)
+
 	dnsResolved := false
-	for _, serverAddr := range dnsServers {
-		resMsg, _, err := client.Exchange(msg, serverAddr)
-		if err != nil {
-			level.Warn(Logger).Log("msg", "DNS exchange failed", "err", err)
-			continue
-		}
-		dnsResolved = true
-		for _, ans := range resMsg.Answer {
-			if srvRecord, ok := ans.(*dns.SRV); ok {
-				result = append(result, srvRecord)
+	client := dns.Client{}
+
+	for _, name := range names {
+		msg := &dns.Msg{}
+		msg.SetQuestion(dns.Fqdn(name), dns.TypeSRV)
+
+		var result []*dns.SRV
+		for _, serverAddr := range dnsServers {
+			resMsg, _, err := client.Exchange(msg, serverAddr)
+			if err != nil {
+				level.Warn(Logger).Log("msg", "DNS exchange failed", "err", err)
+				continue
 			}
-		}
-		if len(result) > 0 {
-			return result, nil
+			dnsResolved = true
+			for _, ans := range resMsg.Answer {
+				if srvRecord, ok := ans.(*dns.SRV); ok {
+					result = append(result, srvRecord)
+				}
+			}
+			if len(result) > 0 {
+				return result, nil
+			}
 		}
 	}
 
 	if !dnsResolved {
-		return result, fmt.Errorf("Couldn't resolve %s: No server responded", name)
+		return nil, fmt.Errorf("Couldn't resolve %s: No server responded", name)
 	}
 
-	return result, nil
+	return nil, nil
 }

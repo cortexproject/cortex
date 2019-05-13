d7d4ddff84f0a95018f3cb777df00ca4b6a73f5d
Goutham Veeramachaneni <gouthamve@gmail.com>
2019-05-13 21:11:22 +0530

    Filter empty series client side too.
    Signed-off-by: Goutham Veeramachaneni <gouthamve@gmail.com>

--- pkg/querier/ingester_streaming_queryable_test.go
+++ pkg/querier/ingester_streaming_queryable_test.go
@@ -4,25 +4,41 @@ import (
 	"context"
 	"testing"
 
+	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/pkg/labels"
 	"github.com/stretchr/testify/require"
 
+	"github.com/cortexproject/cortex/pkg/chunk"
+	"github.com/cortexproject/cortex/pkg/chunk/encoding"
 	"github.com/cortexproject/cortex/pkg/ingester/client"
+	"github.com/cortexproject/cortex/pkg/util/chunkcompat"
 	"github.com/weaveworks/common/user"
 )
 
 func TestIngesterStreaming(t *testing.T) {
+	// We need to make sure that there is atleast one chunk present,
+	// else no series will be selected.
+	promChunk, err := encoding.NewForEncoding(encoding.Bigchunk)
+	require.NoError(t, err)
+
+	clientChunks, err := chunkcompat.ToChunks([]chunk.Chunk{
+		chunk.NewChunk("", 0, nil, promChunk, model.Earliest, model.Earliest),
+	})
+	require.NoError(t, err)
+
 	d := &mockDistributor{
 		r: []client.TimeSeriesChunk{
 			{
 				Labels: []client.LabelAdapter{
 					{Name: "bar", Value: "baz"},
 				},
+				Chunks: clientChunks,
 			},
 			{
 				Labels: []client.LabelAdapter{
 					{Name: "foo", Value: "bar"},
 				},
+				Chunks: clientChunks,
 			},
 		},
 	}

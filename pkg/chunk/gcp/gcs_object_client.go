559771fd339c87ba3be788568557b1760422d055
Jacob Lisi <jacob.t.lisi@gmail.com>
2019-07-23 16:38:51 -0400

    fix rebase error
    Signed-off-by: Jacob Lisi <jacob.t.lisi@gmail.com>

--- pkg/chunk/gcp/gcs_object_client.go
+++ pkg/chunk/gcp/gcs_object_client.go
@@ -36,11 +36,7 @@ func (cfg *GCSConfig) RegisterFlags(f *flag.FlagSet) {
 
 // NewGCSObjectClient makes a new chunk.ObjectClient that writes chunks to GCS.
 func NewGCSObjectClient(ctx context.Context, cfg GCSConfig, schemaCfg chunk.SchemaConfig) (chunk.ObjectClient, error) {
-<<<<<<< HEAD
-	option, err := gcsInstrumentation(ctx, storage.ScopeReadWrite)
-=======
-	option, err := gcsInstrumentation(ctx, "chunk")
->>>>>>> c48b40de... basic version of gcs backed ruler
+	option, err := gcsInstrumentation(ctx, "chunk", storage.ScopeReadWrite)
 	if err != nil {
 		return nil, err
 	}

dbd336ef129c37ac8f97e504702122a2834bcb8b
Ganesh Vernekar <cs15btech11018@iith.ac.in>
2019-09-11 15:30:12 +0530

    More rigorous test for WAL
    Signed-off-by: Ganesh Vernekar <cs15btech11018@iith.ac.in>

--- pkg/ingester/wal_test.go
+++ pkg/ingester/wal_test.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/cortexproject/cortex/pkg/util"
 	"github.com/go-kit/kit/log"
@@ -21,18 +22,37 @@ func TestWAL(t *testing.T) {
 	cfg := defaultIngesterTestConfig()
 	cfg.WALConfig.enabled = true
 	cfg.WALConfig.dir = dirname
+	cfg.WALConfig.checkpointDuration = 10 * time.Millisecond
+
+	numSeries := 10
+	numSamplesPerSeriesPerPush := 1000
+	numRestarts := 5
 
 	// Build an ingester, add some samples, then shut it down.
 	_, ing := newTestStore(t, cfg, defaultClientTestConfig(), defaultLimitsTestConfig())
-	userIDs, testData := pushTestSamples(t, ing, 10, 1000, 0)
+	userIDs, testData := pushTestSamples(t, ing, numSeries, numSamplesPerSeriesPerPush, 0)
 	ing.Shutdown()
 
-	cfg.WALConfig.enabled = false
-	// Start a new ingester and recover the WAL.
+	for r := 0; r < numRestarts; r++ {
+		// Start a new ingester and recover the WAL.
+		_, ing = newTestStore(t, cfg, defaultClientTestConfig(), defaultLimitsTestConfig())
+		require.NoError(t, recoverFromWAL(ing))
+
+		for i, userID := range userIDs {
+			testData[userID] = buildTestMatrix(numSeries, (r+1)*numSamplesPerSeriesPerPush, i)
+		}
+		// Check the samples are still there!
+		retrieveTestSamples(t, ing, userIDs, testData)
+		userIDs, testData = pushTestSamples(t, ing, numSeries, numSamplesPerSeriesPerPush, (r+1)*numSamplesPerSeriesPerPush)
+		ing.Shutdown()
+	}
+
 	_, ing = newTestStore(t, cfg, defaultClientTestConfig(), defaultLimitsTestConfig())
 	defer ing.Shutdown()
 	require.NoError(t, recoverFromWAL(ing))
 
-	// Check the samples are still there!
+	for i, userID := range userIDs {
+		testData[userID] = buildTestMatrix(numSeries, (numRestarts+1)*numSamplesPerSeriesPerPush, i)
+	}
 	retrieveTestSamples(t, ing, userIDs, testData)
 }

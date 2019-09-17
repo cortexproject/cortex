587365defa484067e65de3e64057045e6a06e2c4
Joe Elliott <number101010@gmail.com>
2019-09-17 10:00:50 -0400

    Added key order flag
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- cmd/memcached-dump/main.go
+++ cmd/memcached-dump/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"time"
 
@@ -14,22 +15,37 @@ import (
 	"github.com/bradfitz/gomemcache/memcache"
 )
 
+const (
+	modeDump      = "dump"
+	modeGapSearch = "gaps"
+)
+
+const (
+	keyOrderNormal  = "forward"
+	keyOrderReverse = "reverse"
+	keyOrderRandom  = "random"
+)
+
 var (
-	address string
-	keyfile string
-	rate    time.Duration
+	address  string
+	keyfile  string
+	rate     time.Duration
+	mode     string
+	keyOrder string
 )
 
 func init() {
 	flag.StringVar(&address, "address", "localhost:11211", "Hostname to connect to and dump values.")
 	flag.StringVar(&keyfile, "keyfile", "keys.txt", "File to parse for keys.  Expected to be newline delimited.")
 	flag.DurationVar(&rate, "rate", 500*time.Millisecond, "Query rate.")
+	flag.StringVar(&mode, "mode", "dump", "Specify mode for memcached tool [dump, gaps].")
+	flag.StringVar(&keyOrder, "key-order", "random", "Specify order to consider keys [forward, reverse, random].")
 }
 
 func main() {
 	flag.Parse()
 
-	keys, err := readLines(keyfile)
+	keys, err := readKeys(keyfile, keyOrder)
 	if err != nil {
 		log.Fatalf("Failed to readfile: %v", err)
 	}
@@ -55,13 +71,15 @@ func main() {
 			log.Fatalf("Failed to marshal to json: %v", err)
 		}
 
-		fmt.Println(string(bytes))
+		if mode == modeDump {
+			fmt.Println(string(bytes))
+		}
 
 		<-throttle
 	}
 }
 
-func readLines(path string) ([]string, error) {
+func readKeys(path string, order string) ([]string, error) {
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
@@ -73,5 +91,19 @@ func readLines(path string) ([]string, error) {
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
 	}
-	return lines, scanner.Err()
+
+	if err != nil {
+		return nil, err
+	}
+
+	if order == keyOrderRandom {
+		rand.Seed(time.Now().UnixNano())
+		rand.Shuffle(len(lines), func(i, j int) { lines[i], lines[j] = lines[j], lines[i] })
+	} else if order == keyOrderReverse {
+		for left, right := 0, len(lines)-1; left < right; left, right = left+1, right-1 {
+			lines[left], lines[right] = lines[right], lines[left]
+		}
+	}
+
+	return lines, nil
 }

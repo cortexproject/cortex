224b1b43fdfb1646cbeb441f8db6e25afa5b8d6c
Joe Elliott <number101010@gmail.com>
2019-10-10 17:23:28 -0400

    Rewrote mapper function to clean up bugs with multiple files.  Added tests
    Signed-off-by: Joe Elliott <number101010@gmail.com>
    Signed-off-by: Jacob Lisi <jacob.t.lisi@gmail.com>

--- pkg/ruler/mapper.go
+++ pkg/ruler/mapper.go
@@ -27,7 +27,8 @@ func newMapper(path string) *mapper {
 }
 
 func (m *mapper) MapRules(user string, ruleConfigs map[string][]rulefmt.RuleGroup) (bool, []string, error) {
-	var update bool
+	anyUpdated := false
+	filenames := []string{}
 
 	// user rule files will be stored as `/<path>/<userid>/filename`
 	path := m.Path + "/" + user + "/"
@@ -36,44 +37,72 @@ func (m *mapper) MapRules(user string, ruleConfigs map[string][]rulefmt.RuleGrou
 		return false, nil, err
 	}
 
-	filenames := []string{}
+	// write all rule configs to disk
+	for filename, groups := range ruleConfigs {
+		fullFileName := path + filename
 
-	// iterate through each rule group and map to disk if updated
-	for f, groups := range ruleConfigs {
-		// ensure rule groups are sorted before writing the file to disk
-		sort.Slice(groups, func(i, j int) bool {
-			return groups[i].Name > groups[j].Name
-		})
-
-		rgs := rulefmt.RuleGroups{Groups: groups}
-		file := path + f
-		filenames = append(filenames, file)
-
-		d, err := yaml.Marshal(&rgs)
+		fileUpdated, err := m.writeRuleGroupsIfNewer(groups, fullFileName)
 		if err != nil {
 			return false, nil, err
 		}
-
-		// Determine if the file exists and whether it is identical to the current rule file
-		if _, err := m.FS.Stat(file); err == nil {
-			current, err := afero.ReadFile(m.FS, file)
-			if err != nil {
-				level.Warn(util.Logger).Log("msg", "unable to read rule file on disk", "file", file, "err", err)
-				continue
-			}
-			newHash := md5.New()
-			currentHash := md5.New()
-			if string(currentHash.Sum(current)) == string(newHash.Sum(d)) {
-				continue
-			}
-		}
-
-		level.Info(util.Logger).Log("msg", "updating rule file", "file", file)
-		err = afero.WriteFile(m.FS, file, d, 0777)
-		if err != nil {
-			return false, nil, err
-		}
-		update = true
+		filenames = append(filenames, fullFileName)
+		anyUpdated = anyUpdated || fileUpdated
 	}
-	return update, filenames, nil
+
+	// and clean any up that shouldn't exist
+	existingFiles, err := afero.ReadDir(m.FS, path)
+	if err != nil {
+		return false, nil, err
+	}
+
+	for _, existingFile := range existingFiles {
+		fullFileName := path + existingFile.Name()
+		ruleGroups := ruleConfigs[existingFile.Name()]
+
+		if ruleGroups == nil {
+			err = m.FS.Remove(fullFileName)
+			if err != nil {
+				level.Warn(util.Logger).Log("msg", "unable to remove rule file on disk", "file", fullFileName, "err", err)
+			}
+			anyUpdated = true
+		}
+	}
+
+	return anyUpdated, filenames, nil
+}
+
+func (m *mapper) writeRuleGroupsIfNewer(groups []rulefmt.RuleGroup, filename string) (bool, error) {
+	sort.Slice(groups, func(i, j int) bool {
+		return groups[i].Name > groups[j].Name
+	})
+
+	rgs := rulefmt.RuleGroups{Groups: groups}
+
+	d, err := yaml.Marshal(&rgs)
+	if err != nil {
+		return false, err
+	}
+
+	_, err = m.FS.Stat(filename)
+	if err == nil {
+		current, err := afero.ReadFile(m.FS, filename)
+		if err != nil {
+			return false, err
+		}
+		newHash := md5.New()
+		currentHash := md5.New()
+
+		// bailout if there is no update
+		if string(currentHash.Sum(current)) == string(newHash.Sum(d)) {
+			return false, nil
+		}
+	}
+
+	level.Info(util.Logger).Log("msg", "updating rule file", "file", filename)
+	err = afero.WriteFile(m.FS, filename, d, 0777)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
 }

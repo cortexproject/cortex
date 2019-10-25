6cc0106c5ef84a9487415dc3d77667984dec50b9
Marco Pracucci <marco@pracucci.com>
2019-10-25 10:26:54 +0200

    Commented ReadRingMock struct and functions
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- pkg/ring/testutils/read_ring_mock.go
+++ pkg/ring/testutils/read_ring_mock.go
@@ -6,30 +6,36 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+// ReadRingMock mocks a ReadRing
 type ReadRingMock struct {
 	mock.Mock
 	prometheus.Collector
 }
 
+// NewReadRingMock creates a new ReadRing mock
 func NewReadRingMock() *ReadRingMock {
 	return &ReadRingMock{}
 }
 
+// Get mocks ReadRing's Get
 func (m *ReadRingMock) Get(key uint32, op ring.Operation, buf []ring.IngesterDesc) (ring.ReplicationSet, error) {
 	args := m.Called(key, op, buf)
 	return args.Get(0).(ring.ReplicationSet), args.Error(1)
 }
 
+// GetAll mocks ReadRing's GetAll
 func (m *ReadRingMock) GetAll() (ring.ReplicationSet, error) {
 	args := m.Called()
 	return args.Get(0).(ring.ReplicationSet), args.Error(1)
 }
 
+// ReplicationFactor mocks ReadRing's ReplicationFactor
 func (m *ReadRingMock) ReplicationFactor() int {
 	args := m.Called()
 	return args.Int(0)
 }
 
+// IngesterCount mocks ReadRing's IngesterCount
 func (m *ReadRingMock) IngesterCount() int {
 	args := m.Called()
 	return args.Int(0)

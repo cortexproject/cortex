ed9949b0da24bcb9b226fa98a193287027b492c7
Jacob Lisi <jacob.t.lisi@gmail.com>
2019-11-01 14:57:07 -0400

    add v1 functionality to ParseFormatted w/ tests
    Signed-off-by: Jacob Lisi <jacob.t.lisi@gmail.com>

--- pkg/configs/configs_test.go
+++ pkg/configs/configs_test.go
@@ -4,17 +4,31 @@ import (
 	"encoding/json"
 	"strconv"
 	"testing"
-	"time"
 
-	"github.com/go-kit/kit/log"
-	"github.com/prometheus/prometheus/pkg/labels"
-	"github.com/prometheus/prometheus/promql"
-	"github.com/prometheus/prometheus/rules"
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/pkg/rulefmt"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
-	"github.com/cortexproject/cortex/pkg/util"
 )
+
+var legacyRulesFile = `ALERT TestAlert
+IF up == 0
+FOR 5m
+LABELS { severity = "critical" }
+ANNOTATIONS {
+	message = "I am a message"
+}`
+
+var ruleFile = `groups:
+- name: example
+  rules:
+  - alert: TestAlert
+    expr: up == 0
+    for: 5m
+    labels:
+      severity: critical
+    annotations:
+      message: I am a message`
 
 func TestUnmarshalLegacyConfigWithMissingRuleFormatVersionSucceeds(t *testing.T) {
 	actual := Config{}
@@ -32,6 +46,7 @@ func TestUnmarshalLegacyConfigWithMissingRuleFormatVersionSucceeds(t *testing.T)
 
 	assert.Equal(t, expected, actual)
 }
+
 
 func TestParseLegacyAlerts(t *testing.T) {
 	parsed, err := promql.ParseExpr("up == 0")
@@ -99,6 +114,74 @@ groups:
 	} {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
 			rules, err := tc.cfg.Parse()
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, rules)
+		})
+	}
+}
+
+
+func TestParseFormatted(t *testing.T) {
+	dur, err := model.ParseDuration("5m")
+	require.NoError(t, err)
+
+	rules := []rulefmt.Rule{
+		rulefmt.Rule{
+			Alert: "TestAlert",
+			Expr:  "up == 0",
+			For:   dur,
+			Labels: map[string]string{
+				"severity": "critical",
+			},
+			Annotations: map[string]string{
+				"message": "I am a message",
+			},
+		},
+	}
+
+	for i, tc := range []struct {
+		cfg      RulesConfig
+		expected map[string]rulefmt.RuleGroups
+	}{
+		{
+			cfg: RulesConfig{
+				FormatVersion: RuleFormatV1,
+				Files: map[string]string{
+					"legacy.rules": legacyRulesFile,
+				},
+			},
+			expected: map[string]rulefmt.RuleGroups{
+				"legacy.rules": rulefmt.RuleGroups{
+					Groups: []rulefmt.RuleGroup{
+						rulefmt.RuleGroup{
+							Name:  "rg:legacy.rules",
+							Rules: rules,
+						},
+					},
+				},
+			},
+		},
+		{
+			cfg: RulesConfig{
+				FormatVersion: RuleFormatV2,
+				Files: map[string]string{
+					"alerts.yaml": ruleFile,
+				},
+			},
+			expected: map[string]rulefmt.RuleGroups{
+				"alerts.yaml": rulefmt.RuleGroups{
+					Groups: []rulefmt.RuleGroup{
+						rulefmt.RuleGroup{
+							Name:  "example",
+							Rules: rules,
+						},
+					},
+				},
+			},
+		},
+	} {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			rules, err := tc.cfg.ParseFormatted()
 			require.NoError(t, err)
 			require.Equal(t, tc.expected, rules)
 		})

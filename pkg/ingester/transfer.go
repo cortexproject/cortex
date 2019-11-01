e4d74f8ac40f83d6eba215c08f5893d3070a664b
Marco Pracucci <marco@pracucci.com>
2019-11-01 12:54:31 +0100

    Changed ring key reference in the ingester, to keep code consistency
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- pkg/ingester/transfer.go
+++ pkg/ingester/transfer.go
@@ -159,7 +159,7 @@ func (i *Ingester) TransferChunks(stream client.Ingester_TransferChunksServer) e
 // transfer, as claiming tokens would possibly end up with this ingester owning no tokens, due to conflict
 // resolution in ring merge function. Hopefully the leaving ingester will retry transfer again.
 func (i *Ingester) checkFromIngesterIsInLeavingState(ctx context.Context, fromIngesterID string) error {
-	v, err := i.lifecycler.KVStore.Get(ctx, ring.IngesterRingKey)
+	v, err := i.lifecycler.KVStore.Get(ctx, i.lifecycler.RingKey)
 	if err != nil {
 		return errors.Wrap(err, "get ring")
 	}

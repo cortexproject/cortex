25564185f94ba15ebc429b15d9e0d576cbfaf73e
Peter Štibraný <peter.stibrany@grafana.com>
2019-12-11 15:11:30 +0100

    Explicit healty states (#1890)
    * Be explicit about which states are considered healthy.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * When reporting, all states are OK and we only check timestamp.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Update IsHealthy test to cover reporting.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Use switch, it's bit nicer.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>
    
    * Added parenthesis around state conditions.
    
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>

--- pkg/ring/model.go
+++ pkg/ring/model.go
@@ -182,12 +182,20 @@ func (d *Desc) TokensFor(id string) (tokens, other Tokens) {
 
 // IsHealthy checks whether the ingester appears to be alive and heartbeating
 func (i *IngesterDesc) IsHealthy(op Operation, heartbeatTimeout time.Duration) bool {
-	if op == Write && i.State != ACTIVE {
-		return false
-	} else if op == Read && i.State == JOINING {
-		return false
+	healthy := false
+
+	switch op {
+	case Write:
+		healthy = (i.State == ACTIVE)
+
+	case Read:
+		healthy = (i.State == ACTIVE) || (i.State == LEAVING) || (i.State == PENDING)
+
+	case Reporting:
+		healthy = true
 	}
-	return time.Now().Sub(time.Unix(i.Timestamp, 0)) <= heartbeatTimeout
+
+	return healthy && time.Now().Sub(time.Unix(i.Timestamp, 0)) <= heartbeatTimeout
 }
 
 // Merge merges other ring into this one. Returns sub-ring that represents the change,

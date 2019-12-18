dc273f8ba41921de16af2b7f8d072d8bfbcd4ad4
Ganesh Vernekar <15064823+codesome@users.noreply.github.com>
2019-12-18 22:19:21 +0530

    Replace interface names with local ip in distributor tests. (#1915)
    Hardcoding interface names cause the tests to fail on systems that have interface names different than the ones hardcoded.
    
    Signed-off-by: Ganesh Vernekar <cs15btech11018@iith.ac.in>

--- pkg/distributor/distributor_test.go
+++ pkg/distributor/distributor_test.go
@@ -754,7 +754,7 @@ func prepare(t *testing.T, numIngesters, happyIngesters int, queryDelay time.Dur
 	cfg.DistributorRing.HeartbeatPeriod = 100 * time.Millisecond
 	cfg.DistributorRing.InstanceID = strconv.Itoa(rand.Int())
 	cfg.DistributorRing.KVStore.Mock = kvStore
-	cfg.DistributorRing.InstanceInterfaceNames = []string{"eth0", "en0", "lo0"}
+	cfg.DistributorRing.InstanceAddr = "127.0.0.1"
 
 	overrides, err := validation.NewOverrides(*limits)
 	require.NoError(t, err)

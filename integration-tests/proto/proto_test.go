3a9d0706f894b03c77136f94aaa26fee002ed7c9
Cyril Tovena <cyril.tovena@gmail.com>
2020-01-31 15:42:10 -0500

    Fixes compatibility issue with labelpair custom type
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>

--- integration-tests/proto/proto_test.go
+++ integration-tests/proto/proto_test.go
@@ -133,19 +133,59 @@ func Test_ProtoCompat(t *testing.T) {
 	}, q)
 }
 
-func Test_LabelAdapter(t *testing.T) {
-	l3 := proto13.LabelAdapter{Name: "foo", Value: "bar"}
-	b3, err := l3.Marshal()
+func Test_CustomMarshal(t *testing.T) {
+	l13 := proto13.LabelAdapter{Name: "foo", Value: "bar"}
+	b3, err := l13.Marshal()
 	if err != nil {
 		t.Fatal(err)
 	}
-	t.Log(b3)
-	l2 := proto13.LabelAdapter{}
-	err = l2.Unmarshal(b3)
+	l12 := proto12.LabelAdapter{}
+	err = l12.Unmarshal(b3)
 	if err != nil {
 		t.Fatal(err)
 	}
-	require.EqualValues(t, l3, l2)
+	require.EqualValues(t, l13, l12)
+
+	b2, err := l12.Marshal()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = l13.Unmarshal(b2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	require.EqualValues(t, l13, l12)
+
+	t13 := proto13.TimeSeries{
+		Labels: []proto13.LabelAdapter{
+			proto13.LabelAdapter{Name: "1", Value: "2"},
+		},
+		Samples: []proto13.Sample{
+			proto13.Sample{Value: 10, TimestampMs: 1},
+			proto13.Sample{Value: 10000, TimestampMs: 2},
+		},
+	}
+	bt13, err := t13.Marshal()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t12 := proto12.TimeSeries{}
+	err = t12.Unmarshal(bt13)
+	if err != nil {
+		t.Fatal(err)
+	}
+	require.EqualValues(t, t13, t12)
+
+	bt12, err := t12.Marshal()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t13 = proto13.TimeSeries{}
+	err = t13.Unmarshal(bt12)
+	if err != nil {
+		t.Fatal(err)
+	}
+	require.EqualValues(t, t13, t12)
 }
 
 type fakeIngesterServer struct {

129cfbb885e36cfc7837cc383e64ab1d4c770999
Cyril Tovena <cyril.tovena@gmail.com>
2020-01-31 16:27:01 -0500

    Full ingester tests.
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>

--- integration-tests/proto/proto_test.go
+++ integration-tests/proto/proto_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net"
+	"sync"
 	"testing"
 
 	proto12 "github.com/cortexproject/cortex/integration-tests/proto/1.2.1"
@@ -20,6 +21,7 @@ var (
 	server      *grpc.Server
 	labelNames  = []string{"foo", "fuzz", "bar", "buzz"}
 	labelValues = []string{"lfoo", "lfuzz", "lbar", "lbuzz"}
+	wg          = sync.WaitGroup{}
 )
 
 func init() {
@@ -131,61 +133,123 @@ func Test_ProtoCompat(t *testing.T) {
 			},
 		},
 	}, q)
-}
 
-func Test_CustomMarshal(t *testing.T) {
-	l13 := proto13.LabelAdapter{Name: "foo", Value: "bar"}
-	b3, err := l13.Marshal()
-	if err != nil {
-		t.Fatal(err)
-	}
-	l12 := proto12.LabelAdapter{}
-	err = l12.Unmarshal(b3)
-	if err != nil {
-		t.Fatal(err)
-	}
-	require.EqualValues(t, l13, l12)
-
-	b2, err := l12.Marshal()
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = l13.Unmarshal(b2)
-	if err != nil {
-		t.Fatal(err)
-	}
-	require.EqualValues(t, l13, l12)
-
-	t13 := proto13.TimeSeries{
-		Labels: []proto13.LabelAdapter{
-			proto13.LabelAdapter{Name: "1", Value: "2"},
+	stream, err := client.QueryStream(ctx, &proto12.QueryRequest{
+		StartTimestampMs: 1111,
+		EndTimestampMs:   20000,
+		Matchers: []*proto12.LabelMatcher{
+			&proto12.LabelMatcher{Type: proto12.REGEX_MATCH, Name: "foo", Value: "bar"},
+			&proto12.LabelMatcher{Type: proto12.NOT_EQUAL, Name: "fuzz", Value: "buzz"},
 		},
-		Samples: []proto13.Sample{
-			proto13.Sample{Value: 10, TimestampMs: 1},
-			proto13.Sample{Value: 10000, TimestampMs: 2},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	streamResponse, err := stream.Recv()
+	if err != nil {
+		t.Fatal(err)
+	}
+	require.Equal(t, &proto12.QueryStreamResponse{
+		Timeseries: []proto12.TimeSeriesChunk{
+			proto12.TimeSeriesChunk{
+				FromIngesterId: "1",
+				UserId:         "2",
+				Labels: []proto12.LabelAdapter{
+					proto12.LabelAdapter{Name: "foo", Value: "barr"},
+				},
+				Chunks: []proto12.Chunk{
+					proto12.Chunk{
+						StartTimestampMs: 1,
+						EndTimestampMs:   1000,
+						Encoding:         5,
+						Data:             []byte{1, 1, 3, 5, 12},
+					},
+				},
+			},
 		},
-	}
-	bt13, err := t13.Marshal()
+	}, streamResponse)
+	stats, err := client.AllUserStats(ctx, &proto12.UserStatsRequest{})
 	if err != nil {
 		t.Fatal(err)
 	}
-	t12 := proto12.TimeSeries{}
-	err = t12.Unmarshal(bt13)
-	if err != nil {
-		t.Fatal(err)
-	}
-	require.EqualValues(t, t13, t12)
+	require.Equal(t, &proto12.UsersStatsResponse{
+		Stats: []*proto12.UserIDStatsResponse{
+			&proto12.UserIDStatsResponse{
+				UserId: "fake",
+				Data: &proto12.UserStatsResponse{
+					IngestionRate:     1,
+					NumSeries:         2,
+					ApiIngestionRate:  2,
+					RuleIngestionRate: 1.223,
+				},
+			},
+		},
+	}, stats)
 
-	bt12, err := t12.Marshal()
+	m, err := client.MetricsForLabelMatchers(ctx, &proto12.MetricsForLabelMatchersRequest{
+		StartTimestampMs: 1,
+		EndTimestampMs:   2,
+		MatchersSet: []*proto12.LabelMatchers{
+			&proto12.LabelMatchers{
+				Matchers: []*proto12.LabelMatcher{
+					&proto12.LabelMatcher{
+						Type:  proto12.REGEX_MATCH,
+						Name:  "foo",
+						Value: "buzz",
+					},
+				},
+			},
+		},
+	})
 	if err != nil {
 		t.Fatal(err)
 	}
-	t13 = proto13.TimeSeries{}
-	err = t13.Unmarshal(bt12)
+	require.Equal(t, &proto12.MetricsForLabelMatchersResponse{
+		Metric: []*proto12.Metric{
+			&proto12.Metric{Labels: []proto12.LabelAdapter{
+				proto12.LabelAdapter{Name: "foo", Value: "bar"},
+			}},
+		},
+	}, m)
+
+	chunkStream, err := client.TransferChunks(ctx)
 	if err != nil {
 		t.Fatal(err)
 	}
-	require.EqualValues(t, t13, t12)
+	wg.Add(1)
+	err = chunkStream.Send(&proto12.TimeSeriesChunk{
+		FromIngesterId: "foo",
+		UserId:         "fake",
+		Labels: []proto12.LabelAdapter{
+			proto12.LabelAdapter{Name: "bar", Value: "buzz"},
+		},
+		Chunks: []proto12.Chunk{
+			proto12.Chunk{
+				StartTimestampMs: 1,
+				EndTimestampMs:   2,
+				Encoding:         3,
+				Data:             []byte{1, 23, 24, 43, 4, 123},
+			},
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	wg.Wait()
+	err = stream.CloseSend()
+	require.NoError(t, err)
+	tsStream, err := client.TransferTSDB(ctx)
+	require.NoError(t, err)
+	wg.Add(1)
+	err = tsStream.Send(&proto12.TimeSeriesFile{
+		FromIngesterId: "string",
+		UserId:         "fake",
+		Filename:       "here",
+		Data:           []byte{1, 2, 3, 5, 6, 7},
+	})
+	require.NoError(t, err)
+	wg.Wait()
+	require.NoError(t, tsStream.CloseSend())
 }
 
 type fakeIngesterServer struct {
@@ -267,20 +331,107 @@ func (f *fakeIngesterServer) Query(_ context.Context, req *proto13.QueryRequest)
 		},
 	}, nil
 }
-func (fakeIngesterServer) QueryStream(*proto13.QueryRequest, proto13.Ingester_QueryStreamServer) error {
+func (f *fakeIngesterServer) QueryStream(req *proto13.QueryRequest, stream proto13.Ingester_QueryStreamServer) error {
+	require.Equal(f.t, &proto13.QueryRequest{
+		StartTimestampMs: 1111,
+		EndTimestampMs:   20000,
+		Matchers: []*proto13.LabelMatcher{
+			&proto13.LabelMatcher{Type: proto13.REGEX_MATCH, Name: "foo", Value: "bar"},
+			&proto13.LabelMatcher{Type: proto13.NOT_EQUAL, Name: "fuzz", Value: "buzz"},
+		},
+	}, req)
+	stream.Send(&proto13.QueryStreamResponse{
+		Timeseries: []proto13.TimeSeriesChunk{
+			proto13.TimeSeriesChunk{
+				FromIngesterId: "1",
+				UserId:         "2",
+				Labels: []proto13.LabelAdapter{
+					proto13.LabelAdapter{Name: "foo", Value: "barr"},
+				},
+				Chunks: []proto13.Chunk{
+					proto13.Chunk{
+						StartTimestampMs: 1,
+						EndTimestampMs:   1000,
+						Encoding:         5,
+						Data:             []byte{1, 1, 3, 5, 12},
+					},
+				},
+			},
+		},
+	})
 	return nil
 }
 func (fakeIngesterServer) AllUserStats(context.Context, *proto13.UserStatsRequest) (*proto13.UsersStatsResponse, error) {
-	return nil, nil
+	return &proto13.UsersStatsResponse{
+		Stats: []*proto13.UserIDStatsResponse{
+			&proto13.UserIDStatsResponse{
+				UserId: "fake",
+				Data: &proto13.UserStatsResponse{
+					IngestionRate:     1,
+					NumSeries:         2,
+					ApiIngestionRate:  2,
+					RuleIngestionRate: 1.223,
+				},
+			},
+		},
+	}, nil
 }
-func (fakeIngesterServer) MetricsForLabelMatchers(context.Context, *proto13.MetricsForLabelMatchersRequest) (*proto13.MetricsForLabelMatchersResponse, error) {
-	return nil, nil
+func (f *fakeIngesterServer) MetricsForLabelMatchers(_ context.Context, req *proto13.MetricsForLabelMatchersRequest) (*proto13.MetricsForLabelMatchersResponse, error) {
+	require.Equal(f.t, &proto13.MetricsForLabelMatchersRequest{
+		StartTimestampMs: 1,
+		EndTimestampMs:   2,
+		MatchersSet: []*proto13.LabelMatchers{
+			&proto13.LabelMatchers{
+				Matchers: []*proto13.LabelMatcher{
+					&proto13.LabelMatcher{
+						Type:  proto13.REGEX_MATCH,
+						Name:  "foo",
+						Value: "buzz",
+					},
+				},
+			},
+		},
+	}, req)
+	return &proto13.MetricsForLabelMatchersResponse{
+		Metric: []*proto13.Metric{
+			&proto13.Metric{Labels: []proto13.LabelAdapter{
+				proto13.LabelAdapter{Name: "foo", Value: "bar"},
+			}},
+		},
+	}, nil
 }
-func (fakeIngesterServer) TransferChunks(proto13.Ingester_TransferChunksServer) error {
+func (f *fakeIngesterServer) TransferChunks(s proto13.Ingester_TransferChunksServer) error {
+	c, err := s.Recv()
+	require.NoError(f.t, err)
+	require.Equal(f.t, &proto13.TimeSeriesChunk{
+		FromIngesterId: "foo",
+		UserId:         "fake",
+		Labels: []proto13.LabelAdapter{
+			proto13.LabelAdapter{Name: "bar", Value: "buzz"},
+		},
+		Chunks: []proto13.Chunk{
+			proto13.Chunk{
+				StartTimestampMs: 1,
+				EndTimestampMs:   2,
+				Encoding:         3,
+				Data:             []byte{1, 23, 24, 43, 4, 123},
+			},
+		},
+	}, c)
+	wg.Done()
 	return nil
 }
 
 // TransferTSDB transfers all files of a tsdb to a joining ingester
-func (fakeIngesterServer) TransferTSDB(proto13.Ingester_TransferTSDBServer) error {
+func (f *fakeIngesterServer) TransferTSDB(stream proto13.Ingester_TransferTSDBServer) error {
+	file, err := stream.Recv()
+	require.NoError(f.t, err)
+	require.Equal(f.t, &proto13.TimeSeriesFile{
+		FromIngesterId: "string",
+		UserId:         "fake",
+		Filename:       "here",
+		Data:           []byte{1, 2, 3, 5, 6, 7},
+	}, file)
+	wg.Done()
 	return nil
 }

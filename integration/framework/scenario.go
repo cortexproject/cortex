f81d2825a8c723902c1bdadef639cfa03afd2b54
Marco Pracucci <marco@pracucci.com>
2020-02-07 17:24:13 +0100

    Cleanup the Docker containers and network before starting a new test
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- integration/framework/scenario.go
+++ integration/framework/scenario.go
@@ -3,7 +3,7 @@ package framework
 import (
 	"fmt"
 	"os"
-	"strconv"
+	"strings"
 
 	"github.com/pkg/errors"
 )
@@ -11,32 +11,29 @@ import (
 const (
 	MinioAccessKey = "cortex"
 	MinioSecretKey = "supersecret"
-)
-
-var (
-	nextScenarioID = 1
+	NetworkName    = "cortex-integration-test"
 )
 
 type Scenario struct {
-	id          int
-	networkName string
-	services    []*Service
+	id       int
+	services []*Service
 }
 
 func NewScenario() (*Scenario, error) {
 	s := &Scenario{
-		id:          nextScenarioID,
-		networkName: "cortex-" + strconv.Itoa(nextScenarioID),
-		services:    []*Service{},
+		services: []*Service{},
 	}
+
+	// Force a shutdown in order to cleanup from a spurious situation in case
+	// the previous tests run didn't cleanup correctly
+	s.Shutdown()
 
 	// Setup the docker network
-	if out, err := RunCommandAndGetOutput("docker", "network", "create", s.networkName); err != nil {
+	if out, err := RunCommandAndGetOutput("docker", "network", "create", NetworkName); err != nil {
 		fmt.Println(string(out))
-		return nil, errors.Wrapf(err, "create docker network '%s'", s.networkName)
+		return nil, errors.Wrapf(err, "create docker network '%s'", NetworkName)
 	}
 
-	nextScenarioID++
 	return s, nil
 }
 
@@ -83,7 +80,7 @@ func (s *Scenario) StartConsul() error {
 		// If you change the image tag, remember to update it in the preloading done
 		// by CircleCI too (see .circleci/config.yml).
 		"consul:0.9",
-		s.networkName,
+		NetworkName,
 		[]int{},
 		nil,
 		// Run consul in "dev" mode so that the initial leader election is immediate
@@ -99,7 +96,7 @@ func (s *Scenario) StartMinio() error {
 		// If you change the image tag, remember to update it in the preloading done
 		// by CircleCI too (see .circleci/config.yml).
 		"minio/minio:RELEASE.2019-12-30T05-45-39Z",
-		s.networkName,
+		NetworkName,
 		[]int{9000},
 		map[string]string{
 			"MINIO_ACCESS_KEY": MinioAccessKey,
@@ -119,7 +116,7 @@ func (s *Scenario) StartDynamoDB() error {
 		// If you change the image tag, remember to update it in the preloading done
 		// by CircleCI too (see .circleci/config.yml).
 		"amazon/dynamodb-local:1.11.477",
-		s.networkName,
+		NetworkName,
 		[]int{8000},
 		nil,
 		NewCommand("-jar", "DynamoDBLocal.jar", "-inMemory", "-sharedDb"),
@@ -136,7 +133,7 @@ func (s *Scenario) StartDistributor(name string, flags map[string]string, image 
 	return s.StartService(NewService(
 		name,
 		image,
-		s.networkName,
+		NetworkName,
 		[]int{80},
 		nil,
 		NewCommandWithoutEntrypoint("cortex", BuildArgs(MergeFlags(map[string]string{
@@ -160,7 +157,7 @@ func (s *Scenario) StartQuerier(name string, flags map[string]string, image stri
 	return s.StartService(NewService(
 		name,
 		image,
-		s.networkName,
+		NetworkName,
 		[]int{80},
 		nil,
 		NewCommandWithoutEntrypoint("cortex", BuildArgs(MergeFlags(map[string]string{
@@ -183,7 +180,7 @@ func (s *Scenario) StartIngester(name string, flags map[string]string, image str
 	return s.StartService(NewService(
 		name,
 		image,
-		s.networkName,
+		NetworkName,
 		[]int{80},
 		nil,
 		NewCommandWithoutEntrypoint("cortex", BuildArgs(MergeFlags(map[string]string{
@@ -211,7 +208,7 @@ func (s *Scenario) StartTableManager(name string, flags map[string]string, image
 	return s.StartService(NewService(
 		name,
 		image,
-		s.networkName,
+		NetworkName,
 		[]int{80},
 		nil,
 		NewCommandWithoutEntrypoint("cortex", BuildArgs(MergeFlags(map[string]string{
@@ -270,11 +267,43 @@ func (s *Scenario) Shutdown() {
 		}
 	}
 
-	// Teardown the docker network
-	if out, err := RunCommandAndGetOutput("docker", "network", "rm", s.networkName); err != nil {
-		fmt.Println(out)
-		fmt.Println("Unable to remove docker network", s.networkName, ":", err.Error())
+	// Ensure there are no leftover containers
+	if out, err := RunCommandAndGetOutput("docker", "ps", "-a", "--quiet", "--filter", fmt.Sprintf("network=%s", NetworkName)); err == nil {
+		for _, containerID := range strings.Split(string(out), "\n") {
+			containerID = strings.TrimSpace(containerID)
+			if containerID == "" {
+				continue
+			}
+
+			if out, err = RunCommandAndGetOutput("docker", "rm", "--force", containerID); err != nil {
+				fmt.Println(string(out))
+				fmt.Println("Unable to cleanup leftover container", containerID, ":", err.Error())
+			}
+		}
+	} else {
+		fmt.Println(string(out))
+		fmt.Println("Unable to cleanup leftover containers:", err.Error())
 	}
+
+	// Teardown the docker network. In case the network does not exists (ie. this function
+	// is called during the setup of the scenario) we skip the removal in order to not log
+	// an error which may be misleading.
+	if ok, err := existDockerNetwork(); ok || err != nil {
+		if out, err := RunCommandAndGetOutput("docker", "network", "rm", NetworkName); err != nil {
+			fmt.Println(string(out))
+			fmt.Println("Unable to remove docker network", NetworkName, ":", err.Error())
+		}
+	}
+}
+
+func existDockerNetwork() (bool, error) {
+	out, err := RunCommandAndGetOutput("docker", "network", "ls", "--quiet", "--filter", fmt.Sprintf("name=%s", NetworkName))
+	if err != nil {
+		fmt.Println(string(out))
+		fmt.Println("Unable to check if docker network", NetworkName, "exists:", err.Error())
+	}
+
+	return strings.TrimSpace(string(out)) != "", nil
 }
 
 // getDefaultCortexImage returns the Docker image to use to run Cortex.

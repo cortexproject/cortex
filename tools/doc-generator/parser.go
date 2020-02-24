1d7e4d052f80f24bfdf88a0c837c395ac7b9ebaa
Tom Wilkie <tom@grafana.com>
2020-02-24 16:14:30 +0000

    Teach doc generator how to handle flagext.Secret.
    Signed-off-by: Tom Wilkie <tom@grafana.com>

--- tools/doc-generator/parser.go
+++ tools/doc-generator/parser.go
@@ -332,6 +332,22 @@ func getCustomFieldEntry(field reflect.StructField, fieldValue reflect.Value, fl
 			fieldDefault: fieldFlag.DefValue,
 		}, nil
 	}
+	if field.Type == reflect.TypeOf(flagext.Secret{}) {
+		fieldFlag, err := getFieldFlag(field, fieldValue, flags)
+		if err != nil {
+			return nil, err
+		}
+
+		return &configEntry{
+			kind:         "field",
+			name:         getFieldName(field),
+			required:     isFieldRequired(field),
+			fieldFlag:    fieldFlag.Name,
+			fieldDesc:    fieldFlag.Usage,
+			fieldType:    "string",
+			fieldDefault: fieldFlag.DefValue,
+		}, nil
+	}
 
 	return nil, nil
 }

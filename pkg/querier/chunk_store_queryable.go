15646945c249d8529b8b03d2515b1d1f7cd28acd
Peter Štibraný <peter.stibrany@grafana.com>
2020-03-02 15:15:27 +0100

    Move comment about implementing SeriesWithChunks to type declaration
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>

--- pkg/querier/chunk_store_queryable.go
+++ pkg/querier/chunk_store_queryable.go
@@ -87,6 +87,7 @@ func (q *chunkStoreQuerier) Close() error {
 	return nil
 }
 
+// Implements SeriesWithChunks
 type chunkSeries struct {
 	labels            labels.Labels
 	chunks            []chunk.Chunk
@@ -103,7 +104,7 @@ func (s *chunkSeries) Iterator() storage.SeriesIterator {
 	return s.chunkIteratorFunc(s.chunks, model.Time(s.mint), model.Time(s.maxt))
 }
 
-// implements SeriesWithChunks
+// method from SeriesWithChunks interface
 func (s *chunkSeries) Chunks() []chunk.Chunk {
 	return s.chunks
 }

b5548ad09b7d5d5417532e5c6e02a216236b02ab
Peter Štibraný <peter.stibrany@grafana.com>
2020-03-09 11:09:28 +0100

    Added test case to verify that passed error matches error from Running.
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>

--- pkg/util/services/basic_service_test.go
+++ pkg/util/services/basic_service_test.go
@@ -293,3 +293,23 @@ func (sl *serviceListener) Terminated(from State) {
 	sl.ch <- fmt.Sprintf("terminated: %v", from)
 	close(sl.ch)
 }
+
+func TestFailureCaseFromRunningIsPassedToStopping(t *testing.T) {
+	err := errors.New("test error")
+
+	s := NewBasicService(nil, func(_ context.Context) error {
+		return err
+	}, func(failureCase error) error {
+		if failureCase != err {
+			t.Log("invalid failureCase")
+			t.Fail()
+		}
+		return nil
+	})
+
+	require.NoError(t, s.StartAsync(context.Background()))
+	require.Error(t, s.AwaitTerminated(context.Background())) // service fails, so we expect Failed state
+
+	fc := s.FailureCase()
+	require.Equal(t, err, fc)
+}

98007949793f91bd0fe7deca71d1c91397f8062b
Peter Štibraný <peter.stibrany@grafana.com>
2020-03-11 13:57:42 +0100

    Added trivial test, mostly to show that nil watcher returns nil channel.
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>

---
+++ pkg/util/service_failure_watch_test.go
@@ -0,0 +1,37 @@
+package util
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	e2 "github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+
+	"github.com/cortexproject/cortex/pkg/util/services"
+)
+
+func TestNilServiceFailureWatcher(t *testing.T) {
+	var w *ServiceFailureWatcher = nil
+
+	// prove it doesn't fail, but returns nil channel.
+	require.Nil(t, w.Chan())
+}
+
+func TestServiceFailureWatcher(t *testing.T) {
+	w := NewServiceFailureWatcher()
+
+	err := errors.New("failed...")
+
+	failing := services.NewBasicService(nil, nil, func(_ error) error {
+		return err
+	})
+
+	w.WatchService(failing)
+
+	require.NoError(t, failing.StartAsync(context.Background()))
+
+	e := <-w.Chan()
+	require.NotNil(t, e)
+	require.Equal(t, err, e2.Cause(e))
+}

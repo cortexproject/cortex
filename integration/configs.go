d5f8f81a7a767149d22694bbe50f95ea58208081
Goutham Veeramachaneni <gouthamve@gmail.com>
2020-03-12 10:32:43 +0000

    Walk back integration config changes to support old versions
    Signed-off-by: Goutham Veeramachaneni <gouthamve@gmail.com>

--- integration/configs.go
+++ integration/configs.go
@@ -22,10 +22,10 @@ const (
   schema: v9
   index:
     prefix: cortex_
-    period: 1w 
+    period: 168h 
   chunks:
     prefix: cortex_chunks_
-    period: 1w 
+    period: 168h 
 `
 
 	cortexAlertmanagerUserConfigYaml = `route:
@@ -91,7 +91,7 @@ tsdb:
 		"-dynamodb.url":                   fmt.Sprintf("dynamodb://u:p@%s-dynamodb.:8000", networkName),
 		"-dynamodb.poll-interval":         "1m",
 		"-schema-config-file":             filepath.Join(e2e.ContainerSharedDir, cortexSchemaConfigFile),
-		"-table-manager.retention-period": "1w",
+		"-table-manager.retention-period": "168h",
 	}
 
 	ChunksStorageConfig = buildConfigFromTemplate(`
@@ -102,7 +102,7 @@ storage:
 
 table_manager:
   dynamodb_poll_interval: 1m
-  retention_period:       1w 
+  retention_period:       168h 
 
 schema:
 {{.SchemaConfig}}

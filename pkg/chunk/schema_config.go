5dca9a60d4d74a9446cdf6b9b5f5b1206fe91f77
Owen Diehl <ow.diehl@gmail.com>
2020-03-25 12:32:17 -0400

    periodconfig rowshards defaults Signed-off-by: Owen Diehl <ow.diehl@gmail.com>

--- pkg/chunk/schema_config.go
+++ pkg/chunk/schema_config.go
@@ -107,7 +107,9 @@ func (cfg *SchemaConfig) loadFromFile() error {
 // Validate the schema config and returns an error if the validation
 // doesn't pass
 func (cfg *SchemaConfig) Validate() error {
-	for _, periodCfg := range cfg.Configs {
+	for i, _ := range cfg.Configs {
+		periodCfg := &cfg.Configs[i]
+		periodCfg.applyDefaults()
 		if err := periodCfg.validate(); err != nil {
 			return err
 		}
@@ -142,10 +144,6 @@ func (cfg *SchemaConfig) ForEachAfter(t model.Time, f func(config *PeriodConfig)
 
 // CreateSchema returns the schema defined by the PeriodConfig
 func (cfg PeriodConfig) CreateSchema() Schema {
-	rowShards := defaultRowShards(cfg.Schema)
-	if cfg.RowShards > 0 {
-		rowShards = cfg.RowShards
-	}
 
 	var e entries
 	switch cfg.Schema {
@@ -165,12 +163,12 @@ func (cfg PeriodConfig) CreateSchema() Schema {
 		e = v9Entries{}
 	case "v10":
 		e = v10Entries{
-			rowShards: rowShards,
+			rowShards: cfg.RowShards,
 		}
 	case "v11":
 		e = v11Entries{
 			v10Entries: v10Entries{
-				rowShards: rowShards,
+				rowShards: cfg.RowShards,
 			},
 		}
 	default:
@@ -191,6 +189,13 @@ func (cfg PeriodConfig) createBucketsFunc() (schemaBucketsFunc, time.Duration) {
 	}
 }
 
+func (cfg *PeriodConfig) applyDefaults() {
+	// apply default shard values
+	if cfg.RowShards == 0 {
+		cfg.RowShards = defaultRowShards(cfg.Schema)
+	}
+}
+
 // validate the period config
 func (cfg PeriodConfig) validate() error {
 	// Ensure the schema version exists
@@ -208,6 +213,17 @@ func (cfg PeriodConfig) validate() error {
 
 	if cfg.ChunkTables.Period > 0 && cfg.ChunkTables.Period%bucketsPeriod != 0 {
 		return errInvalidTablePeriod
+	}
+
+	switch cfg.Schema {
+	case "v1", "v2", "v3", "v4", "v5", "v6", "v9":
+	case "v10", "v11":
+		if cfg.RowShards == 0 {
+			return fmt.Errorf("Must have row_shards > 0 (current: %d) for schema (%s)", cfg.RowShards, cfg.Schema)
+		}
+	default:
+		// protects us from adding schemas and not updating this block
+		return fmt.Errorf("unexpected schema (%s)", cfg.Schema)
 	}
 
 	return nil

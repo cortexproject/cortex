d2dfb21236457222c2443c7df773df1746e249d1
Peter Štibraný <peter.stibrany@grafana.com>
2020-04-01 12:39:47 +0200

    Moved comment outside to make Lint happier.
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>

--- integration/e2e/images/images.go
+++ integration/e2e/images/images.go
@@ -1,10 +1,11 @@
 package images
 
+// If you change the image tag, remember to update it in the preloading done
+// by CircleCI too (see .circleci/config.yml).
+
 // These are variables so that they can be modified.
 
 var (
-	// If you change the image tag, remember to update it in the preloading done
-	// by CircleCI too (see .circleci/config.yml).
 	Memcached        = "memcached:1.6.1"
 	Minio            = "minio/minio:RELEASE.2019-12-30T05-45-39Z"
 	Consul           = "consul:0.9"

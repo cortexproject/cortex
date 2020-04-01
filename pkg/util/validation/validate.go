a81e8b2840ab877ea88a740a123c9d8fc9917aa0
gotjosh <josue@grafana.com>
2020-04-01 14:25:15 +0100

    Update pkg/util/validation/validate.go
    Co-Authored-By: Marco Pracucci <marco@pracucci.com>
    Signed-off-by: gotjosh <josue@grafana.com>

--- pkg/util/validation/validate.go
+++ pkg/util/validation/validate.go
@@ -68,7 +68,7 @@ var DiscardedSamples = prometheus.NewCounterVec(
 )
 var DiscardedMetadata = prometheus.NewCounterVec(
 	prometheus.CounterOpts{
-		Name: "cortex_dicarded_metadata_total",
+		Name: "cortex_discarded_metadata_total",
 		Help: "The total number of metadata that were discarded",
 	},
 	[]string{discardReasonLabel, "user"},

057846a4a2f1fda25a4488cb3dba5c3554dcf555
Marco Pracucci <marco@pracucci.com>
2020-04-03 19:01:15 +0200

    Changed health check logic
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- pkg/cortex/health_check.go
+++ pkg/cortex/health_check.go
@@ -22,7 +22,7 @@ func newHealthCheck(sm *services.Manager) *healthCheck {
 
 // Check implements the grpc healthcheck.
 func (h *healthCheck) Check(_ context.Context, _ *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
-	if !h.sm.IsHealthy() {
+	if !h.isHealthy() {
 		return &grpc_health_v1.HealthCheckResponse{Status: grpc_health_v1.HealthCheckResponse_NOT_SERVING}, nil
 	}
 
@@ -32,4 +32,19 @@ func (h *healthCheck) Check(_ context.Context, _ *grpc_health_v1.HealthCheckRequ
 // Watch implements the grpc healthcheck.
 func (h *healthCheck) Watch(_ *grpc_health_v1.HealthCheckRequest, _ grpc_health_v1.Health_WatchServer) error {
 	return status.Error(codes.Unimplemented, "Watching is not supported")
+}
+
+// isHealthy returns whether the Cortex instance should be considered healthy.
+func (h *healthCheck) isHealthy() bool {
+	states := h.sm.ServicesByState()
+
+	// Given this is an health check endpoint for the whole instance, we should consider
+	// it healthy after all services have been started (running) and until there's
+	// still a service stopping (because some services, like ingesters, are still
+	// fully functioning while stopping).
+	if len(states[services.New]) > 0 || len(states[services.Starting]) > 0 || len(states[services.Failed]) > 0 {
+		return false
+	}
+
+	return len(states[services.Running]) > 0 || len(states[services.Stopping]) > 0
 }

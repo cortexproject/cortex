c5e8b2de815e8be6fdde1cc40966bbe8f16b2fe1
Marco Pracucci <marco@pracucci.com>
2020-04-15 12:00:05 +0200

    Explicit states in the store-gateway replication strategy
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- pkg/storegateway/replication_strategy.go
+++ pkg/storegateway/replication_strategy.go
@@ -36,7 +36,7 @@ func (s *ReplicationStrategy) ShouldExtendReplicaSet(instance ring.IngesterDesc,
 		// - JOINING: the previous replica set should be kept while an instance is JOINING
 		// - LEAVING: the instance is going to be decommissioned soon so we need to include
 		//   		  another replica in the set
-		return instance.GetState() != ring.ACTIVE
+		return instance.GetState() == ring.JOINING || instance.GetState() == ring.LEAVING
 	case ring.BlocksRead:
 		return false
 	default:

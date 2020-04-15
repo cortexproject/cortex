985c089947c5bbe288dde789c4389c56d796cfe6
Wing924 <weihe924stephen@gmail.com>
2020-04-15 19:10:26 +0900

    fix
    Signed-off-by: Wing924 <weihe924stephen@gmail.com>

--- pkg/util/push/push_test.go
+++ pkg/util/push/push_test.go
@@ -70,19 +70,19 @@ func createRemoteWriteRequest(t *testing.T) *http.Request {
 
 func createCortexRemoteWriteRequest(t *testing.T) *http.Request {
 	t.Helper()
-	ts := &client.TimeSeries{
-		Labels: []client.LabelAdapter{
-			{Name: "__name__", Value: "foo"},
-		},
-		Samples: []client.Sample{
-			{Value: 1, TimestampMs: time.Date(2020, 4, 1, 0, 0, 0, 0, time.UTC).UnixNano()},
+	ts := client.PreallocTimeseries{
+		TimeSeries: &client.TimeSeries{
+			Labels: []client.LabelAdapter{
+				{Name: "__name__", Value: "foo"},
+			},
+			Samples: []client.Sample{
+				{Value: 1, TimestampMs: time.Date(2020, 4, 1, 0, 0, 0, 0, time.UTC).UnixNano()},
+			},
 		},
 	}
 	input := client.WriteRequest{
-		Timeseries: []client.PreallocTimeseries{
-			client.PreallocTimeseries{ts},
-		},
-		Source: client.RULE,
+		Timeseries: []client.PreallocTimeseries{ts},
+		Source:     client.RULE,
 	}
 	inoutBytes, err := input.Marshal()
 	require.NoError(t, err)

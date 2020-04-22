8f7ea1f019aac07045e5809026a54017f826c8e5
Bryan Boreham <bryan@weave.works>
2020-04-22 11:26:43 +0000

    Re-use buffers when serialising chunks.
    Improves performance of ingester handover and WAL checkpoint by
    reducing garbage.
    
    Signed-off-by: Bryan Boreham <bryan@weave.works>

--- pkg/ingester/wal.go
+++ pkg/ingester/wal.go
@@ -386,7 +386,7 @@ func (w *walWrapper) deleteCheckpoints(maxIndex int) (err error) {
 // checkpointSeries write the chunks of the series to the checkpoint.
 func (w *walWrapper) checkpointSeries(cp *wal.WAL, userID string, fp model.Fingerprint, series *memorySeries, wireChunks []client.Chunk) ([]client.Chunk, error) {
 	var err error
-	wireChunks, err = toWireChunks(series.chunkDescs, wireChunks[:0])
+	wireChunks, err = toWireChunks(series.chunkDescs, wireChunks)
 	if err != nil {
 		return wireChunks, err
 	}

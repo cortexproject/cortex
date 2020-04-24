ca85d8abbfe814112c7142671cf94655a9150d9e
Peter Štibraný <peter.stibrany@grafana.com>
2020-04-24 16:33:22 +0200

    Change alertmanager test to match previous test.
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>

--- integration/alertmanager_test.go
+++ integration/alertmanager_test.go
@@ -21,7 +21,7 @@ func TestAlertmanager(t *testing.T) {
 
 	alertmanager := e2ecortex.NewAlertmanager("alertmanager", AlertmanagerFlags, "")
 	require.NoError(t, s.StartAndWaitReady(alertmanager))
-	require.NoError(t, alertmanager.WaitForPromQL("cortex_alertmanager_configs{status='valid'} > 0"))
+	require.NoError(t, alertmanager.WaitForPromQL("cortex_alertmanager_configs{status='valid'} == 1"))
 
 	c, err := e2ecortex.NewClient("", "", alertmanager.HTTPEndpoint(), "", "user-1")
 	require.NoError(t, err)

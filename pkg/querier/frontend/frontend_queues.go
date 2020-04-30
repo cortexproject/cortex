07074f28bc5c13a1bfb724bb8ed3ed7aeca02857
Joe Elliott <number101010@gmail.com>
2020-04-30 15:41:53 -0400

    Moved isConsistent into tests
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- pkg/querier/frontend/frontend_queues.go
+++ pkg/querier/frontend/frontend_queues.go
@@ -2,11 +2,10 @@ package frontend
 
 import (
 	"container/list"
-	"fmt"
 )
 
 type queueRecord struct {
-	q      chan *request
+	ch     chan *request
 	userID string
 }
 
@@ -45,7 +44,7 @@ func (q *queueManager) getNextQueue() (chan *request, string) {
 
 	qr := current.Value.(queueRecord)
 
-	return qr.q, qr.userID
+	return qr.ch, qr.userID
 }
 
 func (q *queueManager) deleteQueue(userID string) {
@@ -69,7 +68,7 @@ func (q *queueManager) getOrAddQueue(userID string) chan *request {
 
 	if element == nil {
 		qr := queueRecord{
-			q:      make(chan *request, q.maxQueueSize),
+			ch:     make(chan *request, q.maxQueueSize),
 			userID: userID,
 		}
 
@@ -83,44 +82,5 @@ func (q *queueManager) getOrAddQueue(userID string) chan *request {
 		q.queues[userID] = element
 	}
 
-	return element.Value.(queueRecord).q
-}
-
-// isConsistent() returns true if every userID in the map is also in the linked list and vice versa.
-//  This is horribly inefficient.  Use for testing only.
-func (q *queueManager) isConsistent() error {
-	// let's confirm that every element in the map is in the list and all values are request queues
-	for k, v := range q.queues {
-		found := false
-
-		for e := q.l.Front(); e != nil; e = e.Next() {
-			qr, ok := e.Value.(queueRecord)
-			if !ok {
-				return fmt.Errorf("element value is not a queueRecord %v", e.Value)
-			}
-
-			if e == v {
-				if qr.userID != k {
-					return fmt.Errorf("mismatched userID between map %s and linked list %s", k, qr.userID)
-				}
-
-				found = true
-				break
-			}
-		}
-
-		if !found {
-			return fmt.Errorf("userID %s not found in list", k)
-		}
-	}
-
-	// now check the length to make sure there's not extra list items somehow
-	listLen := q.l.Len()
-	mapLen := len(q.queues)
-
-	if listLen != mapLen {
-		return fmt.Errorf("Length mismatch list:%d map:%d", listLen, mapLen)
-	}
-
-	return nil
+	return element.Value.(queueRecord).ch
 }
--- pkg/querier/frontend/frontend_queues_test.go
+++ pkg/querier/frontend/frontend_queues_test.go
@@ -11,7 +11,7 @@ import (
 func TestFrontendQueues(t *testing.T) {
 	m := newQueueManager(0)
 	assert.NotNil(t, m)
-	assert.NoError(t, m.isConsistent())
+	assert.NoError(t, isConsistent(m))
 	q, _ := m.getNextQueue()
 	assert.Nil(t, q)
 
@@ -32,7 +32,7 @@ func TestFrontendQueues(t *testing.T) {
 
 	// remove one and round robin the others
 	m.deleteQueue("one")
-	assert.NoError(t, m.isConsistent())
+	assert.NoError(t, isConsistent(m))
 
 	confirmOrder(t, m, qTwo, qThree, qTwo)
 
@@ -42,41 +42,24 @@ func TestFrontendQueues(t *testing.T) {
 
 	// remove current and confirm round robin continues
 	m.deleteQueue("two")
-	assert.NoError(t, m.isConsistent())
+	assert.NoError(t, isConsistent(m))
 
 	confirmOrder(t, m, qFour, qThree, qFour)
 
 	m.deleteQueue("three")
-	assert.NoError(t, m.isConsistent())
+	assert.NoError(t, isConsistent(m))
 
 	m.deleteQueue("four")
-	assert.NoError(t, m.isConsistent())
+	assert.NoError(t, isConsistent(m))
 
 	q, _ = m.getNextQueue()
 	assert.Nil(t, q)
 }
 
-func getOrAddQueue(t *testing.T, m *queueManager, tenant string) chan *request {
-	q := m.getOrAddQueue(tenant)
-	assert.NotNil(t, q)
-	assert.NoError(t, m.isConsistent())
-	assert.Equal(t, q, m.getOrAddQueue(tenant))
-
-	return q
-}
-
-func confirmOrder(t *testing.T, m *queueManager, qs ...chan *request) {
-	for _, q := range qs {
-		qNext, _ := m.getNextQueue()
-		assert.Equal(t, q, qNext)
-		assert.NoError(t, m.isConsistent())
-	}
-}
-
 func TestFrontendQueuesConsistency(t *testing.T) {
 	m := newQueueManager(0)
 	assert.NotNil(t, m)
-	assert.NoError(t, m.isConsistent())
+	assert.NoError(t, isConsistent(m))
 	q, _ := m.getNextQueue()
 	assert.Nil(t, q)
 
@@ -90,10 +73,66 @@ func TestFrontendQueuesConsistency(t *testing.T) {
 			m.deleteQueue(generateTenant())
 		}
 
-		assert.NoErrorf(t, m.isConsistent(), "last action %d", i)
+		assert.NoErrorf(t, isConsistent(m), "last action %d", i)
 	}
 }
 
 func generateTenant() string {
 	return fmt.Sprint("tenant-", rand.Int()%5)
+}
+
+func getOrAddQueue(t *testing.T, m *queueManager, tenant string) chan *request {
+	q := m.getOrAddQueue(tenant)
+	assert.NotNil(t, q)
+	assert.NoError(t, isConsistent(m))
+	assert.Equal(t, q, m.getOrAddQueue(tenant))
+
+	return q
+}
+
+func confirmOrder(t *testing.T, m *queueManager, qs ...chan *request) {
+	for _, q := range qs {
+		qNext, _ := m.getNextQueue()
+		assert.Equal(t, q, qNext)
+		assert.NoError(t, isConsistent(m))
+	}
+}
+
+// isConsistent() returns true if every userID in the map is also in the linked list and vice versa.
+//  This is horribly inefficient.  Use for testing only.
+func isConsistent(q *queueManager) error {
+	// let's confirm that every element in the map is in the list and all values are request queues
+	for k, v := range q.queues {
+		found := false
+
+		for e := q.l.Front(); e != nil; e = e.Next() {
+			qr, ok := e.Value.(queueRecord)
+			if !ok {
+				return fmt.Errorf("element value is not a queueRecord %v", e.Value)
+			}
+
+			if e == v {
+				if qr.userID != k {
+					return fmt.Errorf("mismatched userID between map %s and linked list %s", k, qr.userID)
+				}
+
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			return fmt.Errorf("userID %s not found in list", k)
+		}
+	}
+
+	// now check the length to make sure there's not extra list items somehow
+	listLen := q.l.Len()
+	mapLen := len(q.queues)
+
+	if listLen != mapLen {
+		return fmt.Errorf("Length mismatch list:%d map:%d", listLen, mapLen)
+	}
+
+	return nil
 }

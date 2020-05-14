d07b5fa15c20e4d3b69ba31631ba4955250711df
Annanay <annanayagarwal@gmail.com>
2020-05-14 12:54:32 +0530

    Add metric to monitor cert expiry time
    Signed-off-by: Annanay <annanayagarwal@gmail.com>

--- pkg/util/tls/tls.go
+++ pkg/util/tls/tls.go
@@ -7,8 +7,17 @@ import (
 	"io/ioutil"
 
 	"github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+)
+
+var (
+	tlsCertNotAfterTimestamp = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "cortex",
+		Name:      "tls_cert_not_after_timestamp",
+		Help:      "Timestamp of expiry for the TLS certificate",
+	}, []string{"filename"})
 )
 
 // ClientConfig is the config for client TLS.
@@ -41,6 +50,13 @@ func (cfg *ClientConfig) GetTLSConfig() (*tls.Config, error) {
 		caCertPool = x509.NewCertPool()
 		caCertPool.AppendCertsFromPEM(caCert)
 		if len(clientCert.Certificate) > 0 && caCertPool != nil {
+			prometheus.MustRegister(tlsCertNotAfterTimestamp)
+			var x509Cert *x509.Certificate
+			var err error
+			if x509Cert, err = x509.ParseCertificate(clientCert.Certificate[0]); err != nil {
+				return nil, errors.Wrap(err, "error parsing TLS certificate")
+			}
+			tlsCertNotAfterTimestamp.WithLabelValues(cfg.CertPath).Set(float64(x509Cert.NotAfter.Unix()))
 			return &tls.Config{
 				InsecureSkipVerify: true,
 				Certificates:       []tls.Certificate{clientCert},

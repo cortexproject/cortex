0f4b13495ea56eb70a584c8b9d7278d55cba4d38
Jacob Lisi <jacob.t.lisi@gmail.com>
2020-05-28 18:24:20 -0400

    different name for block store queryable
    Signed-off-by: Jacob Lisi <jacob.t.lisi@gmail.com>

--- pkg/querier/blocks_store_queryable.go
+++ pkg/querier/blocks_store_queryable.go
@@ -150,7 +150,7 @@ func NewBlocksStoreQueryableFromConfig(querierCfg Config, gatewayCfg storegatewa
 
 	if gatewayCfg.ShardingEnabled {
 		storesRingCfg := gatewayCfg.ShardingRing.ToRingConfig()
-		storesRingBackend, err := kv.NewClient(storegateway.RingNameForClient, storesRingCfg.KVStore, ring.GetCodec(), reg)
+		storesRingBackend, err := kv.NewClient(storegateway.RingNameForClient+"-block-store", storesRingCfg.KVStore, ring.GetCodec(), reg)
 		if err != nil {
 			return nil, errors.Wrap(err, "failed to create store-gateway ring backend")
 		}

cortexproject/cortex

commit b40c11774a5522b8168d44ea6427912b1c6e3fd5

Upgrade Thanos and Prometheus (#2604)
* Upgrade Thanos and Prometheus

Signed-off-by: Ganesh Vernekar <cs15btech11018@iith.ac.in>

* Fix review comments

Signed-off-by: Ganesh Vernekar <cs15btech11018@iith.ac.in>

* Fix review comments

Signed-off-by: Ganesh Vernekar <cs15btech11018@iith.ac.in>

* Move etcd to v3.4.9 and address reviews

Signed-off-by: Ganesh Vernekar <cs15btech11018@iith.ac.in>

* Fix lint

Signed-off-by: Ganesh Vernekar <cs15btech11018@iith.ac.in>

* Reviews

Signed-off-by: Ganesh Vernekar <cs15btech11018@iith.ac.in>

* Update pkg/querier/blocks_store_balanced_set.go

Signed-off-by: Marco Pracucci <marco@pracucci.com>

Co-authored-by: Marco Pracucci <marco@pracucci.com>

vendor/github.com/prometheus/prometheus/storage/generic.go
@@ -1,0 +1,133 @@
+// Copyright 2020 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This file holds boilerplate adapters for generic MergeSeriesSet and MergeQuerier functions, so we can have one optimized
+// solution that works for both ChunkSeriesSet as well as SeriesSet.
+
+package storage
+
+import "github.com/prometheus/prometheus/pkg/labels"
+
+type genericQuerier interface {
+	baseQuerier
+	Select(bool, *SelectHints, ...*labels.Matcher) (genericSeriesSet, Warnings, error)
+}
+
+type genericSeriesSet interface {
+	Next() bool
+	At() Labels
+	Err() error
+}
+
+type genericSeriesMergeFunc func(...Labels) Labels
+
+type genericSeriesSetAdapter struct {
+	SeriesSet
+}
+
+func (a *genericSeriesSetAdapter) At() Labels {
+	return a.SeriesSet.At()
+}
+
+type genericChunkSeriesSetAdapter struct {
+	ChunkSeriesSet
+}
+
+func (a *genericChunkSeriesSetAdapter) At() Labels {
+	return a.ChunkSeriesSet.At()
+}
+
+type genericQuerierAdapter struct {
+	baseQuerier
+
+	// One-of. If both are set, Querier will be used.
+	q  Querier
+	cq ChunkQuerier
+}
+
+func (q *genericQuerierAdapter) Select(sortSeries bool, hints *SelectHints, matchers ...*labels.Matcher) (genericSeriesSet, Warnings, error) {
+	if q.q != nil {
+		s, w, err := q.q.Select(sortSeries, hints, matchers...)
+		return &genericSeriesSetAdapter{s}, w, err
+	}
+	s, w, err := q.cq.Select(sortSeries, hints, matchers...)
+	return &genericChunkSeriesSetAdapter{s}, w, err
+}
+
+func newGenericQuerierFrom(q Querier) genericQuerier {
+	return &genericQuerierAdapter{baseQuerier: q, q: q}
+}
+
+func newGenericQuerierFromChunk(cq ChunkQuerier) genericQuerier {
+	return &genericQuerierAdapter{baseQuerier: cq, cq: cq}
+}
+
+type querierAdapter struct {
+	genericQuerier
+}
+
+type seriesSetAdapter struct {
+	genericSeriesSet
+}
+
+func (a *seriesSetAdapter) At() Series {
+	return a.genericSeriesSet.At().(Series)
+}
+
+func (q *querierAdapter) Select(sortSeries bool, hints *SelectHints, matchers ...*labels.Matcher) (SeriesSet, Warnings, error) {
+	s, w, err := q.genericQuerier.Select(sortSeries, hints, matchers...)
+	return &seriesSetAdapter{s}, w, err
+}
+
+type chunkQuerierAdapter struct {
+	genericQuerier
+}
+
+type chunkSeriesSetAdapter struct {
+	genericSeriesSet
+}
+
+func (a *chunkSeriesSetAdapter) At() ChunkSeries {
+	return a.genericSeriesSet.At().(ChunkSeries)
+}
+
+func (q *chunkQuerierAdapter) Select(sortSeries bool, hints *SelectHints, matchers ...*labels.Matcher) (ChunkSeriesSet, Warnings, error) {
+	s, w, err := q.genericQuerier.Select(sortSeries, hints, matchers...)
+	return &chunkSeriesSetAdapter{s}, w, err
+}
+
+type seriesMergerAdapter struct {
+	VerticalSeriesMergeFunc
+	buf []Series
+}
+
+func (a *seriesMergerAdapter) Merge(s ...Labels) Labels {
+	a.buf = a.buf[:0]
+	for _, ser := range s {
+		a.buf = append(a.buf, ser.(Series))
+	}
+	return a.VerticalSeriesMergeFunc(a.buf...)
+}
+
+type chunkSeriesMergerAdapter struct {
+	VerticalChunkSeriesMergerFunc
+	buf []ChunkSeries
+}
+
+func (a *chunkSeriesMergerAdapter) Merge(s ...Labels) Labels {
+	a.buf = a.buf[:0]
+	for _, ser := range s {
+		a.buf = append(a.buf, ser.(ChunkSeries))
+	}
+	return a.VerticalChunkSeriesMergerFunc(a.buf...)
+}

pkg/querier/blocks_store_queryable_test.go
@@ -227,7 +227,7 @@ func TestBlocksStoreQuerier_SelectSorted(t *testing.T) {
 				labels.MustNewMatcher(labels.MatchEqual, labels.MetricName, metricName),
 			}
 
-			set, warnings, err := q.SelectSorted(nil, matchers...)
+			set, warnings, err := q.Select(true, nil, matchers...)
 			if testData.expectedErr != "" {
 				assert.EqualError(t, err, testData.expectedErr)
 				assert.Nil(t, set)

pkg/querier/duplicates_test.go
@@ -120,12 +120,8 @@ type testQuerier struct {
 	ts *timeSeriesSeriesSet
 }
 
-func (m testQuerier) SelectSorted(sp *storage.SelectParams, matchers ...*labels.Matcher) (storage.SeriesSet, storage.Warnings, error) {
+func (m testQuerier) Select(_ bool, sp *storage.SelectHints, matchers ...*labels.Matcher) (storage.SeriesSet, storage.Warnings, error) {
 	return m.ts, nil, nil
-}
-
-func (m testQuerier) Select(sp *storage.SelectParams, matchers ...*labels.Matcher) (storage.SeriesSet, storage.Warnings, error) {
-	return m.SelectSorted(sp, matchers...)
 }
 
 func (m testQuerier) LabelValues(name string) ([]string, storage.Warnings, error) {

commit dfe5eaafece29331fed4b02521201019b427503e

Fixed incorrect query results introduced in #2604 caused by a buffer incorrectly reused while iterating samples.
Signed-off-by: Marco Pracucci <marco@pracucci.com>

vendor/github.com/prometheus/prometheus/storage/generic.go
@@ -108,26 +108,24 @@ func (q *chunkQuerierAdapter) Select(sortSeries bool, hints *SelectHints, matche
 
 type seriesMergerAdapter struct {
 	VerticalSeriesMergeFunc
-	buf []Series
 }
 
 func (a *seriesMergerAdapter) Merge(s ...Labels) Labels {
-	a.buf = a.buf[:0]
+	buf := make([]Series, 0, len(s))
 	for _, ser := range s {
-		a.buf = append(a.buf, ser.(Series))
+		buf = append(buf, ser.(Series))
 	}
-	return a.VerticalSeriesMergeFunc(a.buf...)
+	return a.VerticalSeriesMergeFunc(buf...)
 }
 
 type chunkSeriesMergerAdapter struct {
 	VerticalChunkSeriesMergerFunc
-	buf []ChunkSeries
 }
 
 func (a *chunkSeriesMergerAdapter) Merge(s ...Labels) Labels {
-	a.buf = a.buf[:0]
+	buf := make([]ChunkSeries, 0, len(s))
 	for _, ser := range s {
-		a.buf = append(a.buf, ser.(ChunkSeries))
+		buf = append(buf, ser.(ChunkSeries))
 	}
-	return a.VerticalChunkSeriesMergerFunc(a.buf...)
+	return a.VerticalChunkSeriesMergerFunc(buf...)
 }

pkg/querier/block_test.go
@@ -2,12 +2,14 @@ package querier
 
 import (
 	"math"
+	"sort"
 	"strconv"
 	"testing"
 	"time"
 
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/pkg/labels"
+	"github.com/prometheus/prometheus/promql"
 	"github.com/prometheus/prometheus/storage"
 	"github.com/prometheus/prometheus/tsdb/chunkenc"
 	"github.com/stretchr/testify/assert"
@@ -119,7 +121,7 @@ func TestBlockQuerierSeriesSet(t *testing.T) {
 			{
 				Labels: mkLabels("__name__", "first", "a", "a"),
 				Chunks: []storepb.AggrChunk{
-					createChunkWithSineSamples(now, now.Add(100*time.Second), 3*time.Millisecond), // ceil(100 / 0.003) samples (= 33334)
+					createAggrChunkWithSineSamples(now, now.Add(100*time.Second), 3*time.Millisecond), // ceil(100 / 0.003) samples (= 33334)
 				},
 			},
 
@@ -127,7 +129,7 @@ func TestBlockQuerierSeriesSet(t *testing.T) {
 			{
 				Labels: mkLabels("__name__", "first", "a", "a"),
 				Chunks: []storepb.AggrChunk{
-					createChunkWithSineSamples(now.Add(100*time.Second), now.Add(200*time.Second), 3*time.Millisecond), // ceil(100 / 0.003) samples more, 66668 in total
+					createAggrChunkWithSineSamples(now.Add(100*time.Second), now.Add(200*time.Second), 3*time.Millisecond), // ceil(100 / 0.003) samples more, 66668 in total
 				},
 			},
 
@@ -136,9 +138,9 @@ func TestBlockQuerierSeriesSet(t *testing.T) {
 				Labels: mkLabels("__name__", "second"),
 				Chunks: []storepb.AggrChunk{
 					// unordered chunks
-					createChunkWithSineSamples(now.Add(400*time.Second), now.Add(600*time.Second), 5*time.Millisecond), // 200 / 0.005 (= 40000 samples, = 120000 in total)
-					createChunkWithSineSamples(now.Add(200*time.Second), now.Add(400*time.Second), 5*time.Millisecond), // 200 / 0.005 (= 40000 samples)
-					createChunkWithSineSamples(now, now.Add(200*time.Second), 5*time.Millisecond),                      // 200 / 0.005 (= 40000 samples)
+					createAggrChunkWithSineSamples(now.Add(400*time.Second), now.Add(600*time.Second), 5*time.Millisecond), // 200 / 0.005 (= 40000 samples, = 120000 in total)
+					createAggrChunkWithSineSamples(now.Add(200*time.Second), now.Add(400*time.Second), 5*time.Millisecond), // 200 / 0.005 (= 40000 samples)
+					createAggrChunkWithSineSamples(now, now.Add(200*time.Second), 5*time.Millisecond),                      // 200 / 0.005 (= 40000 samples)
 				},
 			},
 
@@ -146,14 +148,14 @@ func TestBlockQuerierSeriesSet(t *testing.T) {
 			{
 				Labels: mkLabels("__name__", "overlapping"),
 				Chunks: []storepb.AggrChunk{
-					createChunkWithSineSamples(now, now.Add(10*time.Second), 5*time.Millisecond), // 10 / 0.005 = 2000 samples
+					createAggrChunkWithSineSamples(now, now.Add(10*time.Second), 5*time.Millisecond), // 10 / 0.005 = 2000 samples
 				},
 			},
 			{
 				Labels: mkLabels("__name__", "overlapping"),
 				Chunks: []storepb.AggrChunk{
 					// 10 / 0.005 = 2000 samples, but first 1000 are overlapping with previous series, so this chunk only contributes 1000
-					createChunkWithSineSamples(now.Add(5*time.Second), now.Add(15*time.Second), 5*time.Millisecond),
+					createAggrChunkWithSineSamples(now.Add(5*time.Second), now.Add(15*time.Second), 5*time.Millisecond),
 				},
 			},
 
@@ -162,21 +164,21 @@ func TestBlockQuerierSeriesSet(t *testing.T) {
 				Labels: mkLabels("__name__", "overlapping2"),
 				Chunks: []storepb.AggrChunk{
 					// entire range overlaps with the next chunk, so this chunks contributes 0 samples (it will be sorted as second)
-					createChunkWithSineSamples(now.Add(3*time.Second), now.Add(7*time.Second), 5*time.Millisecond),
+					createAggrChunkWithSineSamples(now.Add(3*time.Second), now.Add(7*time.Second), 5*time.Millisecond),
 				},
 			},
 			{
 				Labels: mkLabels("__name__", "overlapping2"),
 				Chunks: []storepb.AggrChunk{
 					// this chunk has completely overlaps previous chunk. Since its minTime is lower, it will be sorted as first.
-					createChunkWithSineSamples(now, now.Add(10*time.Second), 5*time.Millisecond), // 10 / 0.005 = 2000 samples
+					createAggrChunkWithSineSamples(now, now.Add(10*time.Second), 5*time.Millisecond), // 10 / 0.005 = 2000 samples
 				},
 			},
 			{
 				Labels: mkLabels("__name__", "overlapping2"),
 				Chunks: []storepb.AggrChunk{
 					// no samples
-					createChunkWithSineSamples(now, now, 5*time.Millisecond),
+					createAggrChunkWithSineSamples(now, now, 5*time.Millisecond),
 				},
 			},
 
@@ -184,7 +186,7 @@ func TestBlockQuerierSeriesSet(t *testing.T) {
 				Labels: mkLabels("__name__", "overlapping2"),
 				Chunks: []storepb.AggrChunk{
 					// 2000 samples more (10 / 0.005)
-					createChunkWithSineSamples(now.Add(20*time.Second), now.Add(30*time.Second), 5*time.Millisecond),
+					createAggrChunkWithSineSamples(now.Add(20*time.Second), now.Add(30*time.Second), 5*time.Millisecond),
 				},
 			},
 		},
@@ -216,24 +218,43 @@ func verifyNextSeries(t *testing.T, ss storage.SeriesSet, labels labels.Labels, 
 	require.Equal(t, samples, count)
 }
 
-func createChunkWithSineSamples(minTime, maxTime time.Time, step time.Duration) storepb.AggrChunk {
-	chunk := chunkenc.NewXORChunk()
-	appender, err := chunk.Appender()
-	if err != nil {
-		panic(err)
-	}
+func createAggrChunkWithSineSamples(minTime, maxTime time.Time, step time.Duration) storepb.AggrChunk {
+	var samples []promql.Point
 
 	minT := minTime.Unix() * 1000
 	maxT := maxTime.Unix() * 1000
 	stepMillis := step.Milliseconds()
 
 	for t := minT; t < maxT; t += stepMillis {
-		appender.Append(t, math.Sin(float64(t)))
+		samples = append(samples, promql.Point{t, math.Sin(float64(t))})
+	}
+
+	return createAggrChunk(minT, maxT, samples...)
+}
+
+func createAggrChunkWithSamples(samples ...promql.Point) storepb.AggrChunk {
+	return createAggrChunk(samples[0].T, samples[len(samples)-1].T, samples...)
+}
+
+func createAggrChunk(minTime, maxTime int64, samples ...promql.Point) storepb.AggrChunk {
+	// Ensure samples are sorted by timestamp.
+	sort.Slice(samples, func(i, j int) bool {
+		return samples[i].T < samples[j].T
+	})
+
+	chunk := chunkenc.NewXORChunk()
+	appender, err := chunk.Appender()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, s := range samples {
+		appender.Append(s.T, s.V)
 	}
 
 	return storepb.AggrChunk{
-		MinTime: minT,
-		MaxTime: maxT,
+		MinTime: minTime,
+		MaxTime: maxTime,
 		Raw: &storepb.Chunk{
 			Type: storepb.Chunk_XOR,
 			Data: chunk.Bytes(),
@@ -268,7 +289,7 @@ func Benchmark_newBlockQuerierSeries(b *testing.B) {
 		"label_9", "value_9")
 
 	chunks := []storepb.AggrChunk{
-		createChunkWithSineSamples(time.Now(), time.Now().Add(-time.Hour), time.Minute),
+		createAggrChunkWithSineSamples(time.Now(), time.Now().Add(-time.Hour), time.Minute),
 	}
 
 	b.ResetTimer()
@@ -292,7 +313,7 @@ func Benchmark_blockQuerierSeriesSet_iteration(b *testing.B) {
 
 		// Create chunks with 1 sample per second.
 		for minT := int64(0); minT < numChunksPerSeries*numSamplesPerChunk; minT += numSamplesPerChunk {
-			chunks = append(chunks, createChunkWithSineSamples(util.TimeFromMillis(minT), util.TimeFromMillis(minT+numSamplesPerChunk), time.Millisecond))
+			chunks = append(chunks, createAggrChunkWithSineSamples(util.TimeFromMillis(minT), util.TimeFromMillis(minT+numSamplesPerChunk), time.Millisecond))
 		}
 
 		series = append(series, &storepb.Series{

pkg/querier/blocks_store_queryable_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/prometheus/pkg/labels"
+	"github.com/prometheus/prometheus/promql"
 	"github.com/prometheus/prometheus/storage"
 	"github.com/prometheus/prometheus/tsdb"
 	"github.com/prometheus/prometheus/tsdb/chunkenc"
@@ -22,6 +23,7 @@ import (
 	"github.com/thanos-io/thanos/pkg/block/metadata"
 	"github.com/thanos-io/thanos/pkg/store/hintspb"
 	"github.com/thanos-io/thanos/pkg/store/storepb"
+	"github.com/weaveworks/common/user"
 	"google.golang.org/grpc"
 
 	"github.com/cortexproject/cortex/pkg/storegateway/storegatewaypb"
@@ -393,6 +395,126 @@ func TestBlocksStoreQuerier_SelectSortedShouldHonorQueryStoreAfter(t *testing.T)
 			}
 		})
 	}
+}
+
+func TestBlocksStoreQuerier_PromQLExecution(t *testing.T) {
+	block1 := ulid.MustNew(1, nil)
+	block2 := ulid.MustNew(2, nil)
+	series1 := []storepb.Label{{Name: "__name__", Value: "metric_1"}}
+	series2 := []storepb.Label{{Name: "__name__", Value: "metric_2"}}
+
+	series1Samples := []promql.Point{
+		{T: 1589759955000, V: 1},
+		{T: 1589759970000, V: 1},
+		{T: 1589759985000, V: 1},
+		{T: 1589760000000, V: 1},
+		{T: 1589760015000, V: 1},
+		{T: 1589760030000, V: 1},
+	}
+
+	series2Samples := []promql.Point{
+		{T: 1589759955000, V: 2},
+		{T: 1589759970000, V: 2},
+		{T: 1589759985000, V: 2},
+		{T: 1589760000000, V: 2},
+		{T: 1589760015000, V: 2},
+		{T: 1589760030000, V: 2},
+	}
+
+	// Mock the finder to simulate we need to query two blocks.
+	finder := &blocksFinderMock{
+		Service: services.NewIdleService(nil, nil),
+	}
+	finder.On("GetBlocks", "user-1", mock.Anything, mock.Anything).Return([]*BlockMeta{
+		{Meta: metadata.Meta{BlockMeta: tsdb.BlockMeta{ULID: block1}}},
+		{Meta: metadata.Meta{BlockMeta: tsdb.BlockMeta{ULID: block2}}},
+	}, map[ulid.ULID]*metadata.DeletionMark(nil), error(nil))
+
+	// Mock the store to simulate each block is queried from a different store-gateway.
+	gateway1 := &storeGatewayClientMock{remoteAddr: "1.1.1.1", mockedResponses: []*storepb.SeriesResponse{
+		{
+			Result: &storepb.SeriesResponse_Series{
+				Series: &storepb.Series{
+					Labels: series1,
+					Chunks: []storepb.AggrChunk{
+						createAggrChunkWithSamples(series1Samples[:3]...),
+					},
+				},
+			},
+		}, {
+			Result: &storepb.SeriesResponse_Series{
+				Series: &storepb.Series{
+					Labels: series2,
+					Chunks: []storepb.AggrChunk{
+						createAggrChunkWithSamples(series2Samples[:3]...),
+					},
+				},
+			},
+		},
+		mockHintsResponse(block1),
+	}}
+
+	gateway2 := &storeGatewayClientMock{remoteAddr: "2.2.2.2", mockedResponses: []*storepb.SeriesResponse{
+		{
+			Result: &storepb.SeriesResponse_Series{
+				Series: &storepb.Series{
+					Labels: series1,
+					Chunks: []storepb.AggrChunk{
+						createAggrChunkWithSamples(series1Samples[3:]...),
+					},
+				},
+			},
+		}, {
+			Result: &storepb.SeriesResponse_Series{
+				Series: &storepb.Series{
+					Labels: series2,
+					Chunks: []storepb.AggrChunk{
+						createAggrChunkWithSamples(series2Samples[3:]...),
+					},
+				},
+			},
+		},
+		mockHintsResponse(block2),
+	}}
+
+	stores := &blocksStoreSetMock{
+		Service: services.NewIdleService(nil, nil),
+		mockedResult: map[BlocksStoreClient][]ulid.ULID{
+			gateway1: {block1},
+			gateway2: {block2},
+		},
+	}
+
+	// Instance the querier that will be executed to run the query.
+	logger := log.NewNopLogger()
+	queryable, err := NewBlocksStoreQueryable(stores, finder, NewBlocksConsistencyChecker(0, 0, logger, nil), 0, logger, nil)
+	require.NoError(t, err)
+	require.NoError(t, services.StartAndAwaitRunning(context.Background(), queryable))
+	defer services.StopAndAwaitTerminated(context.Background(), queryable) // nolint:errcheck
+
+	engine := promql.NewEngine(promql.EngineOpts{
+		Logger:     logger,
+		Timeout:    10 * time.Second,
+		MaxSamples: 1e6,
+	})
+
+	// Run a query.
+	q, err := engine.NewRangeQuery(queryable, `{__name__=~"metric.*"}`, time.Unix(1589759955, 0), time.Unix(1589760030, 0), 15*time.Second)
+	require.NoError(t, err)
+
+	ctx := user.InjectOrgID(context.Background(), "user-1")
+	res := q.Exec(ctx)
+	require.NoError(t, err)
+	require.NoError(t, res.Err)
+
+	matrix, err := res.Matrix()
+	require.NoError(t, err)
+	require.Len(t, matrix, 2)
+
+	assert.Equal(t, storepb.LabelsToPromLabels(series1), matrix[0].Metric)
+	assert.Equal(t, storepb.LabelsToPromLabels(series2), matrix[1].Metric)
+	assert.Equal(t, series1Samples, matrix[0].Points)
+	assert.Equal(t, series2Samples, matrix[1].Points)
 }
 
 type blocksStoreSetMock struct {

pkg/querier/duplicates_test.go
@@ -109,7 +109,7 @@ func runPromQLAndGetJSONResult(t *testing.T, query string, ts client.TimeSeries,
 }
 
 type testQueryable struct {
-	ts *timeSeriesSeriesSet
+	ts storage.SeriesSet
 }
 
 func (t *testQueryable) Querier(ctx context.Context, mint, maxt int64) (storage.Querier, error) {
@@ -117,7 +117,7 @@ func (t *testQueryable) Querier(ctx context.Context, mint, maxt int64) (storage.
 }
 
 type testQuerier struct {
-	ts *timeSeriesSeriesSet
+	ts storage.SeriesSet
 }
 
 func (m testQuerier) Select(_ bool, sp *storage.SelectHints, matchers ...*labels.Matcher) (storage.SeriesSet, storage.Warnings, error) {
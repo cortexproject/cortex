e64d6a27bf5cef02f37ab086b300ca086330b77e
Joe Elliott <number101010@gmail.com>
2020-07-07 09:09:36 -0400

    Added yaml tags and descriptions
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- pkg/chunk/aws/s3_storage_client.go
+++ pkg/chunk/aws/s3_storage_client.go
@@ -46,14 +46,14 @@ type S3Config struct {
 	S3ForcePathStyle bool
 
 	BucketNames     string
-	Endpoint        string
-	Region          string
-	AccessKeyID     string
-	SecretAccessKey string
-	Insecure        bool
-	HTTPConfig      HTTPConfig
-	SSEEncryption   bool
-	PutUserMetadata map[string]*string
+	Endpoint        string             `yaml:"endpoint"`
+	Region          string             `yaml:"region"`
+	AccessKeyID     string             `yaml:"access_key_id"`
+	SecretAccessKey string             `yaml:"secret_access_key"`
+	Insecure        bool               `yaml:"insecure"`
+	SSEEncryption   bool               `yaml:"sse_encryption"`
+	PutUserMetadata map[string]*string `yaml:"put_user_metadata"`
+	HTTPConfig      HTTPConfig         `yaml:"http_config"`
 }
 
 // HTTPConfig stores the http.Transport configuration
@@ -74,6 +74,16 @@ func (cfg *S3Config) RegisterFlagsWithPrefix(prefix string, f *flag.FlagSet) {
 		"If only region is specified as a host, proper endpoint will be deduced. Use inmemory:///<bucket-name> to use a mock in-memory implementation.")
 	f.BoolVar(&cfg.S3ForcePathStyle, prefix+"s3.force-path-style", false, "Set this to `true` to force the request to use path-style addressing.")
 	f.StringVar(&cfg.BucketNames, prefix+"s3.buckets", "", "Comma separated list of bucket names to evenly distribute chunks over. Overrides any buckets specified in s3.url flag")
+
+	f.StringVar(&cfg.Endpoint, prefix+"s3.endpoint", "", "S3 Endpoint to connect to.")
+	f.StringVar(&cfg.Region, prefix+"s3.region", "", "AWS region to use.")
+	f.StringVar(&cfg.AccessKeyID, prefix+"s3.access-key-id", "", "AWS Access Key ID")
+	f.StringVar(&cfg.SecretAccessKey, prefix+"s3.secret-access-key", "", "AWS Secret Access Key")
+	f.BoolVar(&cfg.SSEEncryption, prefix+"s3.sse-encryption", false, "Enable AES256 AWS Server Side Encryption")
+
+	f.DurationVar(&cfg.HTTPConfig.IdleConnTimeout, prefix+"s3.http.idle-conn-timeout", 90*time.Second, "The maximum amount of time an idle connection will be held open.")
+	f.DurationVar(&cfg.HTTPConfig.ResponseHeaderTimeout, prefix+"s3.http.response-header-timeout", 0, "If non-zero, specifies the amount of time to wait for a server's response headers after fully writing the request.")
+	f.BoolVar(&cfg.HTTPConfig.InsecureSkipVerify, prefix+"s3.http.insecure-skip-verify", false, "Set to false to skip verifying the certificate chain and hostname.")
 }
 
 type S3ObjectClient struct {

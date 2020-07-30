97bd88dd0aee55a929bba54655e83e880211745e
Wing924 <weihe924stephen@gmail.com>
2020-07-30 12:06:03 +0900

    fix
    Signed-off-by: Wing924 <weihe924stephen@gmail.com>

--- pkg/util/grpc/encoding/snappy/snappy_test.go
+++ pkg/util/grpc/encoding/snappy/snappy_test.go
@@ -66,6 +66,6 @@ func BenchmarkSnappyDecompress(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		r, _ := c.Decompress(reader)
 		_, _ = ioutil.ReadAll(r)
-		reader.Seek(0, io.SeekStart)
+		_, _ = reader.Seek(0, io.SeekStart)
 	}
 }

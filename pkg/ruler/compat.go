764bf00775ea5ae43d202b6947eb2e78a8cd230c
Marco Pracucci <marco@pracucci.com>
2020-08-12 17:04:16 +0200

    Reverted logic to pass userID to pusherAppender just for clarity
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- pkg/ruler/compat.go
+++ pkg/ruler/compat.go
@@ -26,6 +26,7 @@ type pusherAppender struct {
 	pusher  Pusher
 	labels  []labels.Labels
 	samples []client.Sample
+	userID  string
 }
 
 func (a *pusherAppender) Add(l labels.Labels, t int64, v float64) (uint64, error) {
@@ -44,15 +45,13 @@ func (a *pusherAppender) AddFast(_ uint64, _ int64, _ float64) error {
 func (a *pusherAppender) Commit() error {
 	// Since a.pusher is distributor, client.ReuseSlice will be called in a.pusher.Push.
 	// We shouldn't call client.ReuseSlice here.
-	_, err := a.pusher.Push(a.ctx, client.ToWriteRequest(a.labels, a.samples, nil, client.RULE))
-	a.ctx = nil
+	_, err := a.pusher.Push(user.InjectOrgID(a.ctx, a.userID), client.ToWriteRequest(a.labels, a.samples, nil, client.RULE))
 	a.labels = nil
 	a.samples = nil
 	return err
 }
 
 func (a *pusherAppender) Rollback() error {
-	a.ctx = nil
 	a.labels = nil
 	a.samples = nil
 	return nil
@@ -61,6 +60,7 @@ func (a *pusherAppender) Rollback() error {
 // PusherAppendable fulfills the storage.Appendable interface for prometheus manager
 type PusherAppendable struct {
 	pusher Pusher
+	userID string
 }
 
 // Appender returns a storage.Appender
@@ -68,6 +68,7 @@ func (t *PusherAppendable) Appender(ctx context.Context) storage.Appender {
 	return &pusherAppender{
 		ctx:    ctx,
 		pusher: t.pusher,
+		userID: t.userID,
 	}
 }
 
@@ -102,7 +103,7 @@ func DefaultTenantManagerFactory(
 		reg prometheus.Registerer,
 	) *rules.Manager {
 		return rules.NewManager(&rules.ManagerOptions{
-			Appendable:      &PusherAppendable{pusher: p},
+			Appendable:      &PusherAppendable{pusher: p, userID: userID},
 			Queryable:       q,
 			QueryFunc:       engineQueryFunc(engine, q, cfg.EvaluationDelay),
 			Context:         user.InjectOrgID(ctx, userID),

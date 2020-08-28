c99a9cb57aa716487577294fa17147f5825b43eb
Peter Štibraný <peter.stibrany@grafana.com>
2020-08-28 12:23:00 +0200

    Search for tables now always goes through all configured periods, skipping unknown schemas.
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>

--- tools/blocksconvert/scanner/scanner.go
+++ tools/blocksconvert/scanner/scanner.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"flag"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
@@ -22,6 +21,8 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/thanos-io/thanos/pkg/objstore"
 
+	"github.com/cortexproject/cortex/pkg/chunk"
+	"github.com/cortexproject/cortex/pkg/chunk/storage"
 	"github.com/cortexproject/cortex/pkg/util/services"
 	"github.com/cortexproject/cortex/tools/blocksconvert"
 )
@@ -52,56 +53,27 @@ func (cfg *Config) RegisterFlags(f *flag.FlagSet) {
 type Scanner struct {
 	services.Service
 
-	cfg          Config
+	cfg        Config
+	storageCfg storage.Config
+
 	bucketPrefix string
-	indexReader  IndexReader
+	bucket       objstore.Bucket
+
+	logger log.Logger
+	reg    prometheus.Registerer
 
 	series       prometheus.Counter
 	openFiles    prometheus.Gauge
 	indexEntries *prometheus.CounterVec
-	logger       log.Logger
 
-	tablePeriod time.Duration
-
-	table       string
-	tablePrefix string
-	bucket      objstore.Bucket
-	ignored     *regexp.Regexp
+	schema  chunk.SchemaConfig
+	ignored *regexp.Regexp
 }
 
 func NewScanner(cfg Config, scfg blocksconvert.SharedConfig, l log.Logger, reg prometheus.Registerer) (*Scanner, error) {
-	bigTable := scfg.StorageConfig.GCPStorageConfig
-
-	if bigTable.Project == "" || bigTable.Instance == "" {
-		return nil, errors.New("missing BigTable configuration")
-	}
-
-	tablePrefix := ""
-	tablePeriod := time.Duration(0)
-	if cfg.TableName == "" {
-		err := scfg.SchemaConfig.Load()
-		if err != nil {
-			return nil, errors.Wrap(err, "no table name provided, and schema failed to load")
-		}
-
-		for _, c := range scfg.SchemaConfig.Configs {
-			if c.IndexTables.Period%(24*time.Hour) != 0 {
-				return nil, errors.Errorf("invalid index table period: %v", c.IndexTables.Period)
-			}
-
-			if c.Schema != "v9" && c.Schema != "v10" && c.Schema != "v11" {
-				return nil, errors.Errorf("unsupported schema version: %v", c.Schema)
-			}
-
-			if tablePrefix == "" {
-				tablePrefix = c.IndexTables.Prefix
-				tablePeriod = c.IndexTables.Period
-			} else if tablePrefix != c.IndexTables.Prefix {
-				return nil, errors.Errorf("multiple index table prefixes found in schema: %v, %v", tablePrefix, c.IndexTables.Prefix)
-			} else if tablePeriod != c.IndexTables.Period {
-				return nil, errors.Errorf("multiple index table periods found in schema: %v, %v", tablePeriod, c.IndexTables.Period)
-			}
-		}
+	err := scfg.SchemaConfig.Load()
+	if err != nil {
+		return nil, errors.Wrap(err, "no table name provided, and schema failed to load")
 	}
 
 	if cfg.OutputDirectory == "" {
@@ -126,22 +98,19 @@ func NewScanner(cfg Config, scfg blocksconvert.SharedConfig, l log.Logger, reg p
 		ignoredUserRegex = re
 	}
 
-	err := os.MkdirAll(cfg.OutputDirectory, os.FileMode(0700))
-	if err != nil {
+	if err := os.MkdirAll(cfg.OutputDirectory, os.FileMode(0700)); err != nil {
 		return nil, errors.Wrapf(err, "failed to create new output directory %s", cfg.OutputDirectory)
 	}
 
 	s := &Scanner{
 		cfg:          cfg,
-		indexReader:  newBigtableIndexReader(bigTable.Project, bigTable.Instance, l, reg),
-		table:        cfg.TableName,
-		tablePrefix:  tablePrefix,
-		tablePeriod:  tablePeriod,
+		schema:       scfg.SchemaConfig,
+		storageCfg:   scfg.StorageConfig,
 		logger:       l,
 		bucket:       bucketClient,
 		bucketPrefix: scfg.BucketPrefix,
-
-		ignored: ignoredUserRegex,
+		reg:          reg,
+		ignored:      ignoredUserRegex,
 
 		series: promauto.With(reg).NewCounter(prometheus.CounterOpts{
 			Name: "scanner_series_written_total",
@@ -164,69 +133,81 @@ func NewScanner(cfg Config, scfg blocksconvert.SharedConfig, l log.Logger, reg p
 }
 
 func (s *Scanner) running(ctx context.Context) error {
-	var tables []string
-	if s.table == "" {
-		// Use table prefix to discover tables to scan.
-		tableNames, err := s.indexReader.IndexTableNames(ctx)
-		if err != nil {
-			return err
-		}
+	allTables := []tableToProcess{}
 
-		tables = findTables(s.logger, tableNames, s.tablePrefix, s.tablePeriod)
-		sort.Sort(sort.Reverse(sort.StringSlice(tables)))
-
-		level.Info(s.logger).Log("msg", fmt.Sprintf("found %d tables to scan", len(tables)), "prefix", s.tablePrefix, "period", s.tablePeriod)
-		if s.cfg.TablesLimit > 0 && len(tables) > s.cfg.TablesLimit {
-			level.Info(s.logger).Log("msg", "applied tables limit", "limit", s.cfg.TablesLimit)
-			tables = tables[:s.cfg.TablesLimit]
-		}
-	} else {
-		tables = []string{s.table}
-	}
-
-	for _, t := range tables {
-		tableProcessedFile := filepath.Join(s.cfg.OutputDirectory, t+".processed")
-
-		if shouldSkipOperationBecauseFileExists(tableProcessedFile) {
-			level.Info(s.logger).Log("msg", "skipping table because it was already scanned", "table", t)
+	for ix, c := range s.schema.Configs {
+		if c.Schema != "v9" && c.Schema != "v10" && c.Schema != "v11" {
+			level.Warn(s.logger).Log("msg", "skipping unsupported schema version", "version", c.Schema, "schemaFrom", c.From.String())
 			continue
 		}
 
-		dir := filepath.Join(s.cfg.OutputDirectory, t)
-		level.Info(s.logger).Log("msg", "scanning table", "table", t, "output", dir)
-
-		err := scanSingleTable(ctx, s.indexReader, t, dir, s.cfg.Concurrency, s.openFiles, s.series, s.ignored, s.indexEntries)
-		if err != nil {
-			return errors.Wrapf(err, "failed to scan table %s and generate plan files", t)
+		if c.IndexTables.Period%(24*time.Hour) != 0 {
+			level.Warn(s.logger).Log("msg", "skipping invalid index table period", "period", c.IndexTables.Period, "schemaFrom", c.From.String())
+			continue
 		}
 
-		err = verifyPlanFiles(ctx, dir, s.logger)
-		if err != nil {
-			return errors.Wrap(err, "failed to verify plans")
-		}
+		var reader IndexReader
+		switch c.IndexType {
+		case "gcp", "gcp-columnkey", "bigtable", "bigtable-hashed":
+			bigTable := s.storageCfg.GCPStorageConfig
 
-		if s.bucket != nil {
-			level.Info(s.logger).Log("msg", "uploading generated plan files for table", "table", t, "source", dir)
-
-			err := objstore.UploadDir(ctx, s.logger, s.bucket, dir, s.bucketPrefix)
-			if err != nil {
-				return errors.Wrapf(err, "failed to upload plan files for table %s to bucket", t)
+			if bigTable.Project == "" || bigTable.Instance == "" {
+				level.Error(s.logger).Log("msg", "cannot scan BigTable, missing configuration", "schemaFrom", c.From.String())
+				continue
 			}
 
-			level.Info(s.logger).Log("msg", "uploaded generated files for table", "table", t)
-			if !s.cfg.KeepFiles {
-				if err := os.RemoveAll(dir); err != nil {
-					return errors.Wrapf(err, "failed to delete uploaded plan files for table %s", t)
-				}
+			reader = newBigtableIndexReader(bigTable.Project, bigTable.Instance, s.logger, s.reg)
+		default:
+			level.Warn(s.logger).Log("msg", "unsupported index type", "type", c.IndexType, "schemaFrom", c.From.String())
+			continue
+		}
+
+		toTimestamp := int64(0)
+		if ix < len(s.schema.Configs)-1 {
+			toTimestamp = s.schema.Configs[ix+1].From.Unix()
+		}
+
+		level.Info(s.logger).Log("msg", "scanning for schema tables", "schemaFrom", c.From.String(), "prefix", c.IndexTables.Prefix, "period", c.IndexTables.Period)
+		tables, err := s.findTablesToProcess(ctx, reader, c.From.Unix(), toTimestamp, c.IndexTables)
+		if err != nil {
+			return errors.Wrapf(err, "finding tables for schema %s", c.From.String())
+		}
+
+		level.Info(s.logger).Log("msg", "found tables", "count", len(tables))
+		allTables = append(allTables, tables...)
+	}
+
+	level.Info(s.logger).Log("msg", "total found tables", "count", len(allTables))
+
+	// Recent tables go first.
+	sort.Slice(allTables, func(i, j int) bool {
+		return allTables[i].start.After(allTables[j].start)
+	})
+
+	if s.cfg.TableName != "" {
+		// Find single table.
+		foundIx := -1
+		for ix := 0; ix < len(allTables); ix++ {
+			if allTables[ix].table == s.cfg.TableName {
+				foundIx = ix
+				break
 			}
 		}
-
-		err = ioutil.WriteFile(tableProcessedFile, []byte("Finished on "+time.Now().String()+"\n"), 0600)
-		if err != nil {
-			return errors.Wrapf(err, "failed to create file %s", tableProcessedFile)
+		if foundIx >= 0 {
+			allTables = allTables[foundIx : foundIx+1]
+		} else {
+			level.Error(s.logger).Log("msg", "specified table not found", "table", s.cfg.TableName)
+			allTables = nil
 		}
+	} else if s.cfg.TablesLimit > 0 && len(allTables) > s.cfg.TablesLimit {
+		level.Info(s.logger).Log("msg", "applied tables limit", "limit", s.cfg.TablesLimit)
+		allTables = allTables[:s.cfg.TablesLimit]
+	}
 
-		level.Info(s.logger).Log("msg", "done scanning table", "table", t)
+	for _, t := range allTables {
+		if err := s.processTable(ctx, t.table, t.reader); err != nil {
+			return errors.Wrapf(err, "failed to process table %s", t.table)
+		}
 	}
 
 	// All good, just wait until context is done, to avoid restarts.
@@ -235,56 +216,106 @@ func (s *Scanner) running(ctx context.Context) error {
 	return nil
 }
 
+type tableToProcess struct {
+	table  string
+	reader IndexReader
+	start  time.Time
+}
+
+func (s *Scanner) findTablesToProcess(ctx context.Context, indexReader IndexReader, fromUnixTimestamp, toUnixTimestamp int64, tablesConfig chunk.PeriodicTableConfig) ([]tableToProcess, error) {
+	tables, err := indexReader.IndexTableNames(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var result []tableToProcess
+
+	for _, t := range tables {
+		if !strings.HasPrefix(t, tablesConfig.Prefix) {
+			continue
+		}
+
+		var tp tableToProcess
+		if tablesConfig.Period == 0 {
+			tp = tableToProcess{
+				table:  t,
+				reader: indexReader,
+				start:  time.Unix(fromUnixTimestamp, 0),
+			}
+		} else {
+			p, err := strconv.ParseInt(t[len(tablesConfig.Prefix):], 10, 64)
+			if err != nil {
+				level.Warn(s.logger).Log("msg", "failed to parse period index of table", "table", t)
+				continue
+			}
+
+			start := time.Unix(p*int64(tablesConfig.Period/time.Second), 0)
+			tp = tableToProcess{
+				table:  t,
+				reader: indexReader,
+				start:  start,
+			}
+		}
+
+		if tp.start.Unix() >= fromUnixTimestamp && (toUnixTimestamp <= 0 || tp.start.Unix() < toUnixTimestamp) {
+			result = append(result, tp)
+		}
+	}
+
+	return result, nil
+}
+
+func (s *Scanner) processTable(ctx context.Context, table string, indexReader IndexReader) error {
+	tableProcessedFile := filepath.Join(s.cfg.OutputDirectory, table+".processed")
+
+	if shouldSkipOperationBecauseFileExists(tableProcessedFile) {
+		level.Info(s.logger).Log("msg", "skipping table because it was already scanned", "table", table)
+		return nil
+	}
+
+	dir := filepath.Join(s.cfg.OutputDirectory, table)
+	level.Info(s.logger).Log("msg", "scanning table", "table", table, "output", dir)
+
+	err := scanSingleTable(ctx, indexReader, table, dir, s.cfg.Concurrency, s.openFiles, s.series, s.ignored, s.indexEntries)
+	if err != nil {
+		return errors.Wrapf(err, "failed to scan table %s and generate plan files", table)
+	}
+
+	err = verifyPlanFiles(ctx, dir, s.logger)
+	if err != nil {
+		return errors.Wrap(err, "failed to verify plans")
+	}
+
+	if s.bucket != nil {
+		level.Info(s.logger).Log("msg", "uploading generated plan files for table", "table", table, "source", dir)
+
+		err := objstore.UploadDir(ctx, s.logger, s.bucket, dir, s.bucketPrefix)
+		if err != nil {
+			return errors.Wrapf(err, "failed to upload plan files for table %s to bucket", table)
+		}
+
+		level.Info(s.logger).Log("msg", "uploaded generated files for table", "table", table)
+		if !s.cfg.KeepFiles {
+			if err := os.RemoveAll(dir); err != nil {
+				return errors.Wrapf(err, "failed to delete uploaded plan files for table %s", table)
+			}
+		}
+	}
+
+	err = ioutil.WriteFile(tableProcessedFile, []byte("Finished on "+time.Now().String()+"\n"), 0600)
+	if err != nil {
+		return errors.Wrapf(err, "failed to create file %s", tableProcessedFile)
+	}
+
+	level.Info(s.logger).Log("msg", "done processing table", "table", table)
+	return nil
+}
+
 func shouldSkipOperationBecauseFileExists(file string) bool {
 	// If file exists, we should skip the operation.
 	_, err := os.Stat(file)
 	// Any error (including ErrNotExists) indicates operation should continue.
 	return err == nil
-}
-
-func findTables(logger log.Logger, tableNames []string, prefix string, period time.Duration) []string {
-	type table struct {
-		name        string
-		periodIndex int64
-	}
-
-	var tables []table
-
-	for _, t := range tableNames {
-		if !strings.HasPrefix(t, prefix) {
-			continue
-		}
-
-		if period == 0 {
-			tables = append(tables, table{
-				name:        t,
-				periodIndex: 0,
-			})
-			continue
-		}
-
-		p, err := strconv.ParseInt(t[len(prefix):], 10, 64)
-		if err != nil {
-			level.Warn(logger).Log("msg", "failed to parse period index of table", "table", t)
-			continue
-		}
-
-		tables = append(tables, table{
-			name:        t,
-			periodIndex: p,
-		})
-	}
-
-	sort.Slice(tables, func(i, j int) bool {
-		return tables[i].periodIndex < tables[j].periodIndex
-	})
-
-	var out []string
-	for _, t := range tables {
-		out = append(out, t.name)
-	}
-
-	return out
 }
 
 func scanSingleTable(ctx context.Context, indexReader IndexReader, tableName string, outDir string, concurrency int, openFiles prometheus.Gauge, series prometheus.Counter, ignored *regexp.Regexp, indexEntries *prometheus.CounterVec) error {

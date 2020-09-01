c6a5af5ee0f9bf9ddffe4cd090a15304ccd05e79
gotjosh <josue@grafana.com>
2020-09-01 17:23:01 +0100

    Start an Alertmanager when it is requested if we have a fallback config (#3073)
    * Start an Alertmanager when the first request is received
    
    When a fallback configuration is specified.
    
    Signed-off-by: gotjosh <josue@grafana.com>
    
    * Review feedback:
    
    - Simplify AM starting logic
    - Don't upload any config to object store
    
    Signed-off-by: gotjosh <josue@grafana.com>
    
    * Address review comments
    
    Signed-off-by: gotjosh <josue@grafana.com>

--- pkg/alertmanager/multitenant_test.go
+++ pkg/alertmanager/multitenant_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
@@ -48,7 +49,8 @@ func (m *mockAlertStore) GetAlertConfig(ctx context.Context, user string) (alert
 }
 
 func (m *mockAlertStore) SetAlertConfig(ctx context.Context, cfg alerts.AlertConfigDesc) error {
-	return fmt.Errorf("not implemented")
+	m.configs[cfg.User] = cfg
+	return nil
 }
 
 func (m *mockAlertStore) DeleteAlertConfig(ctx context.Context, user string) error {
@@ -239,5 +241,70 @@ func TestAlertmanager_ServeHTTP(t *testing.T) {
 
 	resp = w.Result()
 	body, _ = ioutil.ReadAll(resp.Body)
+	require.Equal(t, "the Alertmanager is not configured\n", string(body))
+}
+
+func TestAlertmanager_ServeHTTPWithFallbackConfig(t *testing.T) {
+	mockStore := &mockAlertStore{
+		configs: map[string]alerts.AlertConfigDesc{},
+	}
+
+	externalURL := flagext.URLValue{}
+	err := externalURL.Set("http://localhost:8080/alertmanager")
+	require.NoError(t, err)
+
+	tempDir, err := ioutil.TempDir(os.TempDir(), "alertmanager")
+	require.NoError(t, err)
+	defer os.RemoveAll(tempDir)
+
+	fallbackCfg := `
+global:
+  smtp_smarthost: 'localhost:25'
+  smtp_from: 'youraddress@example.org'
+route:
+  receiver: example-email
+receivers:
+  - name: example-email
+    email_configs:
+    - to: 'youraddress@example.org'
+`
+
+	// Create the Multitenant Alertmanager.
+	am := createMultitenantAlertmanager(&MultitenantAlertmanagerConfig{
+		ExternalURL: externalURL,
+		DataDir:     tempDir,
+	}, nil, nil, mockStore, log.NewNopLogger(), nil)
+	am.fallbackConfig = fallbackCfg
+
+	// Request when no user configuration is present.
+	req := httptest.NewRequest("GET", externalURL.String()+"/api/v1/status", nil)
+	req.Header.Add(user.OrgIDHeaderName, "user1")
+	w := httptest.NewRecorder()
+
+	am.ServeHTTP(w, req)
+
+	resp := w.Result()
+
+	// It succeeds and the Alertmanager is started
+	require.Equal(t, http.StatusOK, resp.StatusCode)
+	require.Len(t, am.alertmanagers, 1)
+	require.True(t, am.alertmanagers["user1"].IsActive())
+
+	// Even after a poll it does not pause your Alertmanager
+	err = am.updateConfigs()
+	require.NoError(t, err)
+
+	require.True(t, am.alertmanagers["user1"].IsActive())
+	require.Len(t, am.alertmanagers, 1)
+
+	// Pause the alertmanager
+	am.alertmanagers["user1"].Pause()
+
+	// Request when user configuration is paused.
+	w = httptest.NewRecorder()
+	am.ServeHTTP(w, req)
+
+	resp = w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
 	require.Equal(t, "the Alertmanager is not configured\n", string(body))
 }

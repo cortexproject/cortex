7d2c6b6a646d75b792184cd8d6920c9e14c6e509
Peter Štibraný <peter.stibrany@grafana.com>
2020-09-10 11:20:59 +0200

    Implemented structure to keep track of active series.
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>

---
+++ pkg/storage/tsdb/active_series.go
@@ -0,0 +1,168 @@
+package tsdb
+
+import (
+	"math"
+	"sync"
+	"time"
+
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/pkg/labels"
+
+	"github.com/cortexproject/cortex/pkg/ingester/client"
+	"github.com/cortexproject/cortex/pkg/util"
+)
+
+const (
+	numActiveSeriesStripes = 512
+)
+
+// ActiveSeries is keeping track of recently active series for a single tenant.
+type ActiveSeries struct {
+	stripes [numActiveSeriesStripes]activeSeriesStripe
+}
+
+// activeSeriesStripe holds a subset of the series timestamps for a single tenant.
+type activeSeriesStripe struct {
+	refsMu        sync.Mutex
+	refs          map[model.Fingerprint][]activeSeriesEntry
+	oldestEntryTs int64 // Unix nanoseconds. Only used and updated by purge. Zero = unknown.
+	active        int   // Number of active entries in this stripe. Only decreased during purge.
+}
+
+// activeSeriesEntry holds a timestamp for single series.
+type activeSeriesEntry struct {
+	lbs labels.Labels
+	ts  int64 // Unix nanoseconds
+}
+
+func NewActiveSeries() *ActiveSeries {
+	c := &ActiveSeries{}
+
+	// Stripes are pre-allocated so that we only read on them and no lock is required.
+	for i := 0; i < numActiveSeriesStripes; i++ {
+		c.stripes[i].refs = map[model.Fingerprint][]activeSeriesEntry{}
+	}
+
+	return c
+}
+
+// Updates series timestamp to 'now'. Function is called to make a copy of labels if entry doesn't exist yet.
+func (c *ActiveSeries) UpdateSeries(series labels.Labels, now time.Time, labelsCopy func(labels.Labels) labels.Labels) {
+	fp := client.Fingerprint(series)
+	stripeID := util.HashFP(fp) % numActiveSeriesStripes
+
+	c.stripes[stripeID].updateSeriesTimestamp(now, series, fp, labelsCopy)
+}
+
+// Purge removes expired entries from the cache. This function should be called
+// periodically to avoid memory leaks.
+func (c *ActiveSeries) Purge(keepUntil time.Time) {
+	for s := 0; s < numRefCacheStripes; s++ {
+		c.stripes[s].purge(keepUntil)
+	}
+}
+
+func (c *ActiveSeries) Active() int {
+	total := 0
+	for s := 0; s < numRefCacheStripes; s++ {
+		total += c.stripes[s].getActive()
+	}
+	return total
+}
+
+func (s *activeSeriesStripe) updateSeriesTimestamp(now time.Time, series labels.Labels, fp model.Fingerprint, labelsCopy func(labels.Labels) labels.Labels) {
+	s.refsMu.Lock()
+	defer s.refsMu.Unlock()
+
+	nanos := now.UnixNano()
+	if nanos < s.oldestEntryTs {
+		s.oldestEntryTs = 0
+	}
+
+	// Check if already exists within the entries.
+	for ix, entry := range s.refs[fp] {
+		if !labels.Equal(entry.lbs, series) {
+			continue
+		}
+
+		if nanos > entry.ts {
+			entry.ts = nanos
+			s.refs[fp][ix] = entry
+		}
+		return
+	}
+
+	// The entry doesn't exist, so we have to add a new one.
+	entry := activeSeriesEntry{
+		lbs: labelsCopy(series),
+		ts:  nanos,
+	}
+	s.refs[fp] = append(s.refs[fp], entry)
+	s.active++
+}
+
+func (s *activeSeriesStripe) purge(keepUntil time.Time) {
+	s.refsMu.Lock()
+	defer s.refsMu.Unlock()
+
+	keepUntilNanos := keepUntil.UnixNano()
+	if s.oldestEntryTs > 0 && keepUntilNanos <= s.oldestEntryTs {
+		// Nothing to do.
+		return
+	}
+
+	s.active = 0
+
+	oldest := int64(math.MaxInt64)
+	for fp, entries := range s.refs {
+		// Since we do expect very few fingerprint collisions, we
+		// have an optimized implementation for the common case.
+		if len(entries) == 1 {
+			if entries[0].ts < keepUntilNanos {
+				delete(s.refs, fp)
+				continue
+			}
+
+			s.active++
+			if entries[0].ts < oldest {
+				oldest = entries[0].ts
+			}
+			continue
+		}
+
+		// We have more entries, which means there's a collision,
+		// so we have to iterate over the entries.
+		for i := 0; i < len(entries); {
+			if entries[i].ts < keepUntilNanos {
+				entries = append(entries[:i], entries[i+1:]...)
+			} else {
+				if entries[i].ts < oldest {
+					oldest = entries[i].ts
+				}
+
+				i++
+			}
+		}
+
+		// Either update or delete the entries in the map
+		if cnt := len(entries); cnt == 0 {
+			delete(s.refs, fp)
+		} else {
+			s.active += cnt
+			s.refs[fp] = entries
+		}
+	}
+
+	if oldest == math.MaxInt64 {
+		s.oldestEntryTs = 0
+	} else {
+		s.oldestEntryTs = oldest
+	}
+}
+
+func (s *activeSeriesStripe) getActive() int {
+	s.refsMu.Lock()
+	defer s.refsMu.Unlock()
+
+	return s.active
+}
---
+++ pkg/storage/tsdb/active_series_test.go
@@ -0,0 +1,219 @@
+package tsdb
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/prometheus/prometheus/pkg/labels"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/cortexproject/cortex/pkg/ingester/client"
+)
+
+func copyFn(l labels.Labels) labels.Labels { return l }
+
+func TestActiveSeries_UpdateSeries(t *testing.T) {
+	ls1 := []labels.Label{{Name: "a", Value: "1"}}
+	ls2 := []labels.Label{{Name: "a", Value: "2"}}
+
+	c := NewActiveSeries()
+	assert.Equal(t, 0, c.Active())
+
+	c.UpdateSeries(ls1, time.Now(), copyFn)
+	assert.Equal(t, 1, c.Active())
+
+	c.UpdateSeries(ls1, time.Now(), copyFn)
+	assert.Equal(t, 1, c.Active())
+
+	c.UpdateSeries(ls2, time.Now(), copyFn)
+	assert.Equal(t, 2, c.Active())
+}
+
+func TestActiveSeries_ShouldCorrectlyHandleFingerprintCollisions(t *testing.T) {
+	metric := labels.NewBuilder(labels.FromStrings("__name__", "logs"))
+	ls1 := metric.Set("_", "ypfajYg2lsv").Labels()
+	ls2 := metric.Set("_", "KiqbryhzUpn").Labels()
+
+	require.True(t, client.Fingerprint(ls1) == client.Fingerprint(ls2))
+
+	c := NewActiveSeries()
+	c.UpdateSeries(ls1, time.Now(), copyFn)
+	c.UpdateSeries(ls2, time.Now(), copyFn)
+
+	assert.Equal(t, 2, c.Active())
+}
+
+func TestActiveSeries_Purge(t *testing.T) {
+	series := [][]labels.Label{
+		{{Name: "a", Value: "1"}},
+		{{Name: "a", Value: "2"}},
+		// The two following series have the same Fingerprint
+		{{Name: "_", Value: "ypfajYg2lsv"}, {Name: "__name__", Value: "logs"}},
+		{{Name: "_", Value: "KiqbryhzUpn"}, {Name: "__name__", Value: "logs"}},
+	}
+
+	// Run the same test for increasing TTL values
+	for ttl := 0; ttl < len(series); ttl++ {
+		c := NewActiveSeries()
+
+		for i := 0; i < len(series); i++ {
+			c.UpdateSeries(series[i], time.Unix(int64(i), 0), copyFn)
+		}
+
+		c.Purge(time.Unix(int64(ttl+1), 0))
+		// call purge twice, just to hit "quick" path. It doesn't really do anything.
+		c.Purge(time.Unix(int64(ttl+1), 0))
+
+		exp := len(series) - (ttl + 1)
+		assert.Equal(t, exp, c.Active())
+	}
+}
+
+func TestActiveSeries_PurgeOpt(t *testing.T) {
+	metric := labels.NewBuilder(labels.FromStrings("__name__", "logs"))
+	ls1 := metric.Set("_", "ypfajYg2lsv").Labels()
+	ls2 := metric.Set("_", "KiqbryhzUpn").Labels()
+
+	c := NewActiveSeries()
+
+	now := time.Now()
+	c.UpdateSeries(ls1, now.Add(-2*time.Minute), copyFn)
+	c.UpdateSeries(ls2, now, copyFn)
+	c.Purge(now)
+
+	assert.Equal(t, 1, c.Active())
+
+	c.UpdateSeries(ls1, now.Add(-1*time.Minute), copyFn)
+	c.UpdateSeries(ls2, now, copyFn)
+	c.Purge(now)
+
+	assert.Equal(t, 1, c.Active())
+
+	// This will *not* update the series, since there is already newer timestamp.
+	c.UpdateSeries(ls2, now.Add(-1*time.Minute), copyFn)
+	c.Purge(now)
+
+	assert.Equal(t, 1, c.Active())
+}
+
+var activeSeriesTestGoroutines = []int{50, 100, 500}
+
+func BenchmarkActiveSeriesTest_single_label(b *testing.B) {
+	const seriesCount = 1e5
+
+	series := make([]labels.Labels, seriesCount)
+
+	for s := 0; s < len(series); s++ {
+		series[s] = labels.Labels{
+			{Name: "a", Value: strconv.Itoa(s)},
+		}
+	}
+
+	for _, num := range activeSeriesTestGoroutines {
+		b.Run(fmt.Sprintf("%d", num), func(b *testing.B) {
+			benchmarkActiveSeriesConcurrency(b, series, num)
+		})
+	}
+}
+
+func benchmarkActiveSeriesConcurrency(b *testing.B, series []labels.Labels, goroutines int) {
+	c := NewActiveSeries()
+
+	fn := func(wg *sync.WaitGroup, start chan struct{}, step int) {
+		defer wg.Done()
+		<-start
+
+		for i := 0; i < b.N; i++ {
+			now := time.Now()
+
+			for s := 0; s < len(series); s += step {
+				c.UpdateSeries(series[s], now, copyFn)
+			}
+		}
+	}
+
+	start := make(chan struct{})
+	wg := &sync.WaitGroup{}
+	for i := 0; i < goroutines; i++ {
+		wg.Add(1)
+		go fn(wg, start, 1+(i%10))
+	}
+
+	b.ResetTimer()
+	close(start)
+	wg.Wait()
+
+	require.Equal(b, len(series), c.Active())
+}
+
+func BenchmarkActiveSeries_UpdateSeries(b *testing.B) {
+	const numSeries = 10000
+
+	c := NewActiveSeries()
+
+	// Prepare series
+	series := [numSeries]labels.Labels{}
+	for s := 0; s < numSeries; s++ {
+		series[s] = labels.Labels{{Name: "a", Value: strconv.Itoa(s)}}
+	}
+
+	now := time.Now()
+
+	b.ResetTimer()
+	for i := 0; i < int(math.Ceil(float64(b.N)/numSeries)); i++ {
+		for s := 0; s < numSeries; s++ {
+			c.UpdateSeries(series[s], now, copyFn)
+		}
+	}
+}
+
+func BenchmarkActiveSeries_Purge_once(b *testing.B) {
+	benchmarkPurge(b, false)
+}
+
+func BenchmarkActiveSeries_Purge_twice(b *testing.B) {
+	benchmarkPurge(b, true)
+}
+
+func benchmarkPurge(b *testing.B, twice bool) {
+	const numSeries = 10000
+	const numExpiresSeries = numSeries / 25
+
+	now := time.Now()
+	c := NewActiveSeries()
+
+	series := [numSeries]labels.Labels{}
+	for s := 0; s < numSeries; s++ {
+		series[s] = labels.Labels{{Name: "a", Value: strconv.Itoa(s)}}
+	}
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+
+		// Prepare series
+		for ix, s := range series {
+			if ix < numExpiresSeries {
+				c.UpdateSeries(s, now.Add(-time.Minute), copyFn)
+			} else {
+				c.UpdateSeries(s, now, copyFn)
+			}
+		}
+
+		assert.Equal(b, numSeries, c.Active())
+		b.StartTimer()
+
+		// Purge everything
+		c.Purge(now)
+		assert.Equal(b, numSeries-numExpiresSeries, c.Active())
+
+		if twice {
+			c.Purge(now)
+			assert.Equal(b, numSeries-numExpiresSeries, c.Active())
+		}
+	}
+}

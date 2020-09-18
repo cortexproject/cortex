e45d6cb26efd7981a8f1070cacfd3631794e2ea3
Peter Štibraný <peter.stibrany@grafana.com>
2020-09-18 11:49:35 +0200

    Don't remove two spaces at the end of line.
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>

--- tools/cleanup-white-noise.sh
+++ tools/cleanup-white-noise.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 SED_BIN=${SED_BIN:-sed}
 
-${SED_BIN} -i 's/[ \t]*$//' "$@"
+# Remove one, or three or more spaces, but not two, as that is used by Markdown for line breaks.
+${SED_BIN} -i 's/[^[:space:]][[:space:]]{1}$//' "$@"
+${SED_BIN} -i 's/[^[:space:]][[:space:]]{3,}$//' "$@"

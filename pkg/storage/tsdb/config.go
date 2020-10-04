19dbe9e5c2015b8de82808eeccddaafd0d7eee67
Jacob Lisi <jacob.t.lisi@gmail.com>
2020-10-04 16:55:43 -0400

    fix: rename Middleware to Middlewares
    Signed-off-by: Jacob Lisi <jacob.t.lisi@gmail.com>

--- pkg/storage/tsdb/config.go
+++ pkg/storage/tsdb/config.go
@@ -68,7 +68,7 @@ type BucketConfig struct {
 
 	// Not used internally, meant to allow callers to wrap Buckets
 	// created using this config
-	Middleware []func(objstore.Bucket) objstore.Bucket `yaml:"-"`
+	Middlewares []func(objstore.Bucket) objstore.Bucket `yaml:"-"`
 }
 
 // BlocksStorageConfig holds the config information for the blocks storage.

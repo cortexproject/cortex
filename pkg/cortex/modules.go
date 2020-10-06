8ec8912beae83b5a410618944675702a00e391e6
Julien Pivotto <roidelapluie@inuits.eu>
2020-10-06 21:06:14 +0200

    Error if the store gateway is run on an invalid engine
    This pull request returns an explicit error if the user asks to start
    the store gateway but omits to set the storage type to `blocks`.
    
    The previous behaviour was to silently ignore the -target=store-gateway
    argument.
    
    Note that this does not apply when target is `all`.
    
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- pkg/cortex/modules.go
+++ pkg/cortex/modules.go
@@ -593,6 +593,9 @@ func (t *Cortex) initCompactor() (serv services.Service, err error) {
 
 func (t *Cortex) initStoreGateway() (serv services.Service, err error) {
 	if t.Cfg.Storage.Engine != storage.StorageEngineBlocks {
+		if t.Cfg.Target != All {
+			return nil, fmt.Errorf("storage engine must be set to blocks to enable the store-gateway")
+		}
 		return nil, nil
 	}
 

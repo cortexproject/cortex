7a6749fb4fc333f7235ce81651736fefabba7955
Michel Hollands <michel.hollands@grafana.com>
2020-10-09 11:12:08 +0100

    Rename grpc metadata key to be more unique and not binary
    Signed-off-by: Michel Hollands <michel.hollands@grafana.com>

--- pkg/util/extract_forwarded.go
+++ pkg/util/extract_forwarded.go
@@ -7,7 +7,7 @@ import (
 )
 
 // ipAddressesKey is key for the GRPC metadata where the IP addresses are stored
-const ipAddressesKey = "x-forwarded-for-bin"
+const ipAddressesKey = "extract-forwarded-x-forwarded-for"
 
 // GetSourceIPsFromOutgoingCtx extracts the source field from the GRPC context
 func GetSourceIPsFromOutgoingCtx(ctx context.Context) string {

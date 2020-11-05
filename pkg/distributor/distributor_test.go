4385980c48579b1b1a1f3beff9fb2e01054ec5db
Michel Hollands <michel.hollands@grafana.com>
2020-11-05 09:12:42 +0000

    Add lock around mockIngester call at correct place
    Signed-off-by: Michel Hollands <michel.hollands@grafana.com>

--- pkg/distributor/distributor_test.go
+++ pkg/distributor/distributor_test.go
@@ -1283,6 +1283,9 @@ func (i *mockIngester) series() map[uint32]*client.PreallocTimeseries {
 }
 
 func (i *mockIngester) Check(ctx context.Context, in *grpc_health_v1.HealthCheckRequest, opts ...grpc.CallOption) (*grpc_health_v1.HealthCheckResponse, error) {
+	i.Lock()
+	defer i.Unlock()
+
 	i.trackCall("Check")
 
 	return &grpc_health_v1.HealthCheckResponse{}, nil
@@ -1487,9 +1490,6 @@ func (i *mockIngester) MetricsMetadata(ctx context.Context, req *client.MetricsM
 }
 
 func (i *mockIngester) trackCall(name string) {
-	i.Lock()
-	defer i.Unlock()
-
 	if i.calls == nil {
 		i.calls = map[string]int{}
 	}

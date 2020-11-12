5c60b0d451f1bec1801b32769f89b78c1f20a376
Robert Fratto <robertfratto@gmail.com>
2020-11-12 03:06:22 -0500

    Support custom log.Logger instances in spanlogger (#3480)
    * support custom log.Logger instances in spanlogger
    
    Signed-off-by: Robert Fratto <robertfratto@gmail.com>
    
    * Update pkg/util/spanlogger/spanlogger.go
    
    Co-authored-by: Peter Štibraný <peter.stibrany@grafana.com>
    Signed-off-by: Robert Fratto <robertfratto@gmail.com>
    
    * add test, fix bug
    
    Signed-off-by: Robert Fratto <robertfratto@gmail.com>
    
    * ignore errors from logging to test spanlogger
    
    Signed-off-by: Robert Fratto <robertfratto@gmail.com>
    
    Co-authored-by: Peter Štibraný <peter.stibrany@grafana.com>

--- pkg/util/spanlogger/spanlogger_test.go
+++ pkg/util/spanlogger/spanlogger_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/go-kit/kit/log"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
 )
@@ -18,4 +19,33 @@ func TestSpanLogger_Log(t *testing.T) {
 	_ = noSpan.Log("foo")
 	require.Error(t, noSpan.Error(errors.New("err")))
 	require.NoError(t, noSpan.Error(nil))
+}
+
+func TestSpanLogger_CustomLogger(t *testing.T) {
+	var logged [][]interface{}
+	var logger funcLogger = func(keyvals ...interface{}) error {
+		logged = append(logged, keyvals)
+		return nil
+	}
+	span, ctx := NewWithLogger(context.Background(), logger, "test")
+	_ = span.Log("msg", "original spanlogger")
+
+	span = FromContextWithFallback(ctx, log.NewNopLogger())
+	_ = span.Log("msg", "restored spanlogger")
+
+	span = FromContextWithFallback(context.Background(), logger)
+	_ = span.Log("msg", "fallback spanlogger")
+
+	expect := [][]interface{}{
+		{"method", "test", "msg", "original spanlogger"},
+		{"msg", "restored spanlogger"},
+		{"msg", "fallback spanlogger"},
+	}
+	require.Equal(t, expect, logged)
+}
+
+type funcLogger func(keyvals ...interface{}) error
+
+func (f funcLogger) Log(keyvals ...interface{}) error {
+	return f(keyvals...)
 }

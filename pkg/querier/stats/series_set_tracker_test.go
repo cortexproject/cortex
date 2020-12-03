956ce3cae953842eb1618b6bef1ed3ffaf6cc50f
Marco Pracucci <marco@pracucci.com>
2020-12-03 13:06:42 +0100

    Fixed tests
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- pkg/querier/stats/series_set_tracker_test.go
+++ pkg/querier/stats/series_set_tracker_test.go
@@ -5,25 +5,26 @@ import (
 
 	"github.com/prometheus/prometheus/pkg/labels"
 	"github.com/prometheus/prometheus/storage"
+	"github.com/prometheus/prometheus/tsdb/chunkenc"
 	"github.com/stretchr/testify/require"
 
 	"github.com/cortexproject/cortex/pkg/querier/series"
 )
 
-func TestSeriesSetTracker_ShouldTrackStatsOnIterationEnd(t *testing.T) {
+func TestSeriesSetTracker_ShouldTrackStats(t *testing.T) {
 	set := series.NewConcreteSeriesSet([]storage.Series{
-		series.NewEmptySeries(labels.New(labels.Label{
-			Name:  labels.MetricName,
-			Value: "metric_1",
-		})),
-		series.NewEmptySeries(labels.New(labels.Label{
-			Name:  labels.MetricName,
-			Value: "metric_2",
-		})),
-		series.NewEmptySeries(labels.New(labels.Label{
-			Name:  labels.MetricName,
-			Value: "metric_2",
-		})),
+		newMockSeries(
+			labels.New(labels.Label{Name: labels.MetricName, Value: "metric_1"}),
+			[]mockSample{{1, 1}, {2, 4}},
+		),
+		newMockSeries(
+			labels.New(labels.Label{Name: labels.MetricName, Value: "metric_2"}),
+			[]mockSample{{1, 2}, {2, 5}},
+		),
+		newMockSeries(
+			labels.New(labels.Label{Name: labels.MetricName, Value: "metric_2"}),
+			[]mockSample{{1, 3}, {2, 6}},
+		),
 	})
 
 	// Wrap the original series set with the tracker.
@@ -31,13 +32,69 @@ func TestSeriesSetTracker_ShouldTrackStatsOnIterationEnd(t *testing.T) {
 	set = NewSeriesSetTracker(set, &stats)
 
 	for set.Next() {
-		require.Equal(t, int32(0), stats.GetSeries())
+		for it := set.At().Iterator(); it.Next(); {
+			it.At()
+		}
 	}
 
 	require.NoError(t, set.Err())
-	require.Equal(t, int32(3), stats.GetSeries())
+	require.Equal(t, int64(6), stats.GetSamples())
+}
 
-	// Another call to Next() shouldn't cause stats to be double counted.
-	require.False(t, set.Next())
-	require.Equal(t, int32(3), stats.GetSeries())
+type mockSample struct {
+	t int64
+	v float64
+}
+
+type mockSeries struct {
+	labels  labels.Labels
+	samples []mockSample
+}
+
+func newMockSeries(lbls labels.Labels, samples []mockSample) storage.Series {
+	return &mockSeries{
+		labels:  lbls,
+		samples: samples,
+	}
+}
+
+func (s *mockSeries) Iterator() chunkenc.Iterator {
+	return newMockIterator(s.samples)
+}
+
+func (s *mockSeries) Labels() labels.Labels {
+	return s.labels
+}
+
+type mockIterator struct {
+	samples []mockSample
+	idx     int
+}
+
+func newMockIterator(samples []mockSample) chunkenc.Iterator {
+	return &mockIterator{
+		samples: samples,
+		idx:     -1,
+	}
+}
+
+func (i *mockIterator) Next() bool {
+	i.idx++
+	return i.idx < len(i.samples)
+}
+
+func (i *mockIterator) Seek(_ int64) bool {
+	panic("Seek() not implemented")
+}
+
+func (i *mockIterator) At() (int64, float64) {
+	if i.idx < 0 || i.idx >= len(i.samples) {
+		return 0, 0
+	}
+
+	return i.samples[i.idx].t, i.samples[i.idx].v
+}
+
+func (i *mockIterator) Err() error {
+	return nil
 }

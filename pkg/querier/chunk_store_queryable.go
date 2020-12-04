60bc27069b262f308af5d27665a6ee35c315275d
Goutham Veeramachaneni <gouthamve@gmail.com>
2020-12-04 19:14:10 +0100

    Ignore series API lookups to chunks store (#3559)
    Signed-off-by: Goutham Veeramachaneni <gouthamve@gmail.com>

--- pkg/querier/chunk_store_queryable.go
+++ pkg/querier/chunk_store_queryable.go
@@ -43,6 +43,14 @@ func (q *chunkStoreQuerier) Select(_ bool, sp *storage.SelectHints, matchers ...
 	if err != nil {
 		return storage.ErrSeriesSet(err)
 	}
+
+	// We will hit this for /series lookup when -querier.query-store-for-labels-enabled is set.
+	// If we don't skip here, it'll make /series lookups extremely slow as all the chunks will be loaded.
+	// That flag is only to be set with blocks storage engine, and this is a protective measure.
+	if sp == nil || sp.Func == "series" {
+		return storage.EmptySeriesSet()
+	}
+
 	chunks, err := q.store.Get(q.ctx, userID, model.Time(sp.Start), model.Time(sp.End), matchers...)
 	if err != nil {
 		return storage.ErrSeriesSet(err)

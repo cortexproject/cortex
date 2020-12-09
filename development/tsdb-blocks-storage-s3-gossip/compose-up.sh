6e431759484d88185b5f7b7c01b7e9f0749e0e24
Peter Štibraný <peter.stibrany@grafana.com>
2020-12-09 17:10:46 +0100

    Add docker-compose setup using gossip.
    Signed-off-by: Peter Štibraný <peter.stibrany@grafana.com>

---
+++ development/tsdb-blocks-storage-s3-gossip/compose-up.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=$(cd `dirname $0` && pwd)
+
+# -gcflags "all=-N -l" disables optimizations that allow for better run with combination with Delve debugger.
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -mod=vendor -gcflags "all=-N -l" -o ${SCRIPT_DIR}/cortex ${SCRIPT_DIR}/../../cmd/cortex
+docker-compose -f ${SCRIPT_DIR}/docker-compose.yml build distributor
+docker-compose -f ${SCRIPT_DIR}/docker-compose.yml up $@

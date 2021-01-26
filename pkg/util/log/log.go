65e963dbf61036260bf21988682fc271cdfb5457
Kevin Minehart <kmineh0151@gmail.com>
2021-01-26 11:16:55 -0600

    add deprecation warning to pkg/log/log.go
    Signed-off-by: Kevin Minehart <kmineh0151@gmail.com>

--- pkg/util/log/log.go
+++ pkg/util/log/log.go
@@ -10,6 +10,9 @@ import (
 )
 
 var (
+	// Logger is a shared go-kit logger.
+	// TODO: Change all components to take a non-global logger via their constructors.
+	// Prefer accepting a non-global logger as an argument.
 	Logger = kitlog.NewNopLogger()
 )
 

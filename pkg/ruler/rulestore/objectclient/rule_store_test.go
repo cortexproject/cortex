5fd20ed7f436c7c025e62f728f9fd665bbb41d22
Marco Pracucci <marco@pracucci.com>
2021-02-26 16:07:01 +0100

    Fixed comment
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- pkg/ruler/rulestore/objectclient/rule_store_test.go
+++ pkg/ruler/rulestore/objectclient/rule_store_test.go
@@ -1,4 +1,4 @@
 package objectclient
 
 // This is an empty placeholder test file to signal that the tests for RuleStore are in:
-// pkg/ruler/rulestore/bucket_client_test.go
+// pkg/ruler/rulestore/bucketclient/bucket_client_test.go

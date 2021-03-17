5d0976bf8ab897bb82961add446eec96824e896a
Levi Harrison <levisamuelharrison@gmail.com>
2021-03-17 12:23:43 -0400

    Support GetRuleGroup, SetRuleGroup, DeleteRuleGroup, DeleteNamespace for local client
    Signed-off-by: Levi Harrison <levisamuelharrison@gmail.com>

--- pkg/ruler/rulestore/local/local.go
+++ pkg/ruler/rulestore/local/local.go
@@ -8,7 +8,9 @@ import (
 	"path/filepath"
 
 	"github.com/pkg/errors"
+	"github.com/prometheus/prometheus/pkg/rulefmt"
 	promRules "github.com/prometheus/prometheus/rules"
+	"gopkg.in/yaml.v3"
 
 	"github.com/cortexproject/cortex/pkg/ruler/rulespb"
 )
@@ -108,28 +110,79 @@ func (l *Client) LoadRuleGroups(_ context.Context, _ map[string]rulespb.RuleGrou
 
 // GetRuleGroup implements RuleStore
 func (l *Client) GetRuleGroup(ctx context.Context, userID, namespace, group string) (*rulespb.RuleGroupDesc, error) {
-	return nil, errors.New("GetRuleGroup unsupported in rule local store")
+	groups, err := l.loadAllRulesGroupsForUserAndNamespace(ctx, userID, namespace)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, g := range groups {
+		if g.Name == group {
+			return g, nil
+		}
+	}
+
+	return nil, errors.Errorf("group %s not found", group)
 }
 
 // SetRuleGroup implements RuleStore
 func (l *Client) SetRuleGroup(ctx context.Context, userID, namespace string, group *rulespb.RuleGroupDesc) error {
-	return errors.New("SetRuleGroup unsupported in rule local store")
+	groups, err := l.loadAllRulesGroupsForUserAndNamespace(ctx, userID, namespace)
+	if err != nil {
+		return err
+	}
+
+	for i, g := range groups {
+		if g.Name == group.Name {
+			if g == group {
+				// The rule group hasn't changed
+				return nil
+			}
+
+			groups[i] = group
+			return l.saveRuleGroupsForUserAndNamespace(userID, namespace, groups)
+		}
+	}
+
+	groups = append(groups, group)
+
+	return l.saveRuleGroupsForUserAndNamespace(userID, namespace, groups)
 }
 
 // DeleteRuleGroup implements RuleStore
-func (l *Client) DeleteRuleGroup(ctx context.Context, userID, namespace string, group string) error {
-	return errors.New("DeleteRuleGroup unsupported in rule local store")
+func (l *Client) DeleteRuleGroup(ctx context.Context, userID, namespace, group string) error {
+	groups, err := l.loadAllRulesGroupsForUserAndNamespace(ctx, userID, namespace)
+	if err != nil {
+		return err
+	}
+
+	for i, g := range groups {
+		if g.Name == group {
+			groups = append(groups[:i], groups[i+1:]...)
+			return l.saveRuleGroupsForUserAndNamespace(userID, namespace, groups)
+		}
+	}
+
+	return errors.Errorf("group %s not found", group)
 }
 
 // DeleteNamespace implements RulerStore
 func (l *Client) DeleteNamespace(ctx context.Context, userID, namespace string) error {
-	return errors.New("DeleteNamespace unsupported in rule local store")
+	filename := filepath.Join(l.cfg.Directory, userID, namespace)
+	return errors.Wrapf(os.Remove(filename), "error removing rule groups file %s", filename)
 }
 
 func (l *Client) loadAllRulesGroupsForUser(ctx context.Context, userID string) (rulespb.RuleGroupList, error) {
 	var allLists rulespb.RuleGroupList
 
 	root := filepath.Join(l.cfg.Directory, userID)
+
+	_, err := os.Stat(root)
+	if err != nil && !os.IsNotExist(err) {
+		return nil, errors.Wrapf(err, "unable to stat dir %s", root)
+	} else if os.IsNotExist(err) {
+		return rulespb.RuleGroupList{}, nil
+	}
+
 	infos, err := ioutil.ReadDir(root)
 	if err != nil {
 		return nil, errors.Wrapf(err, "unable to read dir %s", root)
@@ -165,6 +218,13 @@ func (l *Client) loadAllRulesGroupsForUser(ctx context.Context, userID string) (
 func (l *Client) loadAllRulesGroupsForUserAndNamespace(_ context.Context, userID string, namespace string) (rulespb.RuleGroupList, error) {
 	filename := filepath.Join(l.cfg.Directory, userID, namespace)
 
+	_, err := os.Stat(filename)
+	if err != nil && !os.IsNotExist(err) {
+		return nil, errors.Wrapf(err, "unable to stat dir %s", filename)
+	} else if os.IsNotExist(err) {
+		return rulespb.RuleGroupList{}, nil
+	}
+
 	rulegroups, allErrors := l.loader.Load(filename)
 	if len(allErrors) > 0 {
 		return nil, errors.Wrapf(allErrors[0], "error parsing %s", filename)
@@ -178,4 +238,27 @@ func (l *Client) loadAllRulesGroupsForUserAndNamespace(_ context.Context, userID
 	}
 
 	return list, nil
+}
+
+func (l *Client) saveRuleGroupsForUserAndNamespace(userID, namespace string, ruleGroups rulespb.RuleGroupList) error {
+	var list []rulefmt.RuleGroup
+	for _, group := range ruleGroups {
+		list = append(list, rulespb.FromProto(group))
+	}
+
+	g := rulefmt.RuleGroups{Groups: list}
+	out, err := yaml.Marshal(g)
+	if err != nil {
+		return errors.Wrap(err, "unable to marshal rule groups")
+	}
+
+	filename := filepath.Join(l.cfg.Directory, userID, namespace)
+	dir := filepath.Dir(filename)
+
+	err = os.MkdirAll(dir, 07777)
+	if err != nil {
+		return errors.Wrapf(err, "error creating rule groups file dir %s", dir)
+	}
+
+	return errors.Wrapf(ioutil.WriteFile(filename, out, 0777), "error writing rule groups file %s", filename)
 }

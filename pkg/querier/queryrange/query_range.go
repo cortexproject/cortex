6f6025fe3808b632e8ee7de0e55a1a030ac5f083
Alvin Lin <alvinlin@amazon.com>
2021-03-25 22:33:14 -0700

    Make comment more precise
    Signed-off-by: Alvin Lin <alvinlin@amazon.com>

--- pkg/querier/queryrange/query_range.go
+++ pkg/querier/queryrange/query_range.go
@@ -388,9 +388,9 @@ func matrixMerge(resps []*PrometheusResponse) []SampleStream {
 }
 
 // sliceSamples assumes given samples are sorted by timestamp in ascending order and
-// return a sub slice whose first element's timestamp is strictly bigger than
-// the given minTs. Empty slice is returned if minTs is bigger than all the timestamps in
-// samples.
+// return a sub slice whose first element's is the smallest timestamp that is strictly
+// bigger than the given minTs. Empty slice is returned if minTs is bigger than all the
+//timestamps in samples.
 func sliceSamples(samples []cortexpb.Sample, minTs int64) []cortexpb.Sample {
 
 	if len(samples) <= 0 || minTs < samples[0].TimestampMs {

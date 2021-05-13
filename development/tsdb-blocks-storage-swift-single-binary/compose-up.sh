d25f6c79e9f46e665f02545f70591e647d974346
Peter Štibraný <peter.stibrany@grafana.com>
2021-05-13 15:23:10 +0200

    Preserve GOARCH, change GOOS only for Cortex build (#4164)
    Signed-off-by: Peter Štibraný <pstibrany@gmail.com>

--- development/tsdb-blocks-storage-swift-single-binary/compose-up.sh
+++ development/tsdb-blocks-storage-swift-single-binary/compose-up.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR=$(cd `dirname $0` && pwd)
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ${SCRIPT_DIR}/cortex ${SCRIPT_DIR}/../../cmd/cortex && \
+CGO_ENABLED=0 GOOS=linux go build -o ${SCRIPT_DIR}/cortex ${SCRIPT_DIR}/../../cmd/cortex && \
 docker-compose -f ${SCRIPT_DIR}/docker-compose.yml build cortex-1 && \
 docker-compose -f ${SCRIPT_DIR}/docker-compose.yml up $@

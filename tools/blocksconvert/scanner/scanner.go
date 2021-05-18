bdb563f899dc3f752bfb79dd7643b61ec697999a
ubcharron <bcharron@ubisoft.com>
2021-05-18 07:57:34 -0400

    Add Cassandra support to blocksconvert (#3795)
    Signed-off-by: Benjamin Charron <benjamin.charron@ubisoft.com>
    
    Co-authored-by: Benjamin Charron <benjamin.charron@ubisoft.com>

--- tools/blocksconvert/scanner/scanner.go
+++ tools/blocksconvert/scanner/scanner.go
@@ -227,6 +227,10 @@ func (s *Scanner) running(ctx context.Context) error {
 			if err != nil {
 				level.Error(s.logger).Log("msg", "cannot scan DynamoDB", "err", err)
 			}
+		case "cassandra":
+			cass := s.storageCfg.CassandraStorageConfig
+
+			reader = newCassandraIndexReader(cass, s.schema, s.logger, s.indexReaderRowsRead, s.indexReaderParsedIndexEntries, s.currentTableRanges, s.currentTableScannedRanges)
 		default:
 			level.Warn(s.logger).Log("msg", "unsupported index type", "type", c.IndexType, "schemaFrom", c.From.String())
 			continue

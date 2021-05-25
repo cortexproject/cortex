0817da36adbd11924dea636dc650320ca22bbfed
Cyril Tovena <cyril.tovena@gmail.com>
2021-05-25 14:03:24 +0200

    Add the ability to disable OpenCensus within GCS client.
    The rationale is that we're pretty biaised about Prometheus instrumentation here and it should be possible
    to disable this if we don't want it at all.
    
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>

--- pkg/chunk/gcp/gcs_object_client.go
+++ pkg/chunk/gcp/gcs_object_client.go
@@ -38,7 +38,7 @@ func (cfg *GCSConfig) RegisterFlagsWithPrefix(prefix string, f *flag.FlagSet) {
 	f.StringVar(&cfg.BucketName, prefix+"gcs.bucketname", "", "Name of GCS bucket. Please refer to https://cloud.google.com/docs/authentication/production for more information about how to configure authentication.")
 	f.IntVar(&cfg.ChunkBufferSize, prefix+"gcs.chunk-buffer-size", 0, "The size of the buffer that GCS client for each PUT request. 0 to disable buffering.")
 	f.DurationVar(&cfg.RequestTimeout, prefix+"gcs.request-timeout", 0, "The duration after which the requests to GCS should be timed out.")
-	f.BoolVar(&cfg.EnableOpenCensus, prefix+"gcs.request-timeout", true, "Enabled OpenCensus (OC) instrumentation for all requests.")
+	f.BoolVar(&cfg.EnableOpenCensus, prefix+"gcs.enable-opencensus", true, "Enabled OpenCensus (OC) instrumentation for all requests.")
 }
 
 // NewGCSObjectClient makes a new chunk.Client that writes chunks to GCS.

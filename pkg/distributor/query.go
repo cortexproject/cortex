cortexproject/cortex

commit 4c56545d7d88976627bd9b42b94557ffdace3c34

Replace validation error with error in query limiter (#4240)
* Replace validation error with error in query limiter

Signed-off-by: Tyler Reid <tyler.reid@grafana.com>

* Change query limiter integration test to check for 500 rather than contained string

Signed-off-by: Tyler Reid <tyler.reid@grafana.com>

pkg/distributor/query.go
@@ -235,12 +235,12 @@ func (d *Distributor) queryIngesterStream(ctx context.Context, userID string, re
 
 			for _, series := range resp.Chunkseries {
 				if limitErr := queryLimiter.AddSeries(series.Labels); limitErr != nil {
-					return nil, limitErr
+					return nil, validation.LimitError(limitErr.Error())
 				}
 			}
 
 			if chunkBytesLimitErr := queryLimiter.AddChunkBytes(resp.ChunksSize()); chunkBytesLimitErr != nil {
-				return nil, chunkBytesLimitErr
+				return nil, validation.LimitError(chunkBytesLimitErr.Error())
 			}
 
 			for _, series := range resp.Timeseries {

integration/querier_test.go
@@ -962,10 +962,9 @@ func TestQueryLimitsWithBlocksStorageRunningInMicroServices(t *testing.T) {
 	res, err = c.Push(series4)
 	require.NoError(t, err)
 	require.Equal(t, 200, res.StatusCode)
-
 	_, err = c.QueryRange("{__name__=~\"series_.+\"}", series1Timestamp, series4Timestamp.Add(1*time.Hour), blockRangePeriod)
 	require.Error(t, err)
-	assert.Contains(t, err.Error(), "max number of series limit")
+	assert.Contains(t, err.Error(), "500")
 }
 
 func TestHashCollisionHandling(t *testing.T) {

pkg/querier/blocks_store_queryable.go
@@ -616,7 +616,7 @@ func (q *blocksStoreQuerier) fetchSeriesFromStores(
 					// Add series fingerprint to query limiter; will return error if we are over the limit
 					limitErr := queryLimiter.AddSeries(cortexpb.FromLabelsToLabelAdapters(s.PromLabels()))
 					if limitErr != nil {
-						return limitErr
+						return validation.LimitError(limitErr.Error())
 					}
 
 					// Ensure the max number of chunks limit hasn't been reached (max == 0 means disabled).
@@ -631,7 +631,7 @@ func (q *blocksStoreQuerier) fetchSeriesFromStores(
 						chunksSize += c.Size()
 					}
 					if chunkBytesLimitErr := queryLimiter.AddChunkBytes(chunksSize); chunkBytesLimitErr != nil {
-						return chunkBytesLimitErr
+						return validation.LimitError(chunkBytesLimitErr.Error())
 					}
 				}
 

pkg/querier/blocks_store_queryable_test.go
@@ -508,7 +508,7 @@ func TestBlocksStoreQuerier_Select(t *testing.T) {
 			},
 			limits:       &blocksStoreLimitsMock{},
 			queryLimiter: limiter.NewQueryLimiter(1, 0),
-			expectedErr:  validation.LimitError(fmt.Sprintf("The query hit the max number of series limit (limit: %d)", 1)),
+			expectedErr:  validation.LimitError(fmt.Sprintf(limiter.ErrMaxSeriesHit, 1)),
 		},
 		"max chunk bytes per query limit hit while fetching chunks": {
 			finderResult: bucketindex.Blocks{

pkg/util/limiter/query_limiter.go
@@ -10,15 +10,14 @@ import (
 
 	"github.com/cortexproject/cortex/pkg/cortexpb"
 	"github.com/cortexproject/cortex/pkg/ingester/client"
-	"github.com/cortexproject/cortex/pkg/util/validation"
 )
 
 type queryLimiterCtxKey struct{}
 
 var (
 	ctxKey              = &queryLimiterCtxKey{}
-	errMaxSeriesHit     = "The query hit the max number of series limit (limit: %d)"
-	ErrMaxChunkBytesHit = "The query hit the aggregated chunks size limit (limit: %d bytes)"
+	ErrMaxSeriesHit     = "the query hit the max number of series limit (limit: %d series)"
+	ErrMaxChunkBytesHit = "the query hit the aggregated chunks size limit (limit: %d bytes)"
 )
 
 type QueryLimiter struct {
@@ -72,7 +71,7 @@ func (ql *QueryLimiter) AddSeries(seriesLabels []cortexpb.LabelAdapter) error {
 	ql.uniqueSeries[fingerprint] = struct{}{}
 	if len(ql.uniqueSeries) > ql.maxSeriesPerQuery {
 		// Format error with max limit
-		return validation.LimitError(fmt.Sprintf(errMaxSeriesHit, ql.maxSeriesPerQuery))
+		return fmt.Errorf(ErrMaxSeriesHit, ql.maxSeriesPerQuery)
 	}
 	return nil
 }
@@ -90,7 +89,7 @@ func (ql *QueryLimiter) AddChunkBytes(chunkSizeInBytes int) error {
 		return nil
 	}
 	if ql.chunkBytesCount.Add(int64(chunkSizeInBytes)) > int64(ql.maxChunkBytesPerQuery) {
-		return validation.LimitError(fmt.Sprintf(ErrMaxChunkBytesHit, ql.maxChunkBytesPerQuery))
+		return fmt.Errorf(ErrMaxChunkBytesHit, ql.maxChunkBytesPerQuery)
 	}
 	return nil
 }

commit 81b15b4a7c5ea1e90110725dd3169f52871e0552

Fixed regression introduced in #4240
Signed-off-by: Marco Pracucci <marco@pracucci.com>

pkg/distributor/query.go
@@ -245,7 +245,7 @@ func (d *Distributor) queryIngesterStream(ctx context.Context, userID string, re
 
 			for _, series := range resp.Timeseries {
 				if limitErr := queryLimiter.AddSeries(series.Labels); limitErr != nil {
-					return nil, limitErr
+					return nil, validation.LimitError(limitErr.Error())
 				}
 			}
 

integration/querier_test.go
@@ -962,9 +962,10 @@ func TestQueryLimitsWithBlocksStorageRunningInMicroServices(t *testing.T) {
 	res, err = c.Push(series4)
 	require.NoError(t, err)
 	require.Equal(t, 200, res.StatusCode)
+
 	_, err = c.QueryRange("{__name__=~\"series_.+\"}", series1Timestamp, series4Timestamp.Add(1*time.Hour), blockRangePeriod)
 	require.Error(t, err)
-	assert.Contains(t, err.Error(), "500")
+	assert.Contains(t, err.Error(), "max number of series limit")
 }
 
 func TestHashCollisionHandling(t *testing.T) {
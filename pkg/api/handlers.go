20f8931014bc9f3271380884f8eadee8de83ae96
Christian Simon <simon@swine.de>
2021-06-22 14:43:54 +0100

    chore: Define interface api.Distributor (#4297)
    This allows reusing the API handler while swapping out the Ddistributor
    implementation.
    
    Signed-off-by: Christian Simon <simon@swine.de>

--- pkg/api/handlers.go
+++ pkg/api/handlers.go
@@ -23,7 +23,6 @@ import (
 	"github.com/weaveworks/common/middleware"
 
 	"github.com/cortexproject/cortex/pkg/chunk/purger"
-	"github.com/cortexproject/cortex/pkg/distributor"
 	"github.com/cortexproject/cortex/pkg/querier"
 	"github.com/cortexproject/cortex/pkg/querier/stats"
 	"github.com/cortexproject/cortex/pkg/util"
@@ -160,7 +159,7 @@ func NewQuerierHandler(
 	queryable storage.SampleAndChunkQueryable,
 	exemplarQueryable storage.ExemplarQueryable,
 	engine *promql.Engine,
-	distributor *distributor.Distributor,
+	distributor Distributor,
 	tombstonesLoader *purger.TombstonesLoader,
 	reg prometheus.Registerer,
 	logger log.Logger,

1ac1f0abe7fe111b045291ab2d0d689a2d8a58ad
Cyril Tovena <cyril.tovena@gmail.com>
2021-09-07 22:23:21 +0200

    Fixes a panic in the query-tee. (#4465)
    * Fixes a panic in the query-tee.
    
    This was happening because of a race.
    I've change the test suite to cover the code and the race detector shows it up.
    
    The panic:
    
    ```
    panic: runtime error: index out of range [1] with length 1
    
    goroutine 26118 [running]:
    github.com/cortexproject/cortex/tools/querytee.(*ProxyEndpoint).executeBackendRequests(0xc0003160c0, 0xc0002c2600, 0xc0004c44e0)
    	/__w/cortex/cortex/tools/querytee/proxy_endpoint.go:122 +0x5a5
    created by github.com/cortexproject/cortex/tools/querytee.(*ProxyEndpoint).ServeHTTP
    	/__w/cortex/cortex/tools/querytee/proxy_endpoint.go:57 +0x25b
    ```
    
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>
    
    * update changelog.
    
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>
    Signed-off-by: Alvin Lin <alvinlin@amazon.com>

--- tools/querytee/proxy_test.go
+++ tools/querytee/proxy_test.go
@@ -17,8 +17,12 @@ import (
 )
 
 var testRoutes = []Route{
-	{Path: "/api/v1/query", RouteName: "api_v1_query", Methods: []string{"GET"}, ResponseComparator: nil},
+	{Path: "/api/v1/query", RouteName: "api_v1_query", Methods: []string{"GET"}, ResponseComparator: &testComparator{}},
 }
+
+type testComparator struct{}
+
+func (testComparator) Compare(expected, actual []byte) error { return nil }
 
 func Test_NewProxy(t *testing.T) {
 	cfg := ProxyConfig{}
@@ -154,6 +158,10 @@ func Test_Proxy_RequestsForwarding(t *testing.T) {
 				PreferredBackend:   strconv.Itoa(testData.preferredBackendIdx),
 				ServerServicePort:  0,
 				BackendReadTimeout: time.Second,
+			}
+
+			if len(backendURLs) == 2 {
+				cfg.CompareResponses = true
 			}
 
 			p, err := NewProxy(cfg, log.NewNopLogger(), testRoutes, nil)

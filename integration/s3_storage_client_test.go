a71dff8304558bc54568e35544d1096dfeaef503
Marco Pracucci <marco@pracucci.com>
2021-11-02 19:08:00 +0100

    Disable S3 SSE integration tests (#4545)
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- integration/s3_storage_client_test.go
+++ integration/s3_storage_client_test.go
@@ -94,6 +94,11 @@ func TestS3Client(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			switch tt.name {
+			case "config-with-deprecated-sse", "config-with-sse-s3":
+				t.Skip("TODO: Issue #4543")
+			}
+
 			client, err := s3.NewS3ObjectClient(tt.cfg)
 
 			require.NoError(t, err)

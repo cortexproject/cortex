624863f1dbd3f951679b5fe1f55bd36f24d5d022
Arve Knudsen <arve.knudsen@gmail.com>
2022-01-13 19:05:12 +0100

    Reintroduce pkg/util/test, in place of dskit/test (#4618)
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>
    Signed-off-by: Alvin Lin <alvinlin@amazon.com>

--- pkg/ring/basic_lifecycler_test.go
+++ pkg/ring/basic_lifecycler_test.go
@@ -7,13 +7,13 @@ import (
 
 	"github.com/go-kit/log"
 	"github.com/grafana/dskit/services"
-	"github.com/grafana/dskit/test"
 	"github.com/prometheus/client_golang/prometheus/testutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/cortexproject/cortex/pkg/ring/kv"
 	"github.com/cortexproject/cortex/pkg/ring/kv/consul"
+	"github.com/cortexproject/cortex/pkg/util/test"
 )
 
 const (

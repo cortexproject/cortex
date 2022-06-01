36657f5ee954aeb5d06cbfc673b26cf784d5c946
Alvin Lin <alvinlin@amazon.com>
2022-05-31 20:07:28 -0700

    Fix TestNotifierSendsUserIDHeader
    Signed-off-by: Alvin Lin <alvinlin@amazon.com>

--- pkg/ruler/ruler_test.go
+++ pkg/ruler/ruler_test.go
@@ -275,10 +275,10 @@ func TestNotifierSendsUserIDHeader(t *testing.T) {
 
 	// Ensure we have metrics in the notifier.
 	assert.NoError(t, prom_testutil.GatherAndCompare(manager.registry.(*prometheus.Registry), strings.NewReader(`
-		# HELP cortex_prometheus_notifications_dropped_total Total number of alerts dropped due to errors when sending to Alertmanager.
-		# TYPE cortex_prometheus_notifications_dropped_total counter
-		cortex_prometheus_notifications_dropped_total{user="1"} 0
-	`), "cortex_prometheus_notifications_dropped_total"))
+		# HELP prometheus_notifications_dropped_total Total number of alerts dropped due to errors when sending to Alertmanager.
+		# TYPE prometheus_notifications_dropped_total counter
+		prometheus_notifications_dropped_total 0
+	`), "prometheus_notifications_dropped_total"))
 }
 
 func TestRuler_Rules(t *testing.T) {

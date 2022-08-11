b1c62f50008078332c9389b3c5bdb56b79283023
Alan Protasio <approtas@amazon.com>
2022-08-11 00:22:38 -0700

    remove ingester v2 (#4824)
    Signed-off-by: Alan Protasio <approtas@amazon.com>
    
    Signed-off-by: Alan Protasio <approtas@amazon.com>

--- pkg/ingester/flush.go
+++ pkg/ingester/flush.go
@@ -7,11 +7,11 @@ import (
 // Flush triggers a flush of all the chunks and closes the flush queues.
 // Called from the Lifecycler as part of the ingester shutdown.
 func (i *Ingester) Flush() {
-	i.v2LifecyclerFlush()
+	i.lifecyclerFlush()
 }
 
 // FlushHandler triggers a flush of all in memory chunks.  Mainly used for
 // local testing.
 func (i *Ingester) FlushHandler(w http.ResponseWriter, r *http.Request) {
-	i.v2FlushHandler(w, r)
+	i.flushHandler(w, r)
 }

f617e45dbcba453d9433aee643cd48256cdeb203
Zachary Speaks <zspeaks@amazon.com>
2022-08-25 15:40:01 -0400

    Added header decoding to frontend_processor.go for when scheduler service is not being used
    Signed-off-by: Zachary Speaks <zspeaks@amazon.com>

--- pkg/querier/worker/frontend_processor.go
+++ pkg/querier/worker/frontend_processor.go
@@ -95,7 +95,7 @@ func (fp *frontendProcessor) process(c frontendv1pb.Frontend_ProcessClient) erro
 			// and cancel the query.  We don't actually handle queries in parallel
 			// here, as we're running in lock step with the server - each Recv is
 			// paired with a Send.
-
+			ctx = DecodeHTTPHeadersForLogging(ctx, request.HttpRequest)
 			go fp.runRequest(ctx, request.HttpRequest, request.StatsEnabled, func(response *httpgrpc.HTTPResponse, stats *stats.Stats) error {
 				return c.Send(&frontendv1pb.ClientToFrontend{
 					HttpResponse: response,

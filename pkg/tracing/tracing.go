b452503c6f487b79804dfbc137db73bed455df24
Alan Protasio <approtas@amazon.com>
2022-09-01 11:54:33 -0700

    lint
    Signed-off-by: Alan Protasio <approtas@amazon.com>

--- pkg/tracing/tracing.go
+++ pkg/tracing/tracing.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/cortexproject/cortex/pkg/util/tls"
 	"github.com/go-kit/log/level"
 	"github.com/pkg/errors"
 	"github.com/weaveworks/common/tracing"
@@ -24,6 +23,7 @@ import (
 
 	"github.com/cortexproject/cortex/pkg/tracing/migration"
 	util_log "github.com/cortexproject/cortex/pkg/util/log"
+	"github.com/cortexproject/cortex/pkg/util/tls"
 )
 
 const (
@@ -41,7 +41,7 @@ type Otel struct {
 	ExporterType string           `yaml:"exporter_type" json:"exporter_type"`
 	SampleRatio  float64          `yaml:"sample_ratio" json:"sample_ratio"`
 	TLSEnabled   bool             `yaml:"tls_enabled"`
-	Tls          tls.ClientConfig `yaml:"tls"`
+	TLS          tls.ClientConfig `yaml:"tls"`
 }
 
 // RegisterFlags registers flag.
@@ -52,7 +52,7 @@ func (c *Config) RegisterFlags(f *flag.FlagSet) {
 	f.StringVar(&c.Otel.OltpEndpoint, p+".otel.oltp-endpoint", "", "otl collector endpoint that the driver will use to send spans.")
 	f.StringVar(&c.Otel.ExporterType, p+".otel.exporter-type", "", "enhance/modify traces/propagators for specific exporter. If empty, OTEL defaults will apply. Supported values are: `awsxray.`")
 	f.BoolVar(&c.Otel.TLSEnabled, p+".otel.tls-enabled", c.Otel.TLSEnabled, "Enable TLS in the GRPC client. This flag needs to be enabled when any other TLS flag is set. If set to false, insecure connection to gRPC server will be used.")
-	c.Otel.Tls.RegisterFlagsWithPrefix(p+".otel.tls", f)
+	c.Otel.TLS.RegisterFlagsWithPrefix(p+".otel.tls", f)
 }
 
 func (c *Config) Validate() error {
@@ -86,7 +86,7 @@ func SetupTracing(ctx context.Context, name string, c Config) (func(context.Cont
 		}
 
 		if c.Otel.TLSEnabled {
-			tlsConfig, err := c.Otel.Tls.GetTLSConfig()
+			tlsConfig, err := c.Otel.TLS.GetTLSConfig()
 			if err != nil {
 				return nil, errors.Wrap(err, "error creating grpc dial options")
 			}

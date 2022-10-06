bd12a4bd3ce6e21ae9f2e7bd9c90d46149cf06a8
Alan Protasio <approtas@amazon.com>
2022-10-05 17:11:02 -0700

    lint
    Signed-off-by: Alan Protasio <approtas@amazon.com>

--- pkg/tracing/tracing.go
+++ pkg/tracing/tracing.go
@@ -21,7 +21,7 @@ import (
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	"go.opentelemetry.io/otel/semconv/v1.12.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
 
 	"github.com/cortexproject/cortex/pkg/tracing/migration"
 	"github.com/cortexproject/cortex/pkg/tracing/sampler"

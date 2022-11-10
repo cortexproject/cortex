060564d18d74fc209a1a383817b626a35e8bff2e
Alvin Lin <alvinlin@amazon.com>
2022-11-10 08:40:42 -0800

    Upgrade docker engine to latest verison (#4961)
    Signed-off-by: Alvin Lin <alvinlin123@gmail.com>

--- .github/workflows/scripts/install-docker.sh
+++ .github/workflows/scripts/install-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -x
-VER="18.06.3-ce"
+VER="20.10.19"
 curl -L -o /tmp/docker-$VER.tgz https://download.docker.com/linux/static/stable/x86_64/docker-$VER.tgz
 tar -xz -C /tmp -f /tmp/docker-$VER.tgz
 mv /tmp/docker/* /usr/bin

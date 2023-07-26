adce1e02f6520141f56e942b6847abd70fae9b54
Alan Protasio <alanprot@gmail.com>
2023-07-26 12:25:49 -0700

    Backfill lower caches on the multi cache implementation
    Signed-off-by: Alan Protasio <alanprot@gmail.com>

--- pkg/storage/tsdb/multilevel_cache.go
+++ pkg/storage/tsdb/multilevel_cache.go
@@ -30,17 +30,34 @@ func (m *multiLevelCache) StorePostings(blockID ulid.ULID, l labels.Label, v []b
 func (m *multiLevelCache) FetchMultiPostings(ctx context.Context, blockID ulid.ULID, keys []labels.Label) (hits map[labels.Label][]byte, misses []labels.Label) {
 	misses = keys
 	hits = map[labels.Label][]byte{}
-	for _, c := range m.caches {
-		h, m := c.FetchMultiPostings(ctx, blockID, misses)
-		misses = m
+	backfillMap := map[storecache.IndexCache][]map[labels.Label][]byte{}
+	for i, c := range m.caches {
+		backfillMap[c] = []map[labels.Label][]byte{}
+		h, mi := c.FetchMultiPostings(ctx, blockID, misses)
+		misses = mi
 
 		for label, bytes := range h {
 			hits[label] = bytes
 		}
+
+		if i > 0 {
+			backfillMap[m.caches[i-1]] = append(backfillMap[m.caches[i-1]], h)
+		}
+
 		if len(misses) == 0 {
 			break
 		}
 	}
+
+	defer func() {
+		for cache, hit := range backfillMap {
+			for _, values := range hit {
+				for l, b := range values {
+					cache.StorePostings(blockID, l, b)
+				}
+			}
+		}
+	}()
 
 	return hits, misses
 }
@@ -59,8 +76,11 @@ func (m *multiLevelCache) StoreExpandedPostings(blockID ulid.ULID, matchers []*l
 }
 
 func (m *multiLevelCache) FetchExpandedPostings(ctx context.Context, blockID ulid.ULID, matchers []*labels.Matcher) ([]byte, bool) {
-	for _, c := range m.caches {
+	for i, c := range m.caches {
 		if d, h := c.FetchExpandedPostings(ctx, blockID, matchers); h {
+			if i > 0 {
+				m.caches[i-1].StoreExpandedPostings(blockID, matchers, d)
+			}
 			return d, h
 		}
 	}
@@ -84,17 +104,35 @@ func (m *multiLevelCache) StoreSeries(blockID ulid.ULID, id storage.SeriesRef, v
 func (m *multiLevelCache) FetchMultiSeries(ctx context.Context, blockID ulid.ULID, ids []storage.SeriesRef) (hits map[storage.SeriesRef][]byte, misses []storage.SeriesRef) {
 	misses = ids
 	hits = map[storage.SeriesRef][]byte{}
-	for _, c := range m.caches {
-		h, m := c.FetchMultiSeries(ctx, blockID, misses)
-		misses = m
+	backfillMap := map[storecache.IndexCache][]map[storage.SeriesRef][]byte{}
+
+	for i, c := range m.caches {
+		backfillMap[c] = []map[storage.SeriesRef][]byte{}
+		h, miss := c.FetchMultiSeries(ctx, blockID, misses)
+		misses = miss
 
 		for label, bytes := range h {
 			hits[label] = bytes
 		}
+
+		if i > 0 && len(h) > 0 {
+			backfillMap[m.caches[i-1]] = append(backfillMap[m.caches[i-1]], h)
+		}
+
 		if len(misses) == 0 {
 			break
 		}
 	}
+
+	defer func() {
+		for cache, hit := range backfillMap {
+			for _, values := range hit {
+				for m, b := range values {
+					cache.StoreSeries(blockID, m, b)
+				}
+			}
+		}
+	}()
 
 	return hits, misses
 }

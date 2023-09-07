d6ef1f27742d97939191c9e34c2914c25c977556
Alan Protasio <alanprot@gmail.com>
2023-09-07 15:45:30 -0700

    Creating new metric around chunks bytespool
    Signed-off-by: Alan Protasio <alanprot@gmail.com>

--- pkg/storegateway/chunk_bytes_pool.go
+++ pkg/storegateway/chunk_bytes_pool.go
@@ -12,6 +12,8 @@ type chunkBytesPool struct {
 	// Metrics.
 	requestedBytes prometheus.Counter
 	returnedBytes  prometheus.Counter
+
+	poolByteStats *prometheus.CounterVec
 }
 
 func newChunkBytesPool(minBucketSize, maxBucketSize int, maxChunkPoolBytes uint64, reg prometheus.Registerer) (*chunkBytesPool, error) {
@@ -30,6 +32,10 @@ func newChunkBytesPool(minBucketSize, maxBucketSize int, maxChunkPoolBytes uint6
 			Name: "cortex_bucket_store_chunk_pool_returned_bytes_total",
 			Help: "Total bytes returned by the chunk bytes pool.",
 		}),
+		poolByteStats: promauto.With(reg).NewCounterVec(prometheus.CounterOpts{
+			Name: "cortex_bucket_store_chunk_pool_bytes_total",
+			Help: "Total bytes polled by the chunk bytes pool by operation.",
+		}, []string{"operation", "stats"}),
 	}, nil
 }
 
@@ -42,9 +48,15 @@ func (p *chunkBytesPool) Get(sz int) (*[]byte, error) {
 	p.requestedBytes.Add(float64(sz))
 	p.returnedBytes.Add(float64(cap(*buffer)))
 
+	p.poolByteStats.WithLabelValues("Get", "requested").Add(float64(sz))
+	p.poolByteStats.WithLabelValues("Get", "len").Add(float64(len(*buffer)))
+	p.poolByteStats.WithLabelValues("Get", "cap").Add(float64(cap(*buffer)))
+
 	return buffer, err
 }
 
 func (p *chunkBytesPool) Put(b *[]byte) {
+	p.poolByteStats.WithLabelValues("Put", "len").Add(float64(len(*b)))
+	p.poolByteStats.WithLabelValues("Put", "cap").Add(float64(cap(*b)))
 	p.pool.Put(b)
 }

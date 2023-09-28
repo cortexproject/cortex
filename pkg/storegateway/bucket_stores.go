99ef95586e9f0a8e63c96a97dbf13ebc272e8207
Ben Ye <benye@amazon.com>
2023-09-27 17:53:44 -0700

    expose series batch size flag
    Signed-off-by: Ben Ye <benye@amazon.com>
    
    update docs
    
    Signed-off-by: Ben Ye <benye@amazon.com>

--- pkg/storegateway/bucket_stores.go
+++ pkg/storegateway/bucket_stores.go
@@ -572,7 +572,7 @@ func (u *BucketStores) getOrCreateStore(userID string) (*store.BucketStore, erro
 		store.WithIndexCache(u.indexCache),
 		store.WithQueryGate(u.queryGate),
 		store.WithChunkPool(u.chunksPool),
-		store.WithSeriesBatchSize(store.SeriesBatchSize),
+		store.WithSeriesBatchSize(u.cfg.BucketStore.SeriesBatchSize),
 		store.WithBlockEstimatedMaxChunkFunc(func(m thanos_metadata.Meta) uint64 {
 			if m.Thanos.IndexStats.ChunkMaxSize > 0 &&
 				uint64(m.Thanos.IndexStats.ChunkMaxSize) < u.cfg.BucketStore.EstimatedMaxChunkSizeBytes {

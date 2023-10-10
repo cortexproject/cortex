d8230cf6e59aec3dcf4ae5392ed98aaf85a7e6ef
Ben Ye <benye@amazon.com>
2023-10-10 00:20:23 -0700

    check context in multi level cache and add histogram to measure latency
    Signed-off-by: Ben Ye <benye@amazon.com>
    
    add backfill histogram
    
    Signed-off-by: Ben Ye <benye@amazon.com>
    
    update changelog
    
    Signed-off-by: Ben Ye <benye@amazon.com>
    
    lint
    
    Signed-off-by: Ben Ye <benye@amazon.com>

--- pkg/storage/tsdb/index_cache.go
+++ pkg/storage/tsdb/index_cache.go
@@ -205,7 +205,7 @@ func NewIndexCache(cfg IndexCacheConfig, logger log.Logger, registerer prometheu
 		}
 	}
 
-	return newMultiLevelCache(caches...), nil
+	return newMultiLevelCache(registerer, caches...), nil
 }
 
 func newInMemoryIndexCache(cfg InMemoryIndexCacheConfig, logger log.Logger, registerer prometheus.Registerer) (storecache.IndexCache, error) {

5d677f5f69e956629710292a1b3809f49617f42a
Ben Ye <benye@amazon.com>
2023-11-20 18:20:14 +0000

    update docs
    Signed-off-by: Ben Ye <benye@amazon.com>

--- docs/blocks-storage/store-gateway.md
+++ docs/blocks-storage/store-gateway.md
@@ -824,11 +824,13 @@ blocks_storage:
         [enabled_items: <list of string> | default = []]
 
       multilevel:
-        # The maximum number of concurrent asynchronous operations can occur.
+        # The maximum number of concurrent asynchronous operations can occur
+        # when backfilling cache items.
         # CLI flag: -blocks-storage.bucket-store.index-cache.multilevel.max-async-concurrency
         [max_async_concurrency: <int> | default = 50]
 
-        # The maximum number of enqueued asynchronous operations allowed.
+        # The maximum number of enqueued asynchronous operations allowed when
+        # backfilling cache items.
         # CLI flag: -blocks-storage.bucket-store.index-cache.multilevel.max-async-buffer-size
         [max_async_buffer_size: <int> | default = 10000]
 
--- pkg/storage/tsdb/index_cache.go
+++ pkg/storage/tsdb/index_cache.go
@@ -127,8 +127,8 @@ func (cfg *MultiLevelIndexCacheConfig) Validate() error {
 }
 
 func (cfg *MultiLevelIndexCacheConfig) RegisterFlagsWithPrefix(f *flag.FlagSet, prefix string) {
-	f.IntVar(&cfg.MaxAsyncConcurrency, prefix+"max-async-concurrency", 50, "The maximum number of concurrent asynchronous operations can occur.")
-	f.IntVar(&cfg.MaxAsyncBufferSize, prefix+"max-async-buffer-size", 10000, "The maximum number of enqueued asynchronous operations allowed.")
+	f.IntVar(&cfg.MaxAsyncConcurrency, prefix+"max-async-concurrency", 50, "The maximum number of concurrent asynchronous operations can occur when backfilling cache items.")
+	f.IntVar(&cfg.MaxAsyncBufferSize, prefix+"max-async-buffer-size", 10000, "The maximum number of enqueued asynchronous operations allowed when backfilling cache items.")
 }
 
 type InMemoryIndexCacheConfig struct {

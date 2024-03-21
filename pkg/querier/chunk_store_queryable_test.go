941b7ee796575fae77bd8bbfd609e1e21d4f9f0a
Ben Ye <benye@amazon.com>
2024-03-21 00:08:22 -0700

    remove one more test
    Signed-off-by: Ben Ye <benye@amazon.com>

--- pkg/querier/chunk_store_queryable_test.go
+++ pkg/querier/chunk_store_queryable_test.go
@@ -7,49 +7,13 @@ import (
 	"testing"
 	"time"
 
-	"github.com/go-kit/log"
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/model/labels"
-	"github.com/prometheus/prometheus/promql"
 	"github.com/stretchr/testify/require"
-	"github.com/thanos-io/promql-engine/engine"
-	"github.com/thanos-io/promql-engine/logicalplan"
 
 	"github.com/cortexproject/cortex/pkg/chunk"
 	promchunk "github.com/cortexproject/cortex/pkg/chunk/encoding"
 )
-
-func TestChunkQueryable(t *testing.T) {
-	t.Parallel()
-	opts := promql.EngineOpts{
-		Logger:     log.NewNopLogger(),
-		MaxSamples: 1e6,
-		Timeout:    1 * time.Minute,
-	}
-	for _, thanosEngine := range []bool{false, true} {
-		for _, testcase := range testcases {
-			for _, encoding := range encodings {
-				for _, query := range queries {
-					t.Run(fmt.Sprintf("%s/%s/%s/ thanos engine enabled = %t", testcase.name, encoding.name, query.query, thanosEngine), func(t *testing.T) {
-						var queryEngine promql.QueryEngine
-						if thanosEngine {
-							queryEngine = engine.New(engine.Opts{
-								EngineOpts:        opts,
-								LogicalOptimizers: logicalplan.AllOptimizers,
-							})
-						} else {
-							queryEngine = promql.NewEngine(opts)
-						}
-
-						store, from := makeMockChunkStore(t, 24, encoding.e)
-						queryable := newMockStoreQueryable(store, testcase.f)
-						testRangeQuery(t, queryable, queryEngine, from, query)
-					})
-				}
-			}
-		}
-	}
-}
 
 type mockChunkStore struct {
 	chunks []chunk.Chunk

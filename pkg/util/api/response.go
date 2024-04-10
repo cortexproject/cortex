f483eb8289c5597abd3a27fce32a00ebc44f4ea6
Ben Ye <benye@amazon.com>
2024-04-10 08:21:28 -0700

    stash
    Signed-off-by: Ben Ye <benye@amazon.com>

--- pkg/util/api/response.go
+++ pkg/util/api/response.go
@@ -3,6 +3,7 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+	"unsafe"
 
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
@@ -11,19 +12,28 @@ import (
 
 type Response struct {
 	Status    string       `json:"status"`
-	Data      interface{}  `json:"data,omitempty"`
-	ErrorType v1.ErrorType `json:"errorType,omitempty"`
-	Error     string       `json:"error,omitempty"`
+	Data      interface{}  `json:"data"`
+	ErrorType v1.ErrorType `json:"errorType"`
+	Error     string       `json:"error"`
 	Warnings  []string     `json:"warnings,omitempty"`
 }
 
 func RespondError(logger log.Logger, w http.ResponseWriter, errorType v1.ErrorType, msg string, statusCode int) {
-	b, err := json.Marshal(&Response{
-		Status:    "error",
-		ErrorType: errorType,
-		Error:     msg,
-		Data:      nil,
-	})
+	var (
+		res Response
+		b   []byte
+		err error
+	)
+	b = yoloBuf(msg)
+	// Try to deserialize response and see if it is already in Prometheus error format.
+	if err := json.Unmarshal(b, &res); err != nil {
+		b, err = json.Marshal(&Response{
+			Status:    "error",
+			ErrorType: errorType,
+			Error:     msg,
+			Data:      nil,
+		})
+	}
 
 	if err != nil {
 		level.Error(logger).Log("msg", "error marshaling json response", "err", err)
@@ -36,4 +46,9 @@ func RespondError(logger log.Logger, w http.ResponseWriter, errorType v1.ErrorTy
 	if n, err := w.Write(b); err != nil {
 		level.Error(logger).Log("msg", "error writing response", "bytesWritten", n, "err", err)
 	}
+}
+
+// yoloBuf will return an unsafe pointer to a string, as the name yolo.yoloBuf implies use at your own risk.
+func yoloBuf(s string) []byte {
+	return *((*[]byte)(unsafe.Pointer(&s)))
 }

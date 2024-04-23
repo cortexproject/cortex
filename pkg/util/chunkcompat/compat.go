09d7aa89e244e18bb076522f9231b5b5b5a09e9a
Ben Ye <benye@amazon.com>
2024-04-23 01:00:39 -0700

    update
    Signed-off-by: Ben Ye <benye@amazon.com>

--- pkg/util/chunkcompat/compat.go
+++ pkg/util/chunkcompat/compat.go
@@ -2,10 +2,10 @@ package chunkcompat
 
 import (
 	"fmt"
-	"github.com/prometheus/prometheus/storage"
 
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/model/labels"
+	"github.com/prometheus/prometheus/storage"
 	"github.com/prometheus/prometheus/tsdb/chunkenc"
 
 	"github.com/cortexproject/cortex/pkg/chunk"

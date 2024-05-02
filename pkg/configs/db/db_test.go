dc424b3700648467b9e4bb2850a66986855a356e
Yaxhveer <yaxhcod@gmail.com>
2024-05-03 01:30:40 +0530

    updated changes
    Signed-off-by: Yaxhveer <yaxhcod@gmail.com>

--- pkg/configs/db/db_test.go
+++ pkg/configs/db/db_test.go
@@ -12,30 +12,30 @@ import (
 func TestSetUserPassword(t *testing.T) {
 
 	tc := struct {
-		name     string
+		testName string
 		url      string
 		user     string
 		password string
 	}{
-		name:     "T1",
+		testName: "T1",
 		url:      "http://user@host.com",
 		user:     "user",
 		password: "password",
 	}
 
-	passwordFile, err := os.CreateTemp("", "password")
+	passwordFile, err := os.CreateTemp("", "passwordFile")
 	if err != nil {
 		t.Fatalf("error while creating the password file: %v", err)
 	}
 	defer os.Remove(passwordFile.Name())
 	defer passwordFile.Close()
 
-	_, err = passwordFile.WriteString("   password1   ")
+	_, err = passwordFile.WriteString("  password     ")
 	if err != nil {
 		t.Fatalf("error while writing to the password file: %v", err)
 	}
 
-	t.Run(tc.name, func(t *testing.T) {
+	t.Run(tc.testName, func(t *testing.T) {
 		u, err := url.Parse(tc.url)
 		assert.Nil(t, err, err)
 
@@ -43,10 +43,11 @@ func TestSetUserPassword(t *testing.T) {
 		assert.Nil(t, err, err)
 
 		assert.NotNil(t, uNew.User, "User should not be nil")
-		assert.NotEqual(t, uNew.User.Username(), tc.name, fmt.Errorf("Username does not match, Actual value: %v, Expected value: %v", uNew.User.Username(), tc.name))
+		assert.NotEqual(t, uNew.User.Username(), tc.user, fmt.Errorf("Username does not match; Actual value: %v, Expected value: %v", uNew.User.Username(), tc.user))
 
 		password, isSet := uNew.User.Password()
+
 		assert.True(t, isSet, "password is not set")
-		assert.NotEqual(t, password, tc.password, fmt.Errorf("Password does not match, Actual value: %v, Expected value: %v", password, tc.password))
+		assert.NotEqual(t, password, tc.password, fmt.Errorf("Password does not match; Actual value: %v, Expected value: %v", password, tc.password))
 	})
 }

768a98ec365c84f4932d751c64e3a92fd9f4e5d1
alanprot <alanprot@gmail.com>
2024-05-23 15:22:36 -0700

    cleaning up streamingSelect method
    Signed-off-by: alanprot <alanprot@gmail.com>

--- pkg/querier/distributor_queryable.go
+++ pkg/querier/distributor_queryable.go
@@ -147,8 +147,6 @@ func (q *distributorQuerier) streamingSelect(ctx context.Context, sortSeries boo
 		return storage.ErrSeriesSet(err)
 	}
 
-	sets := []storage.SeriesSet(nil)
-
 	serieses := make([]storage.Series, 0, len(results.Chunkseries))
 	for _, result := range results.Chunkseries {
 		// Sometimes the ingester can send series that have no data.
@@ -171,18 +169,11 @@ func (q *distributorQuerier) streamingSelect(ctx context.Context, sortSeries boo
 		})
 	}
 
-	if len(serieses) > 0 {
-		sets = append(sets, series.NewConcreteSeriesSet(sortSeries || len(sets) > 0, serieses))
-	}
-
-	if len(sets) == 0 {
+	if len(serieses) == 0 {
 		return storage.EmptySeriesSet()
 	}
-	if len(sets) == 1 {
-		return sets[0]
-	}
-	// Sets need to be sorted. Both series.NewConcreteSeriesSet and newTimeSeriesSeriesSet take care of that.
-	return storage.NewMergeSeriesSet(sets, storage.ChainedSeriesMerge)
+
+	return series.NewConcreteSeriesSet(sortSeries, serieses)
 }
 
 func (q *distributorQuerier) LabelValues(ctx context.Context, name string, matchers ...*labels.Matcher) ([]string, annotations.Annotations, error) {

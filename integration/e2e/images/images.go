db110ab72931ad513d907e26b1dfae4bf8d173e1
alanprot <alanprot@gmail.com>
2024-05-31 09:37:26 -0700

    Updating minio image on the e2e tests
    Signed-off-by: alanprot <alanprot@gmail.com>

--- integration/e2e/images/images.go
+++ integration/e2e/images/images.go
@@ -8,7 +8,7 @@ package images
 var (
 	Memcached  = "memcached:1.6.1"
 	Redis      = "docker.io/redis:7.0.4-alpine"
-	Minio      = "minio/minio:RELEASE.2021-10-13T00-23-17Z"
+	Minio      = "minio/minio:RELEASE.2024-05-28T17-19-04Z"
 	Consul     = "consul:1.8.4"
 	ETCD       = "gcr.io/etcd-development/etcd:v3.4.7"
 	Prometheus = "quay.io/prometheus/prometheus:v2.51.0"

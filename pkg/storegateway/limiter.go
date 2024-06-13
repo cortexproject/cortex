6700b08a26934dfe5f9e6b244b0b4fcbec9d836d
Justin Jung <jungjust@amazon.com>
2024-06-13 13:09:35 -0700

    Move limiters to a new file
    Signed-off-by: Justin Jung <jungjust@amazon.com>

---
+++ pkg/storegateway/limiter.go
@@ -0,0 +1,121 @@
+package storegateway
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/cortexproject/cortex/pkg/util"
+	"github.com/cortexproject/cortex/pkg/util/validation"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/thanos-io/thanos/pkg/store"
+	"github.com/weaveworks/common/httpgrpc"
+)
+
+type limiter struct {
+	limiter *store.Limiter
+}
+
+func (c *limiter) Reserve(num uint64) error {
+	return c.ReserveWithType(num, 0)
+}
+
+func (c *limiter) ReserveWithType(num uint64, _ store.StoreDataType) error {
+	err := c.limiter.Reserve(num)
+	if err != nil {
+		return httpgrpc.Errorf(http.StatusUnprocessableEntity, err.Error())
+	}
+
+	return nil
+}
+
+type compositeLimiter struct {
+	limiters []store.BytesLimiter
+}
+
+func (c *compositeLimiter) ReserveWithType(num uint64, dataType store.StoreDataType) error {
+	for _, l := range c.limiters {
+		if err := l.ReserveWithType(num, dataType); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+type tokenBucketLimiter struct {
+	podTokenBucket      *util.TokenBucket
+	userTokenBucket     *util.TokenBucket
+	requestTokenBucket  *util.TokenBucket
+	getTokensToRetrieve func(tokens uint64, dataType store.StoreDataType) int64
+}
+
+func (t *tokenBucketLimiter) Reserve(_ uint64) error {
+	return nil
+}
+
+func (t *tokenBucketLimiter) ReserveWithType(num uint64, dataType store.StoreDataType) error {
+	tokensToRetrieve := t.getTokensToRetrieve(num, dataType)
+
+	// check provisioned bucket
+	retrieved := t.requestTokenBucket.Retrieve(tokensToRetrieve)
+	if retrieved {
+		t.userTokenBucket.ForceRetrieve(tokensToRetrieve)
+		t.podTokenBucket.ForceRetrieve(tokensToRetrieve)
+		return nil
+	}
+
+	// if provisioned bucket is not enough, check burst buckets
+	retrieved = t.userTokenBucket.Retrieve(tokensToRetrieve)
+	if !retrieved {
+		return fmt.Errorf("not enough tokens in user bucket")
+	}
+
+	retrieved = t.podTokenBucket.Retrieve(tokensToRetrieve)
+	if !retrieved {
+		t.userTokenBucket.Refund(tokensToRetrieve)
+		return fmt.Errorf("not enough tokens in pod bucket")
+	}
+
+	return nil
+}
+
+func newTokenBucketLimiter(podTokenBucket, userTokenBucket, requestTokenBucket *util.TokenBucket, getTokensToRetrieve func(tokens uint64, dataType store.StoreDataType) int64) *tokenBucketLimiter {
+	return &tokenBucketLimiter{
+		podTokenBucket:      podTokenBucket,
+		userTokenBucket:     userTokenBucket,
+		requestTokenBucket:  requestTokenBucket,
+		getTokensToRetrieve: getTokensToRetrieve,
+	}
+}
+
+func newChunksLimiterFactory(limits *validation.Overrides, userID string) store.ChunksLimiterFactory {
+	return func(failedCounter prometheus.Counter) store.ChunksLimiter {
+		// Since limit overrides could be live reloaded, we have to get the current user's limit
+		// each time a new limiter is instantiated.
+		return &limiter{
+			limiter: store.NewLimiter(uint64(limits.MaxChunksPerQueryFromStore(userID)), failedCounter),
+		}
+	}
+}
+
+func newSeriesLimiterFactory(limits *validation.Overrides, userID string) store.SeriesLimiterFactory {
+	return func(failedCounter prometheus.Counter) store.SeriesLimiter {
+		// Since limit overrides could be live reloaded, we have to get the current user's limit
+		// each time a new limiter is instantiated.
+		return &limiter{
+			limiter: store.NewLimiter(uint64(limits.MaxFetchedSeriesPerQuery(userID)), failedCounter),
+		}
+	}
+}
+
+func newBytesLimiterFactory(limits *validation.Overrides, userID string, podTokenBucket, userTokenBucket *util.TokenBucket, getTokensToRetrieve func(tokens uint64, dataType store.StoreDataType) int64, requestTokenBucketSize int64) store.BytesLimiterFactory {
+	return func(failedCounter prometheus.Counter) store.BytesLimiter {
+		limiters := []store.BytesLimiter{}
+		// Since limit overrides could be live reloaded, we have to get the current user's limit
+		// each time a new limiter is instantiated.
+		limiters = append(limiters, store.NewLimiter(uint64(limits.MaxDownloadedBytesPerRequest(userID)), failedCounter))
+		limiters = append(limiters, newTokenBucketLimiter(podTokenBucket, userTokenBucket, util.NewTokenBucket(requestTokenBucketSize, requestTokenBucketSize, nil), getTokensToRetrieve))
+		return &compositeLimiter{
+			limiters: limiters,
+		}
+	}
+}

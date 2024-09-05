286bb23fe2833f38dbbc697594baba99beea0e4b
Ben Ye <benye@amazon.com>
2024-09-05 18:46:01 +0000

    fix regression of query range result cache unable to parse old cached results
    Signed-off-by: Ben Ye <benye@amazon.com>

--- pkg/querier/tripperware/custom.go
+++ pkg/querier/tripperware/custom.go
@@ -1,28 +1,5 @@
 package tripperware
 
-import (
-	"fmt"
-
-	"github.com/gogo/protobuf/types"
-)
-
-func (e *Extent) ToResponse() (Response, error) {
-	msg, err := types.EmptyAny(e.Response)
-	if err != nil {
-		return nil, err
-	}
-
-	if err := types.UnmarshalAny(e.Response, msg); err != nil {
-		return nil, err
-	}
-
-	resp, ok := msg.(Response)
-	if !ok {
-		return nil, fmt.Errorf("bad cached type")
-	}
-	return resp, nil
-}
-
 func (m *Sample) GetTimestampMs() int64 {
 	if m != nil {
 		if m.Sample != nil {

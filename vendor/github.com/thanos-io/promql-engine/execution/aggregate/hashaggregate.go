65d3c551dacd92983949577ce2499054bb1d4ea6
🌲 Harry 🌊 John 🏔 <johrry@amazon.com>
2025-01-08 13:46:48 -0800

    Update thanos-io/promql-engine to 2414c3d2ab47
    Signed-off-by: 🌲 Harry 🌊 John 🏔 <johrry@amazon.com>

--- vendor/github.com/thanos-io/promql-engine/execution/aggregate/hashaggregate.go
+++ vendor/github.com/thanos-io/promql-engine/execution/aggregate/hashaggregate.go
@@ -220,6 +220,10 @@ func (a *aggregate) initializeTables(ctx context.Context) error {
 }
 
 func (a *aggregate) initializeVectorizedTables(ctx context.Context) ([]aggregateTable, []labels.Labels, error) {
+	// perform initialization of the underlying operator even if we are aggregating the labels away
+	if _, err := a.next.Series(ctx); err != nil {
+		return nil, nil, err
+	}
 	tables, err := newVectorizedTables(a.stepsBatch, a.aggregation)
 	if errors.Is(err, parse.ErrNotSupportedExpr) {
 		return a.initializeScalarTables(ctx)

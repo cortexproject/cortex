2e86c7b211c275fb535cac141bfc3e2ab790e624
Justin Jung <jungjust@amazon.com>
2025-01-20 09:41:04 -0800

    Create partial_data
    Signed-off-by: Justin Jung <jungjust@amazon.com>

---
+++ pkg/querier/partialdata/partia_data.go
@@ -0,0 +1,36 @@
+package partialdata
+
+import (
+	"context"
+	"errors"
+)
+
+const (
+	ctxKey              string = "partialDataCtxKey"
+	partialDataErrorMsg string = "This query result may contain partial data."
+)
+
+type Error struct{}
+
+func (e Error) Error() string {
+	return partialDataErrorMsg
+}
+
+func ContextWithPartialData(ctx context.Context, isEnabled bool) context.Context {
+	if isEnabled {
+		return context.WithValue(ctx, ctxKey, isEnabled)
+	}
+	return ctx
+}
+
+func FromContext(ctx context.Context) bool {
+	o := ctx.Value(ctxKey)
+	if o == nil {
+		return false
+	}
+	return o.(bool)
+}
+
+func ReturnPartialData(err error, isEnabled bool) bool {
+	return isEnabled && errors.As(err, &Error{})
+}

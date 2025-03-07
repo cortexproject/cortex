e14ea8122721cd3a3c78c693c614f6b1fb185a1a
SungJin1212 <tjdwls1201@gmail.com>
2025-03-07 15:23:19 +0900

    Fix flaky TestLoader_ShouldCacheIndexNotFoundOnBackgroundUpdates unit test
    Signed-off-by: SungJin1212 <tjdwls1201@gmail.com>

--- pkg/storage/tsdb/bucketindex/loader_test.go
+++ pkg/storage/tsdb/bucketindex/loader_test.go
@@ -470,23 +470,19 @@ func TestLoader_ShouldCacheIndexNotFoundOnBackgroundUpdates(t *testing.T) {
 	// Delete the bucket index.
 	require.NoError(t, DeleteIndex(ctx, bkt, "user-1", nil))
 
-	// Wait until the next index load attempt occurs.
-	prevLoads := testutil.ToFloat64(loader.loadAttempts)
-	test.Poll(t, 3*time.Second, true, func() interface{} {
-		return testutil.ToFloat64(loader.loadAttempts) > prevLoads
-	})
-
 	// We expect the bucket index is not considered loaded because of the error.
-	assert.NoError(t, testutil.GatherAndCompare(reg, bytes.NewBufferString(`
+	test.Poll(t, 3*time.Second, nil, func() interface{} {
+		return testutil.GatherAndCompare(reg, bytes.NewBufferString(`
 			# HELP cortex_bucket_index_loaded Number of bucket indexes currently loaded in-memory.
 			# TYPE cortex_bucket_index_loaded gauge
 			cortex_bucket_index_loaded 0
 		`),
-		"cortex_bucket_index_loaded",
-	))
+			"cortex_bucket_index_loaded",
+		)
+	})
 
 	// Try to get the index again. We expect no load attempt because the error has been cached.
-	prevLoads = testutil.ToFloat64(loader.loadAttempts)
+	prevLoads := testutil.ToFloat64(loader.loadAttempts)
 	actualIdx, _, err = loader.GetIndex(ctx, "user-1")
 	assert.Equal(t, ErrIndexNotFound, err)
 	assert.Nil(t, actualIdx)

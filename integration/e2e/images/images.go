fe077359cfd680c0adf1578155f6bd8df62d791a
SungJin1212 <tjdwls1201@gmail.com>
2025-03-19 13:09:14 +0900

    Add native histogram query fuzz test
    Signed-off-by: SungJin1212 <tjdwls1201@gmail.com>

--- integration/e2e/images/images.go
+++ integration/e2e/images/images.go
@@ -11,5 +11,5 @@ var (
 	Minio      = "minio/minio:RELEASE.2024-05-28T17-19-04Z"
 	Consul     = "consul:1.8.4"
 	ETCD       = "gcr.io/etcd-development/etcd:v3.4.7"
-	Prometheus = "quay.io/prometheus/prometheus:v2.51.0"
+	Prometheus = "quay.io/prometheus/prometheus:v3.2.1"
 )

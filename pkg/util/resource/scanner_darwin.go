50993e1730a9a5f17ca443abf6b6a686d62a99f3
Justin Jung <jungjust@amazon.com>
2025-04-09 23:10:12 -0700

    move noop scanner to darwin scanner
    Signed-off-by: Justin Jung <jungjust@amazon.com>

--- pkg/util/resource/scanner_darwin.go
+++ pkg/util/resource/scanner_darwin.go
@@ -8,6 +8,12 @@ import (
 	"github.com/cortexproject/cortex/pkg/util/log"
 )
 
+type noopScanner struct{}
+
+func (s *noopScanner) scan() (float64, error) {
+	return 0, nil
+}
+
 func newCPUScanner() (scanner, error) {
 	level.Warn(log.Logger).Log("msg", "CPU scanner not supported in darwin.")
 	return &noopScanner{}, nil

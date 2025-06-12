b8a8d873af7e6151e99cd315834269618ee58e83
Alex Le <leqiyue@amazon.com>
2025-06-12 14:54:56 -0700

    optimize cleaner run time
    Signed-off-by: Alex Le <leqiyue@amazon.com>

--- pkg/storage/bucket/bucket_util_test.go
+++ pkg/storage/bucket/bucket_util_test.go
@@ -2,6 +2,7 @@ package bucket
 
 import (
 	"context"
+	"fmt"
 	"strings"
 	"testing"
 
@@ -24,5 +25,25 @@ func TestDeletePrefix(t *testing.T) {
 	del, err := DeletePrefix(context.Background(), mem, "prefix", log.NewNopLogger())
 	require.NoError(t, err)
 	assert.Equal(t, 4, del)
+	assert.Equal(t, 2, len(mem.Objects()))
+}
+
+func TestDeletePrefixConcurrent(t *testing.T) {
+	mem := objstore.NewInMemBucket()
+
+	require.NoError(t, mem.Upload(context.Background(), "obj", strings.NewReader("hello")))
+	require.NoError(t, mem.Upload(context.Background(), "prefix/1", strings.NewReader("hello")))
+	require.NoError(t, mem.Upload(context.Background(), "prefix/2", strings.NewReader("hello")))
+	require.NoError(t, mem.Upload(context.Background(), "prefix/sub1/3", strings.NewReader("hello")))
+	require.NoError(t, mem.Upload(context.Background(), "prefix/sub2/4", strings.NewReader("hello")))
+	require.NoError(t, mem.Upload(context.Background(), "outside/obj", strings.NewReader("hello")))
+	n := 10000
+	for i := 0; i < n; i++ {
+		require.NoError(t, mem.Upload(context.Background(), fmt.Sprintf("prefix/sub/%d", i), strings.NewReader(fmt.Sprintf("hello%d", i))))
+	}
+
+	del, err := DeletePrefixConcurrent(context.Background(), mem, "prefix", log.NewNopLogger(), 100)
+	require.NoError(t, err)
+	assert.Equal(t, 4+n, del)
 	assert.Equal(t, 2, len(mem.Objects()))
 }

7c7e069acce1b7b5e061ee7e63dd7bab86305d48
Ben Ye <benye@amazon.com>
2025-06-12 11:48:29 -0700

    add unwrap stream interceptor (#6813)
    Signed-off-by: yeya24 <benye@amazon.com>

---
+++ pkg/util/grpcclient/unwrap.go
@@ -0,0 +1,37 @@
+package grpcclient
+
+import (
+	"context"
+	"errors"
+
+	"google.golang.org/grpc"
+)
+
+// unwrapErrorStreamClientInterceptor unwraps errors wrapped by OpenTracingStreamClientInterceptor
+func unwrapErrorStreamClientInterceptor() grpc.StreamClientInterceptor {
+	return func(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, streamer grpc.Streamer, opts ...grpc.CallOption) (grpc.ClientStream, error) {
+		stream, err := streamer(ctx, desc, cc, method, opts...)
+		if err != nil {
+			return nil, err
+		}
+
+		return &unwrapErrorClientStream{
+			ClientStream: stream,
+		}, nil
+	}
+}
+
+type unwrapErrorClientStream struct {
+	grpc.ClientStream
+}
+
+func (s *unwrapErrorClientStream) RecvMsg(m interface{}) error {
+	err := s.ClientStream.RecvMsg(m)
+	if err != nil {
+		// Try to unwrap the error to get the original error
+		if wrappedErr := errors.Unwrap(err); wrappedErr != nil {
+			return wrappedErr
+		}
+	}
+	return err
+}

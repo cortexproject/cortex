ffecf3b4133414cc8bd30eb0ec16632dfb78f931
Alex Le <leqiyue@amazon.com>
2025-06-16 11:42:50 -0700

    Optimize cleaner run time (#6815)
    * optimize cleaner run time
    
    Signed-off-by: Alex Le <leqiyue@amazon.com>
    
    * make old bucket.DeletePrefix concurrent
    
    Signed-off-by: Alex Le <leqiyue@amazon.com>
    
    * added metric to record failure pushing to job channel. updated CHANGELOG. put more logs with timing values
    
    Signed-off-by: Alex Le <leqiyue@amazon.com>
    
    ---------
    
    Signed-off-by: Alex Le <leqiyue@amazon.com>

--- pkg/storage/bucket/bucket_util_test.go
+++ pkg/storage/bucket/bucket_util_test.go
@@ -2,6 +2,7 @@ package bucket
 
 import (
 	"context"
+	"fmt"
 	"strings"
 	"testing"
 
@@ -21,8 +22,28 @@ func TestDeletePrefix(t *testing.T) {
 	require.NoError(t, mem.Upload(context.Background(), "prefix/sub2/4", strings.NewReader("hello")))
 	require.NoError(t, mem.Upload(context.Background(), "outside/obj", strings.NewReader("hello")))
 
-	del, err := DeletePrefix(context.Background(), mem, "prefix", log.NewNopLogger())
+	del, err := DeletePrefix(context.Background(), mem, "prefix", log.NewNopLogger(), 1)
 	require.NoError(t, err)
 	assert.Equal(t, 4, del)
+	assert.Equal(t, 2, len(mem.Objects()))
+}
+
+func TestDeletePrefixConcurrent(t *testing.T) {
+	mem := objstore.NewInMemBucket()
+
+	require.NoError(t, mem.Upload(context.Background(), "obj", strings.NewReader("hello")))
+	require.NoError(t, mem.Upload(context.Background(), "prefix/1", strings.NewReader("hello")))
+	require.NoError(t, mem.Upload(context.Background(), "prefix/2", strings.NewReader("hello")))
+	require.NoError(t, mem.Upload(context.Background(), "prefix/sub1/3", strings.NewReader("hello")))
+	require.NoError(t, mem.Upload(context.Background(), "prefix/sub2/4", strings.NewReader("hello")))
+	require.NoError(t, mem.Upload(context.Background(), "outside/obj", strings.NewReader("hello")))
+	n := 10000
+	for i := 0; i < n; i++ {
+		require.NoError(t, mem.Upload(context.Background(), fmt.Sprintf("prefix/sub/%d", i), strings.NewReader(fmt.Sprintf("hello%d", i))))
+	}
+
+	del, err := DeletePrefix(context.Background(), mem, "prefix", log.NewNopLogger(), 100)
+	require.NoError(t, err)
+	assert.Equal(t, 4+n, del)
 	assert.Equal(t, 2, len(mem.Objects()))
 }

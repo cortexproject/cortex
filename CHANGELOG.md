fbc197ff1c1f14e6b90cf7d378725e46e9d7d56b
Xiaochao Dong <the.xcdong@gmail.com>
2025-06-19 15:08:41 +0800

    Enhance the performance of the frontend JSON codec (#6816)
    * Enhance the performance of the frontend JSON codec
    
    Signed-off-by: Xiaochao Dong (@damnever) <the.xcdong@gmail.com>
    
    * Do not rely on the magical init function
    
    Signed-off-by: Xiaochao Dong (@damnever) <the.xcdong@gmail.com>
    
    ---------
    
    Signed-off-by: Xiaochao Dong (@damnever) <the.xcdong@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -42,6 +42,7 @@
 * [ENHANCEMENT] Parquet Storage: Add some metrics for parquet blocks and converter. #6809 #6821
 * [ENHANCEMENT] Compactor: Optimize cleaner run time. #6815
 * [ENHANCEMENT] Parquet Storage: Allow percentage based dynamic shard size for Parquet Converter. #6817
+* [ENHANCEMENT] Query Frontend: Enhance the performance of the JSON codec. #6816
 * [BUGFIX] Ingester: Avoid error or early throttling when READONLY ingesters are present in the ring #6517
 * [BUGFIX] Ingester: Fix labelset data race condition. #6573
 * [BUGFIX] Compactor: Cleaner should not put deletion marker for blocks with no-compact marker. #6576
--- pkg/querier/tripperware/query.go
+++ pkg/querier/tripperware/query.go
@@ -25,6 +25,7 @@ import (
 	"github.com/prometheus/prometheus/util/jsonutil"
 	"github.com/weaveworks/common/httpgrpc"
 
+	"github.com/cortexproject/cortex/pkg/chunk"
 	"github.com/cortexproject/cortex/pkg/cortexpb"
 	"github.com/cortexproject/cortex/pkg/util/limiter"
 	"github.com/cortexproject/cortex/pkg/util/runutil"
@@ -113,12 +114,8 @@ func decodeSampleStream(ptr unsafe.Pointer, iter *jsoniter.Iterator) {
 	for field := iter.ReadObject(); field != ""; field = iter.ReadObject() {
 		switch field {
 		case "metric":
-			metricString := iter.ReadAny().ToString()
 			lbls := labels.Labels{}
-			if err := json.UnmarshalFromString(metricString, &lbls); err != nil {
-				iter.ReportError("unmarshal SampleStream", err.Error())
-				return
-			}
+			chunk.DecodeLabels(unsafe.Pointer(&lbls), iter)
 			ss.Labels = cortexpb.FromLabelsToLabelAdapters(lbls)
 		case "values":
 			for iter.ReadArray() {
@@ -302,12 +299,8 @@ func encodeSampleStream(ptr unsafe.Pointer, stream *jsoniter.Stream) {
 	stream.WriteObjectStart()
 
 	stream.WriteObjectField(`metric`)
-	lbls, err := cortexpb.FromLabelAdaptersToLabels(ss.Labels).MarshalJSON()
-	if err != nil {
-		stream.Error = err
-		return
-	}
-	stream.SetBuffer(append(stream.Buffer(), lbls...))
+	metric := cortexpb.FromLabelAdaptersToLabels(ss.Labels)
+	chunk.EncodeLabels(unsafe.Pointer(&metric), stream)
 
 	if len(ss.Samples) > 0 {
 		stream.WriteMore()
@@ -343,12 +336,8 @@ func decodeSample(ptr unsafe.Pointer, iter *jsoniter.Iterator) {
 	for field := iter.ReadObject(); field != ""; field = iter.ReadObject() {
 		switch field {
 		case "metric":
-			metricString := iter.ReadAny().ToString()
 			lbls := labels.Labels{}
-			if err := json.UnmarshalFromString(metricString, &lbls); err != nil {
-				iter.ReportError("unmarshal Sample", err.Error())
-				return
-			}
+			chunk.DecodeLabels(unsafe.Pointer(&lbls), iter)
 			ss.Labels = cortexpb.FromLabelsToLabelAdapters(lbls)
 		case "value":
 			ss.Sample = &cortexpb.Sample{}
@@ -368,12 +357,8 @@ func encodeSample(ptr unsafe.Pointer, stream *jsoniter.Stream) {
 	stream.WriteObjectStart()
 
 	stream.WriteObjectField(`metric`)
-	lbls, err := cortexpb.FromLabelAdaptersToLabels(ss.Labels).MarshalJSON()
-	if err != nil {
-		stream.Error = err
-		return
-	}
-	stream.SetBuffer(append(stream.Buffer(), lbls...))
+	metric := cortexpb.FromLabelAdaptersToLabels(ss.Labels)
+	chunk.EncodeLabels(unsafe.Pointer(&metric), stream)
 
 	if ss.Sample != nil {
 		stream.WriteMore()

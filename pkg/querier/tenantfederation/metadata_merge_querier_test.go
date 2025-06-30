7e0ca92b91b8b2bc167aa0a0520206d86cca85b9
SungJin1212 <tjdwls1201@gmail.com>
2025-06-30 13:43:19 +0900

    Implement maxTanant lmit on regex matcher
    Signed-off-by: SungJin1212 <tjdwls1201@gmail.com>

--- pkg/querier/tenantfederation/metadata_merge_querier_test.go
+++ pkg/querier/tenantfederation/metadata_merge_querier_test.go
@@ -169,7 +169,8 @@ func Test_mergeMetadataQuerier_MetricsMetadata_WhenUseRegexResolver(t *testing.T
 	}
 
 	usersScannerConfig := cortex_tsdb.UsersScannerConfig{Strategy: cortex_tsdb.UserScanStrategyList}
-	regexResolver, err := NewRegexResolver(usersScannerConfig, reg, bucketClientFactory, time.Second, log.NewNopLogger())
+	tenantFederationConfig := Config{UserSyncInterval: time.Second}
+	regexResolver, err := NewRegexResolver(usersScannerConfig, tenantFederationConfig, reg, bucketClientFactory, log.NewNopLogger())
 	require.NoError(t, err)
 	tenant.WithDefaultResolver(regexResolver)
 	require.NoError(t, services.StartAndAwaitRunning(context.Background(), regexResolver))

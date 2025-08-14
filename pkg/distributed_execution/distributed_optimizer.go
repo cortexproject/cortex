46f063d178851c6fe8dcb4825ffdd1f72fab0d23
rubywtl <rubyharrisonlee@gmail.com>
2025-08-13 17:00:59 -0700

    add logical plan distributed optimizer to query frontend
    Signed-off-by: rubywtl <rubyharrisonlee@gmail.com>

---
+++ pkg/distributed_execution/distributed_optimizer.go
@@ -0,0 +1,40 @@
+package distributed_execution
+
+import (
+	"fmt"
+
+	"github.com/prometheus/prometheus/util/annotations"
+	"github.com/thanos-io/promql-engine/logicalplan"
+)
+
+// This is a simplified implementation that only handles binary aggregation cases
+// Future versions of the distributed optimizer are expected to:
+// - Support more complex query patterns
+// - Incorporate diverse optimization strategies
+// - Extend support to node types beyond binary operations
+
+type DistributedOptimizer struct{}
+
+func (d *DistributedOptimizer) Optimize(root logicalplan.Node) (logicalplan.Node, annotations.Annotations, error) {
+	warns := annotations.New()
+
+	if root == nil {
+		return nil, *warns, fmt.Errorf("nil root node")
+	}
+
+	logicalplan.TraverseBottomUp(nil, &root, func(parent, current *logicalplan.Node) bool {
+
+		if (*current).Type() == logicalplan.BinaryNode {
+			ch := (*current).Children()
+
+			for _, child := range ch {
+				temp := (*child).Clone()
+				*child = NewRemoteNode()
+				*(*child).Children()[0] = temp
+			}
+		}
+
+		return false
+	})
+	return root, *warns, nil
+}

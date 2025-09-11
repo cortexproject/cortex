fc7d10b1bc277adf5bfcb6a90572d37d9888c4de
Essam Eldaly <eeldaly@amazon.com>
2025-09-11 10:49:31 -0700

    Add metric for number of series with discarded samples
    Signed-off-by: Essam Eldaly <eeldaly@amazon.com>

---
+++ pkg/util/discardedseries/tracker.go
@@ -0,0 +1,164 @@
+package discardedseries
+
+import (
+	"sync"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/prometheus/model/labels"
+)
+
+const (
+	vendMetricsInterval = 30 * time.Second
+)
+
+type labelCounterStruct struct {
+	*sync.RWMutex
+	*labels.Labels
+	inCurrentCycle bool
+}
+
+type seriesCounterStruct struct {
+	*sync.RWMutex
+	seriesCountMap map[uint64]*labelCounterStruct
+}
+
+type userCounterStruct struct {
+	*sync.RWMutex
+	userSeriesMap map[string]*seriesCounterStruct
+}
+
+type DiscardedSeriesTracker struct {
+	*sync.RWMutex
+	reasonUserMap        map[string]*userCounterStruct
+	discardedSeriesGauge *prometheus.GaugeVec
+}
+
+func NewDiscardedSeriesTracker(discardedSeriesGauge *prometheus.GaugeVec) *DiscardedSeriesTracker {
+	tracker := &DiscardedSeriesTracker{
+		RWMutex:              &sync.RWMutex{},
+		reasonUserMap:        make(map[string]*userCounterStruct),
+		discardedSeriesGauge: discardedSeriesGauge,
+	}
+	return tracker
+}
+
+func (t *DiscardedSeriesTracker) Track(reason string, user string, labels *labels.Labels) {
+	series := labels.Hash()
+	t.RLock()
+	userCounter, ok := t.reasonUserMap[reason]
+	t.RUnlock()
+	if !ok {
+		t.Lock()
+		userCounter, ok = t.reasonUserMap[reason]
+		if !ok {
+			userCounter = &userCounterStruct{
+				RWMutex:       &sync.RWMutex{},
+				userSeriesMap: make(map[string]*seriesCounterStruct),
+			}
+			t.reasonUserMap[reason] = userCounter
+		}
+		t.Unlock()
+	}
+
+	userCounter.RLock()
+	seriesCounter, ok := userCounter.userSeriesMap[user]
+	userCounter.RUnlock()
+	if !ok {
+		userCounter.Lock()
+		seriesCounter, ok = userCounter.userSeriesMap[user]
+		if !ok {
+			seriesCounter = &seriesCounterStruct{
+				RWMutex:        &sync.RWMutex{},
+				seriesCountMap: make(map[uint64]*labelCounterStruct),
+			}
+			userCounter.userSeriesMap[user] = seriesCounter
+		}
+		userCounter.Unlock()
+	}
+
+	seriesCounter.RLock()
+	labelCounter, ok := seriesCounter.seriesCountMap[series]
+	seriesCounter.RUnlock()
+	if !ok {
+		seriesCounter.Lock()
+		labelCounter, ok = seriesCounter.seriesCountMap[series]
+		if !ok {
+			labelCounter = &labelCounterStruct{
+				Labels:         labels,
+				RWMutex:        &sync.RWMutex{},
+				inCurrentCycle: true,
+			}
+			seriesCounter.seriesCountMap[series] = labelCounter
+		}
+		seriesCounter.Unlock()
+	}
+
+	labelCounter.Lock()
+	labelCounter.inCurrentCycle = true
+	labelCounter.Unlock()
+}
+
+func (t *DiscardedSeriesTracker) UpdateMetrics() {
+	usersToDelete := make([]string, 0)
+	t.RLock()
+	for reason, userCounter := range t.reasonUserMap {
+		userCounter.RLock()
+		for user, seriesCounter := range userCounter.userSeriesMap {
+			seriesCounter.Lock()
+			for hash, labelCounter := range seriesCounter.seriesCountMap {
+				labelCounter.Lock()
+				val := 0.0
+				if labelCounter.inCurrentCycle {
+					val = 1.0
+				}
+				t.discardedSeriesGauge.WithLabelValues(reason, user, labelCounter.Labels.String()).Set(val)
+				if !labelCounter.inCurrentCycle {
+					delete(seriesCounter.seriesCountMap, hash)
+				} else {
+					labelCounter.inCurrentCycle = false
+				}
+				labelCounter.Unlock()
+			}
+			if len(seriesCounter.seriesCountMap) == 0 {
+				usersToDelete = append(usersToDelete, user)
+			}
+			seriesCounter.Unlock()
+		}
+		userCounter.RUnlock()
+		userCounter.Lock()
+		for _, user := range usersToDelete {
+			_, ok := userCounter.userSeriesMap[user]
+			if ok && userCounter.userSeriesMap[user].seriesCountMap != nil {
+				delete(userCounter.userSeriesMap, user)
+			}
+		}
+		userCounter.Unlock()
+	}
+	t.RUnlock()
+}
+
+func (t *DiscardedSeriesTracker) StartDiscardedSeriesGoroutine() {
+	go func() {
+		ticker := time.NewTicker(vendMetricsInterval)
+		for range ticker.C {
+			t.UpdateMetrics()
+		}
+	}()
+}
+
+// only used in testing
+func (t *DiscardedSeriesTracker) getSeriesCount(reason string, user string) int {
+	count := -1
+	if userCounter, ok := t.reasonUserMap[reason]; ok {
+		if seriesCounter, ok := userCounter.userSeriesMap[user]; ok {
+			count = 0
+			for _, label := range seriesCounter.seriesCountMap {
+				if label.inCurrentCycle {
+					count++
+				}
+			}
+		}
+	}
+	return count
+}
---
+++ pkg/util/discardedseries/tracker_test.go
@@ -0,0 +1,139 @@
+package discardedseries
+
+import (
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/testutil"
+	"github.com/prometheus/prometheus/model/labels"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLabelSetTracker(t *testing.T) {
+	gauge := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "cortex_discarded_series_total",
+			Help: "The total number of series that include discarded samples.",
+		},
+		[]string{"reason", "user", "labelset"},
+	)
+
+	tracker := NewDiscardedSeriesTracker(gauge)
+	reason1 := "sample_out_of_bounds"
+	reason2 := "label_2"
+	reason3 := "unused_label"
+	user1 := "user1"
+	user2 := "user2"
+	series1 := labels.FromStrings("__name__", "1")
+	series2 := labels.FromStrings("__name__", "2")
+
+	tracker.Track(reason1, user1, &series1)
+	tracker.Track(reason2, user1, &series1)
+
+	tracker.Track(reason1, user2, &series1)
+	tracker.Track(reason1, user2, &series1)
+	tracker.Track(reason1, user2, &series1)
+	tracker.Track(reason1, user2, &series2)
+
+	require.Equal(t, tracker.getSeriesCount(reason1, user1), 1)
+	require.Equal(t, tracker.getSeriesCount(reason2, user1), 1)
+	require.Equal(t, tracker.getSeriesCount(reason3, user1), -1)
+
+	compareSeriesVendedCount(t, gauge, reason1, user1, &series1, 0)
+	compareSeriesVendedCount(t, gauge, reason1, user1, &series2, 0)
+	compareSeriesVendedCount(t, gauge, reason2, user1, &series1, 0)
+	compareSeriesVendedCount(t, gauge, reason2, user1, &series2, 0)
+	compareSeriesVendedCount(t, gauge, reason3, user1, &series1, 0)
+	compareSeriesVendedCount(t, gauge, reason3, user1, &series2, 0)
+
+	require.Equal(t, tracker.getSeriesCount(reason1, user2), 2)
+	require.Equal(t, tracker.getSeriesCount(reason2, user2), -1)
+	require.Equal(t, tracker.getSeriesCount(reason3, user2), -1)
+
+	compareSeriesVendedCount(t, gauge, reason1, user2, &series1, 0)
+	compareSeriesVendedCount(t, gauge, reason1, user2, &series2, 0)
+	compareSeriesVendedCount(t, gauge, reason2, user2, &series1, 0)
+	compareSeriesVendedCount(t, gauge, reason2, user2, &series2, 0)
+	compareSeriesVendedCount(t, gauge, reason3, user2, &series1, 0)
+	compareSeriesVendedCount(t, gauge, reason3, user2, &series2, 0)
+
+	tracker.UpdateMetrics()
+
+	tracker.Track(reason1, user1, &series1)
+	tracker.Track(reason1, user1, &series1)
+
+	require.Equal(t, tracker.getSeriesCount(reason1, user1), 1)
+	require.Equal(t, tracker.getSeriesCount(reason2, user1), 0)
+	require.Equal(t, tracker.getSeriesCount(reason3, user1), -1)
+
+	compareSeriesVendedCount(t, gauge, reason1, user1, &series1, 1)
+	compareSeriesVendedCount(t, gauge, reason1, user1, &series2, 0)
+	compareSeriesVendedCount(t, gauge, reason2, user1, &series1, 1)
+	compareSeriesVendedCount(t, gauge, reason2, user1, &series2, 0)
+	compareSeriesVendedCount(t, gauge, reason3, user1, &series1, 0)
+	compareSeriesVendedCount(t, gauge, reason3, user1, &series2, 0)
+
+	require.Equal(t, tracker.getSeriesCount(reason1, user2), 0)
+	require.Equal(t, tracker.getSeriesCount(reason2, user2), -1)
+	require.Equal(t, tracker.getSeriesCount(reason3, user2), -1)
+
+	compareSeriesVendedCount(t, gauge, reason1, user2, &series1, 1)
+	compareSeriesVendedCount(t, gauge, reason1, user2, &series2, 1)
+	compareSeriesVendedCount(t, gauge, reason2, user2, &series1, 0)
+	compareSeriesVendedCount(t, gauge, reason2, user2, &series2, 0)
+	compareSeriesVendedCount(t, gauge, reason3, user2, &series1, 0)
+	compareSeriesVendedCount(t, gauge, reason3, user2, &series2, 0)
+
+	tracker.UpdateMetrics()
+
+	require.Equal(t, tracker.getSeriesCount(reason1, user1), 0)
+	require.Equal(t, tracker.getSeriesCount(reason2, user1), -1)
+	require.Equal(t, tracker.getSeriesCount(reason3, user1), -1)
+
+	compareSeriesVendedCount(t, gauge, reason1, user1, &series1, 1)
+	compareSeriesVendedCount(t, gauge, reason1, user1, &series2, 0)
+	compareSeriesVendedCount(t, gauge, reason2, user1, &series1, 0)
+	compareSeriesVendedCount(t, gauge, reason2, user1, &series2, 0)
+	compareSeriesVendedCount(t, gauge, reason3, user1, &series1, 0)
+	compareSeriesVendedCount(t, gauge, reason3, user1, &series2, 0)
+
+	require.Equal(t, tracker.getSeriesCount(reason1, user2), -1)
+	require.Equal(t, tracker.getSeriesCount(reason2, user2), -1)
+	require.Equal(t, tracker.getSeriesCount(reason3, user2), -1)
+
+	compareSeriesVendedCount(t, gauge, reason1, user2, &series1, 0)
+	compareSeriesVendedCount(t, gauge, reason1, user2, &series2, 0)
+	compareSeriesVendedCount(t, gauge, reason2, user2, &series1, 0)
+	compareSeriesVendedCount(t, gauge, reason2, user2, &series2, 0)
+	compareSeriesVendedCount(t, gauge, reason3, user2, &series1, 0)
+	compareSeriesVendedCount(t, gauge, reason3, user2, &series2, 0)
+
+	tracker.UpdateMetrics()
+
+	require.Equal(t, tracker.getSeriesCount(reason1, user1), -1)
+	require.Equal(t, tracker.getSeriesCount(reason2, user1), -1)
+	require.Equal(t, tracker.getSeriesCount(reason3, user1), -1)
+
+	compareSeriesVendedCount(t, gauge, reason1, user1, &series1, 0)
+	compareSeriesVendedCount(t, gauge, reason1, user1, &series2, 0)
+	compareSeriesVendedCount(t, gauge, reason2, user1, &series1, 0)
+	compareSeriesVendedCount(t, gauge, reason2, user1, &series2, 0)
+	compareSeriesVendedCount(t, gauge, reason3, user1, &series1, 0)
+	compareSeriesVendedCount(t, gauge, reason3, user1, &series2, 0)
+
+	require.Equal(t, tracker.getSeriesCount(reason1, user2), -1)
+	require.Equal(t, tracker.getSeriesCount(reason2, user2), -1)
+	require.Equal(t, tracker.getSeriesCount(reason3, user2), -1)
+
+	compareSeriesVendedCount(t, gauge, reason1, user2, &series1, 0)
+	compareSeriesVendedCount(t, gauge, reason1, user2, &series2, 0)
+	compareSeriesVendedCount(t, gauge, reason2, user2, &series1, 0)
+	compareSeriesVendedCount(t, gauge, reason2, user2, &series2, 0)
+	compareSeriesVendedCount(t, gauge, reason3, user2, &series1, 0)
+	compareSeriesVendedCount(t, gauge, reason3, user2, &series2, 0)
+}
+
+func compareSeriesVendedCount(t *testing.T, gaugeVec *prometheus.GaugeVec, reason string, user string, labels *labels.Labels, val int) {
+	gauge, _ := gaugeVec.GetMetricWithLabelValues(reason, user, labels.String())
+	require.Equal(t, testutil.ToFloat64(gauge), float64(val))
+}

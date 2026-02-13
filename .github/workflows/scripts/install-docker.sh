dcb143608b7c93c14bfb55e9e9e644acffcd4adb
Friedrich Gonzalez <1517449+friedrichg@users.noreply.github.com>
2026-02-12 21:04:21 -0800

    Update buildx too
    Signed-off-by: Friedrich Gonzalez <1517449+friedrichg@users.noreply.github.com>

--- .github/workflows/scripts/install-docker.sh
+++ .github/workflows/scripts/install-docker.sh
@@ -25,7 +25,7 @@ echo "Installing Docker $VER for architecture: $ARCH (docker: $DOCKER_ARCH, buil
 curl -L -o /tmp/docker-$VER.tgz https://download.docker.com/linux/static/stable/$DOCKER_ARCH/docker-$VER.tgz
 tar -xz -C /tmp -f /tmp/docker-$VER.tgz
 mkdir -vp ~/.docker/cli-plugins/
-curl --silent -L "https://github.com/docker/buildx/releases/download/v0.3.0/buildx-v0.3.0.linux-$BUILDX_ARCH" > ~/.docker/cli-plugins/docker-buildx
+curl --silent -L "https://github.com/docker/buildx/releases/download/v0.31.1/buildx-v0.31.1.linux-$BUILDX_ARCH" > ~/.docker/cli-plugins/docker-buildx
 chmod a+x ~/.docker/cli-plugins/docker-buildx
 mv /tmp/docker/* /usr/bin
 docker run --privileged --rm tonistiigi/binfmt --install all

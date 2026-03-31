1c3719584320508b53318bc876865a6789355563
Essam Eldaly <eeldaly@amazon.com>
2026-03-31 09:41:20 -0700

    check protos
    Signed-off-by: Essam Eldaly <eeldaly@amazon.com>

--- pkg/querier/stats/stats.pb.go
+++ pkg/querier/stats/stats.pb.go
@@ -5,12 +5,6 @@ package stats
 
 import (
 	fmt "fmt"
-	io "io"
-	math "math"
-	math_bits "math/bits"
-	reflect "reflect"
-	strings "strings"
-	time "time"
 
 	_ "github.com/gogo/protobuf/gogoproto"
 	proto "github.com/gogo/protobuf/proto"
@@ -18,6 +12,12 @@ import (
 	_ "github.com/gogo/protobuf/types"
 	github_com_gogo_protobuf_types "github.com/gogo/protobuf/types"
 	_ "google.golang.org/protobuf/types/known/durationpb"
+	io "io"
+	math "math"
+	math_bits "math/bits"
+	reflect "reflect"
+	strings "strings"
+	time "time"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.

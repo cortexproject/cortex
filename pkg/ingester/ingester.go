542aa4c1e92d43a299c90646756023e90d24ce3a
Kyle Stang <33678573+kylestang@users.noreply.github.com>
2026-09-16 20:31:53 -0700

    Gracefully skip compaction during shipping (#7842)
    This commit makes ingesters prevented from performing head compaction
    due to block shipping skip compaction rather than treating it as a
    failure.
    
    The shipping/compaction race is intentional functionality and not an
    error. With the old functionality, we would get alarms when these two
    events overlapped, even though there were no issues and compaction
    succeeded on the next attempt.
    
    Signed-off-by: Kyle Stang <kylestng@amazon.com>
    (cherry picked from commit 377177d4d72776868d65b2869524977dda5bd0ee)
    Signed-off-by: Charlie Le <charlie_le@apple.com>

--- pkg/ingester/ingester.go
+++ pkg/ingester/ingester.go
@@ -111,6 +111,7 @@ var (
 	errIngesterStopping = errors.New("ingester stopping")
 	errNoUserDb         = errors.New("no user db")
 	errLabelsOutOfOrder = errors.New("labels out of order")
+	errTsdbShipping     = errors.New("tsdb is in state activeShipping")
 
 	tsChunksPool zeropool.Pool[[]client.TimeSeriesChunk]
 
@@ -471,21 +472,24 @@ func (u *userTSDB) StartTime() (int64, error) {
 	return u.db.StartTime()
 }
 
-func (u *userTSDB) casState(from, to tsdbState) bool {
+func (u *userTSDB) casState(from, to tsdbState) (bool, tsdbState) {
 	u.stateMtx.Lock()
 	defer u.stateMtx.Unlock()
 
 	if u.state != from {
-		return false
+		return false, u.state
 	}
 	u.state = to
-	return true
+	return true, u.state
 }
 
 // compactHead compacts the Head block at specified block durations avoiding a single huge block.
 func (u *userTSDB) compactHead(ctx context.Context, blockDuration int64) error {
-	if !u.casState(active, forceCompacting) {
-		return errors.New("TSDB head cannot be compacted because it is not in active state (possibly being closed or blocks shipping in progress)")
+	if success, state := u.casState(active, forceCompacting); !success {
+		if state == activeShipping {
+			return errTsdbShipping
+		}
+		return fmt.Errorf("TSDB head cannot be compacted because it is not in active state (state: %d)", state)
 	}
 
 	defer u.casState(forceCompacting, active)
@@ -3409,7 +3413,7 @@ func (i *Ingester) shipBlocks(ctx context.Context, allowed *users.AllowedTenants
 
 		// Run the shipper's Sync() to upload unshipped blocks. Make sure the TSDB state is active, in order to
 		// avoid any race condition with closing idle TSDBs.
-		if !userDB.casState(active, activeShipping) {
+		if success, _ := userDB.casState(active, activeShipping); !success {
 			level.Info(logutil.WithContext(ctx, i.logger)).Log("msg", "shipper skipped because the TSDB is not active", "user", userID)
 			return nil
 		}
@@ -3523,8 +3527,13 @@ func (i *Ingester) compactBlocks(ctx context.Context, force bool, allowed *users
 		}
 
 		if err != nil {
-			i.TSDBState.compactionsFailed.Inc()
-			level.Warn(logutil.WithContext(ctx, i.logger)).Log("msg", "TSDB blocks compaction for user has failed", "user", userID, "err", err, "compactReason", reason)
+			// Don't treat blocks shipping as a failure
+			if errors.Is(err, errTsdbShipping) {
+				level.Info(logutil.WithContext(ctx, i.logger)).Log("msg", "TSDB blocks compaction for user was skipped", "user", userID, "err", err, "compactReason", reason)
+			} else {
+				i.TSDBState.compactionsFailed.Inc()
+				level.Warn(logutil.WithContext(ctx, i.logger)).Log("msg", "TSDB blocks compaction for user has failed", "user", userID, "err", err, "compactReason", reason)
+			}
 		} else {
 			level.Debug(logutil.WithContext(ctx, i.logger)).Log("msg", "TSDB blocks compaction completed successfully", "user", userID, "compactReason", reason)
 		}
@@ -3574,7 +3583,7 @@ func (i *Ingester) closeAndDeleteUserTSDBIfIdle(userID string) tsdbCloseCheckRes
 	}
 
 	// This disables pushes and force-compactions. Not allowed to close while shipping is in progress.
-	if !userDB.casState(active, closing) {
+	if success, _ := userDB.casState(active, closing); !success {
 		return tsdbNotActive
 	}
 
